01c052dfa29afb00f1750a622816add33932a8f3
Matthias Kuhn <matthias.kuhn@gmx.ch>
2014-12-24 22:47:07 +0100

    Add basic OpenGL point symbol

--- src/qgssggeometry.h
+++ src/qgssggeometry.h
@@ -20,6 +20,7 @@ class QgsSGGeometry : public QSGNode
 
     static QSGGeometry* qgsPolylineToQSGGeometry( const QgsPolyline& line, int width );
     static QSGGeometry* qgsPolygonToQSGGeometry( const QgsPolygon& polygon );
+    static QSGGeometry* qgsPointToQSGGeometry( const QgsPoint& point, int width );
 
     QSGFlatColorMaterial mMaterial;
 };
