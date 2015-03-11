221975e5d289df7ea7bd56ba4767954700ae1e40
Matthias Kuhn <matthias.kuhn@gmx.ch>
2015-03-11 09:08:53 +0100

    Add coordinate transform object

---
+++ src/coordinatetransform.cpp
@@ -0,0 +1,79 @@
+/***************************************************************************
+              coordinatetransform.cpp
+               ----------------------------------------------------
+              date                 : 26.2.2015
+              copyright            : (C) 2015 by Matthias Kuhn
+              email                : matthias (at) opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "coordinatetransform.h"
+#include <QDebug>
+
+CoordinateTransform::CoordinateTransform(QObject *parent) :
+  QObject(parent)
+, mSourceCRS( 0 )
+, mDestinationCRS( 0 )
+{
+  connect( this, SIGNAL(destinationCRSChanged()), this, SLOT(updateTransform()));
+  connect( this, SIGNAL(sourceCRSChanged()), this, SLOT(updateTransform()));
+}
+
+const QPointF CoordinateTransform::transform( const QPointF& pt ) const
+{
+  qDebug() << "Transforming " << pt.x() << ", " << pt.y();
+  QgsPoint p( pt.x(), pt.y() );
+
+  p = mTransform.transform( p );
+
+  qDebug() << "Result " << p.x() << ", " << p.y();
+  return QPointF( p.x(), p.y() );
+}
+
+CRS* CoordinateTransform::sourceCRS() const
+{
+  return mSourceCRS;
+}
+
+CRS* CoordinateTransform::destinationCRS() const
+{
+  return mDestinationCRS;
+}
+
+void CoordinateTransform::setSourceCRS( CRS* sourceCRS )
+{
+  if ( mSourceCRS != sourceCRS )
+  {
+    mSourceCRS = sourceCRS;
+    connect( mSourceCRS, SIGNAL(sridChanged()), this, SLOT(updateTransform()) );
+    updateTransform();
+    emit sourceCRSChanged();
+  }
+}
+
+void CoordinateTransform::setDestinationCRS( CRS* destCRS )
+{
+  if ( mDestinationCRS != destCRS )
+  {
+    mDestinationCRS = destCRS;
+    connect( mDestinationCRS, SIGNAL(sridChanged()), this, SLOT(updateTransform()) );
+    updateTransform();
+    emit destinationCRSChanged();
+  }
+}
+
+void CoordinateTransform::updateTransform()
+{
+  if ( mSourceCRS && mDestinationCRS )
+  {
+    mTransform.setSourceCrs( mSourceCRS->crs() );
+    mTransform.setDestCRS( mDestinationCRS->crs() );
+    mTransform.initialise();
+  }
+}
