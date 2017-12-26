63a4e5ff2ca8ec49ef533d9568ce14ce24965feb
Matthias Kuhn <matthias@opengis.ch>
2017-12-26 09:27:32 +0100

    Add apk signing to docker build scripts

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This is intended to be run within q field-sdk docker container.
+# This is intended to be run within a opengisch/qfield-sdk docker container.
 #
 #     docker run -v $(pwd):/usr/src/qfield opengisch/qfield-sdk /usr/src/qfield/scripts/docker-build.sh
 #
@@ -13,11 +13,33 @@ SOURCE_DIR=/usr/src/qfield
 BUILD_DIR=${SOURCE_DIR}/build-docker
 INSTALL_DIR=${SOURCE_DIR}/build-docker/out
 
-mkdir ${BUILD_DIR}
+set -e
+
+mkdir -p ${BUILD_DIR}/.gradle
+# androiddeployqt needs gradle and downloads it to /root/.gradle. By linking it to the build folder, this will be cached between builds.
+ln -s ${BUILD_DIR}/.gradle /root/.gradle
+
 pushd ${BUILD_DIR}
 cp ${SOURCE_DIR}/scripts/ci/config.pri ${SOURCE_DIR}/config.pri
 ${QT_ANDROID}/bin/qmake ${SOURCE_DIR}/QField.pro
 make
 make install INSTALL_ROOT=${INSTALL_DIR}
-androiddeployqt --input ${BUILD_DIR}/src/android-libqfield.so-deployment-settings.json --output ${INSTALL_DIR} --deployment bundled --android-platform ${ANDROID_NDK_PLATFORM} --gradle
+if [ -n "${KEYNAME}" ]; then
+    androiddeployqt \
+	    --sign ${SOURCE_DIR}/playstore_keystore.jks "${KEYNAME}" \
+	    --storepass "${STOREPASS}" \
+	    --keypass "${KEYPASS}" \
+	    --input ${BUILD_DIR}/src/android-libqfield.so-deployment-settings.json \
+	    --output ${INSTALL_DIR} \
+	    --deployment bundled \
+	    --android-platform ${ANDROID_NDK_PLATFORM} \
+	    --gradle
+else
+    androiddeployqt \
+	    --input ${BUILD_DIR}/src/android-libqfield.so-deployment-settings.json \
+	    --output ${INSTALL_DIR} \
+	    --deployment bundled \
+	    --android-platform ${ANDROID_NDK_PLATFORM} \
+	    --gradle
+fi
 popd
