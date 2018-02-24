2af1f42df937ca7fdc776db64f76bce7cf2c05ac
Matthias Kuhn <matthias@opengis.ch>
2018-02-24 01:13:20 +0100

    Fix private key file mod

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,7 @@ then
   echo -e "\e[31mDo the release\e[0m"
   sudo mv build-docker/out/build/outputs/apk/out-release-signed.apk build-docker/out/build/outputs/apk/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}.apk
   openssl aes-256-cbc -K $encrypted_dev_upload_key -iv $encrypted_dev_upload_iv -in .ci/id_rsa.enc -out .ci/id_rsa -d
+  chmod 400 .ci/id_rsa
   scp -i .ci/id_rsa -o "StrictHostKeyChecking no" build-docker/out/build/outputs/apk/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}.apk ${DEV_UPLOAD_USER}@${DEV_UPLOAD_HOST}:~/ci-builds/
   echo -e "\e[31Uploaded to https://opengis.ch/download/qfield/ci-builds/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}.apk \e[0m"
   curl -u m-kuhn:${GITHUB_API_TOKEN} -X POST --data '{"body": "Uploaded test apk to https://opengis.ch/download/qfield/ci-builds/qfield-dev-'${TRAVIS_PULL_REQUEST}'-'${TRAVIS_COMMIT}'.apk"}' https://api.github.com/repos/opengisch/QField/issues/${TRAVIS_PULL_REQUEST}/comments
