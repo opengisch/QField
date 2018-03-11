ae40a201433c5769edcfcdc5b1065a18fb329a6e
Matthias Kuhn <matthias@opengis.ch>
2018-03-11 16:42:38 -0400

    Add manual release upload script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,18 +2,20 @@
 
 set -e
 
-if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" -a "${TRAVIS_PULL_REQUEST}" != "false" ];
+if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" ];
 then
-  echo -e "\e[31mDeploying app\e[0m"
-  curl -u m-kuhn:${GITHUB_API_TOKEN} -X POST --data '{"body": "Uploaded test apks for [armv7](https://opengis.ch/download/qfield/ci-builds/qfield-dev-'${TRAVIS_PULL_REQUEST}'-'${TRAVIS_COMMIT}'-armv7.apk) and [x86](https://opengis.ch/download/qfield/ci-builds/qfield-dev-'${TRAVIS_PULL_REQUEST}'-'${TRAVIS_COMMIT}'-x86.apk)"}' https://api.github.com/repos/opengisch/QField/issues/${TRAVIS_PULL_REQUEST}/comments
-else
-  echo -e "Skipping dev build upload because"
-  if [ "${TRAVIS_SECURE_ENV_VARS}" != "true" ];
+  if [ "${TRAVIS_PULL_REQUEST}" != "false" ];
   then
-    echo -e "  TRAVIS_SECURE_ENV_VARS is not true (${TRAVIS_SECURE_ENV_VARS})"
-  fi
-  if [ "${TRAVIS_SECURE_ENV_VARS}" = "false" ];
+    echo -e "\e[31mDeploying app to pull request\e[0m"
+    curl -u m-kuhn:${GITHUB_API_TOKEN} -X POST --data '{"body": "Uploaded test apks for [armv7](https://opengis.ch/download/qfield/ci-builds/qfield-dev-'${TRAVIS_PULL_REQUEST}'-'${TRAVIS_COMMIT}'-armv7.apk) and [x86](https://opengis.ch/download/qfield/ci-builds/qfield-dev-'${TRAVIS_PULL_REQUEST}'-'${TRAVIS_COMMIT}'-x86.apk)"}' https://api.github.com/repos/opengisch/QField/issues/${TRAVIS_PULL_REQUEST}/comments
+  fi 
+
+  if [ "${TRAVIS_TAG}X" != "X" ];
   then
-    echo -e "  TRAVIS_PULL_REQUEST is false (${TRAVIS_PULL_REQUEST})"
+    echo -e "\e[31mDeploying app to releases\e[0m"
+    wget -o /tmp/qfield-${TRAVIS_TAG}-armv7.apk https://opengis.ch/download/qfield/ci-builds/qfield-dev-'${TRAVIS_PULL_REQUEST}'-'${TRAVIS_COMMIT}'-armv7.apk
+    wget -o /tmp/qfield-${TRAVIS_TAG}-x86.apk https://opengis.ch/download/qfield/ci-builds/qfield-dev-'${TRAVIS_PULL_REQUEST}'-'${TRAVIS_COMMIT}'-x86.apk
+    ./release-upload.py --release=${TRAVIS_TAG} --oauth-token=${GITHUB_API_TOKEN} /tmp/qfield-${TRAVIS_TAG}-armv7.apk /tmp/qfield-${TRAVIS_TAG}-x86.apk
+    echo -e "Skipping dev build upload because"
   fi
 fi
---
+++ scripts/release-upload.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""This script creates a new release on github releases
+        Author: Matthias Kuhn (matthias@opengis.ch)
+        git sha              : $TemplateVCSFormat
+"""
+from __future__ import print_function
+from future import standard_library
+
+import sys
+import json
+import http.client
+import os
+from optparse import OptionParser
+
+
+def main(parameters, arguments):
+    """Main entry point.
+
+    :param parameters: Command line parameters.
+    :param arguments: Command line arguments.
+    """
+
+    conn = http.client.HTTPSConnection('api.github.com')
+    headers = {
+        'User-Agent': 'Deploy-Script',
+        'Authorization': 'token {}'.format(parameters.oauth_token)
+    }
+
+    raw_data = {
+        "tag_name": parameters.release
+    }
+    if parameters.changelog:
+        with open(parameters.changelog, 'r') as cl:
+            raw_data['body'] = cl.read()
+    data = json.dumps(raw_data)
+    conn.request('POST', '/repos/{repo_slug}/releases'.format(
+        repo_slug=os.environ['TRAVIS_REPO_SLUG']), body=data, headers=headers)
+    response = conn.getresponse()
+    release = json.loads(response.read().decode())
+    print(release)
+
+    conn = http.client.HTTPSConnection('uploads.github.com')
+    headers['Content-Type'] = 'application/octet-stream'
+    for filename in arguments:
+        url = '{}?name={}'.format(release['upload_url'][:-13], filename)
+        print('Upload to {}'.format(url))
+
+        with open(filename, 'rb') as f:
+            conn.request('POST', url, f, headers)
+
+        print(conn.getresponse().read())
+
+
+if __name__ == "__main__":
+    parser = OptionParser(usage="%prog [options] file [file file]")
+    parser.add_option(
+        "-r", "--release", dest="release",
+        help="Specify the release name", metavar="v1.0.0")
+    parser.add_option(
+        "-a", "--oauth-token", dest="oauth_token",
+        help="Specify the github oauth_token to use")
+    parser.add_option(
+        "-c", "--changelog", dest="changelog",
+        help="Specify the changelog file", metavar="/tmp/changelog")
+    options, args = parser.parse_args()
+    if len(args) != 1:
+        print("Please a release file.\n")
+        parser.print_help()
+        sys.exit(1)
+
+    main(options, args)
