79cfd99a83bfa623ba2443485fc3836d6a509e71
Matthias Kuhn <matthias@opengis.ch>
2018-03-11 09:19:59 -0400

    Deploy files via existing local filepaths

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,12 @@ set -e
 if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" -a "${TRAVIS_PULL_REQUEST}" != "false" ];
 then
   echo -e "\e[31mDo the release\e[0m"
-  sudo mv build-armv7/out/build/outputs/apk/out-release-signed.apk build-docker/out/build/outputs/apk/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}-armv7.apk
-  sudo mv build-x86/out/build/outputs/apk/out-release-signed.apk build-docker/out/build/outputs/apk/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}-x86.apk
+  sudo mv build-armv7/out/build/outputs/apk/out-release-signed.apk /tmp/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}-armv7.apk
+  sudo mv build-x86/out/build/outputs/apk/out-release-signed.apk /tmp/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}-x86.apk
   openssl aes-256-cbc -K $encrypted_dev_upload_key -iv $encrypted_dev_upload_iv -in .ci/id_rsa.enc -out .ci/id_rsa -d
   chmod 400 .ci/id_rsa
-  scp -i .ci/id_rsa -o "StrictHostKeyChecking no" build-armv7/out/build/outputs/apk/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}.apk ${DEV_UPLOAD_USER}@${DEV_UPLOAD_HOST}:~/ci-builds/
-  scp -i .ci/id_rsa -o "StrictHostKeyChecking no" build-x86/out/build/outputs/apk/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}.apk ${DEV_UPLOAD_USER}@${DEV_UPLOAD_HOST}:~/ci-builds/
+  scp -i .ci/id_rsa -o "StrictHostKeyChecking no" /tmp/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}-armv7.apk ${DEV_UPLOAD_USER}@${DEV_UPLOAD_HOST}:~/ci-builds/
+  scp -i .ci/id_rsa -o "StrictHostKeyChecking no" /tmp/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}-x86.apk ${DEV_UPLOAD_USER}@${DEV_UPLOAD_HOST}:~/ci-builds/
   echo -e "\e[31Uploaded to https://opengis.ch/download/qfield/ci-builds/qfield-dev-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}-armv7.apk \e[0m"
   curl -u m-kuhn:${GITHUB_API_TOKEN} -X POST --data '{"body": "Uploaded test apks for [armv7](https://opengis.ch/download/qfield/ci-builds/qfield-dev-'${TRAVIS_PULL_REQUEST}'-'${TRAVIS_COMMIT}'-armv7.apk) and [x86](https://opengis.ch/download/qfield/ci-builds/qfield-dev-'${TRAVIS_PULL_REQUEST}'-'${TRAVIS_COMMIT}'-x86.apk)"}' https://api.github.com/repos/opengisch/QField/issues/${TRAVIS_PULL_REQUEST}/comments
 else
