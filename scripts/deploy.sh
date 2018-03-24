06ab62767b66d4b197e150e420d1709a7afac721
Matthias Kuhn <matthias@opengis.ch>
2018-03-24 10:37:19 +0100

    Improve output of deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,17 +13,17 @@ then
 
   if [ "${TRAVIS_TAG}X" != "X" ];
   then
-    echo -e "\e[31mStarting to deploy a new release\e[0m"
+    echo -e "\e[93;1mStarting to deploy a new release\e[0m"
     openssl aes-256-cbc -K $encrypted_play_upload_key -iv $encrypted_play_upload_iv -in .ci/play_developer.p12.enc -out .ci/play_developer.p12 -d
-    echo -e "\e[31m * Collecting apks to upload...\e[0m"
-    wget -O /tmp/qfield-${TRAVIS_TAG}-armv7.apk https://opengis.ch/download/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-armv7.apk
-    wget -O /tmp/qfield-${TRAVIS_TAG}-x86.apk https://opengis.ch/download/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-x86.apk
+    echo -e "\e[93m * Collecting apks to upload...\e[0m"
+    curl -s -S -o /tmp/qfield-${TRAVIS_TAG}-armv7.apk https://opengis.ch/download/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-armv7.apk
+    curl -s -S -o /tmp/qfield-${TRAVIS_TAG}-x86.apk https://opengis.ch/download/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-x86.apk
 
-#    echo -e "\e[31m * Deploying app to google play...\e[0m"
+#    echo -e "\e[93m * Deploying app to google play...\e[0m"
 #    ./scripts/basic_upload_apks_service_account.py ch.opengis.qfield /tmp/qfield-${TRAVIS_TAG}-armv7.apk
 #    ./scripts/basic_upload_apks_service_account.py ch.opengis.qfield /tmp/qfield-${TRAVIS_TAG}-x86.apk
 
-    echo -e "\e[31m * Deploying app to github release...\e[0m"
+    echo -e "\e[93m * Deploying app to github release...\e[0m"
     ./scripts/release-upload.py --release=${TRAVIS_TAG} --oauth-token=${GITHUB_API_TOKEN} /tmp/qfield-${TRAVIS_TAG}-armv7.apk /tmp/qfield-${TRAVIS_TAG}-x86.apk
   fi
 fi
