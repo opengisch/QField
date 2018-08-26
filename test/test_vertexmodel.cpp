3d180ab6915ec1dd1a5ba9740a92475101b33e1a
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-08-26 09:03:30 +0300

    some test working

--- test/test_vertexmodel.cpp
+++ test/test_vertexmodel.cpp
@@ -2,25 +2,98 @@
 
 #include <QtTest>
 
+
+#include <qgslinestring.h>
+#include <qgsgeometry.h>
+#include <qgspoint.h>
+#include <qgspointxy.h>
+
 #include "vertexmodel.h"
 
 
 class TestVertexModel: public QObject
 {
     Q_OBJECT
-private slots:
+  private slots:
     void initTestCase()
-    { qDebug("called before everything else"); }
-    void myFirstTest()
-    { QVERIFY(1 == 1); }
-    void mySecondTest()
-    { QVERIFY(1 == 2); }
-    void cleanupTestCase()
-    { qDebug("called after myFirstTest and mySecondTest"); }
+    {
+      mModel = new VertexModel();
 
-private:
-    VertexModel mModel;
+      QgsLineString *lineString = new QgsLineString( QVector<QgsPoint>() << QgsPoint( 1, 1 ) << QgsPoint( 2, 1 ) << QgsPoint( 3, 1 ) );
+      mLineGeometry = QgsGeometry( lineString );
+
+      mPolygonGeometry = QgsGeometry::fromPolygonXY( QVector<QVector<QgsPointXY>>()
+                         <<  ( QVector<QgsPointXY>()
+                               << QgsPointXY( 0, 0 )
+                               << QgsPointXY( 2, 0 )
+                               << QgsPointXY( 2, 2 )
+                               << QgsPointXY( 0, 2 )
+                               << QgsPointXY( 0, 0 ) ) ) ;
+    }
+
+    void canRemoveVertexTest()
+    {
+      // line
+      mModel->setGeometry( mLineGeometry, QgsCoordinateReferenceSystem() );
+      QVERIFY( !mModel->canRemoveVertex() );
+      mModel->setEditingMode( VertexModel::EditVertex );
+      QVERIFY( mModel->canRemoveVertex() );
+
+      mModel->setEditingMode( VertexModel::NoEditing );
+      QCOMPARE( mModel->editingMode(), VertexModel::NoEditing );
+      mModel->setCurrentVertex( 0 );
+      QCOMPARE( mModel->editingMode(), VertexModel::EditVertex );
+
+      QCOMPARE( mModel->vertexCount(), 3 );
+      mModel->removeCurrentVertex();
+      QCOMPARE( mModel->vertexCount(), 2 );
+      QVERIFY( !mModel->canRemoveVertex() );
+
+      // polygon
+      mModel->setGeometry(mPolygonGeometry, QgsCoordinateReferenceSystem());
+      QCOMPARE( mModel->editingMode(), VertexModel::NoEditing );
+      QVERIFY( !mModel->canRemoveVertex() );
+      mModel->setCurrentVertex( 0 );
+      mModel->removeCurrentVertex();
+      QCOMPARE( mModel->vertexCount(), 3 );
+      QVERIFY( !mModel->canRemoveVertex() );
+    }
+
+    void transformTest()
+    {
+      // TODO
+    }
+
+    void returnGeometryTest()
+    {
+      // TODO
+    }
+
+    void xxxTest()
+    {
+      // TODO
+    }
+
+    void yyyTest()
+    {
+      // TODO
+    }
+
+    void zzzTest()
+    {
+      // TODO
+    }
+
+    void cleanupTestCase()
+    {
+      delete mModel;
+    }
+
+  private:
+    VertexModel *mModel;
+    QgsGeometry mLineGeometry;
+    QgsGeometry mPolygonGeometry;
 };
 
-QTEST_MAIN(TestVertexModel)
+QTEST_MAIN( TestVertexModel )
 #include "test_vertexmodel.moc"
