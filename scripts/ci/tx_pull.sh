7fddf752e22e47398f72304f8c31b49dd8e7323b
Matthias Kuhn <matthias@opengis.ch>
2018-09-16 17:13:12 +0200

    Remove i18n folder from sources

--- scripts/ci/tx_pull.sh
+++ scripts/ci/tx_pull.sh
@@ -4,5 +4,6 @@ source $(git rev-parse --show-toplevel)/scripts/ci/envvars.sh
 
 echo "travis_fold:start:tx-pull"
 echo "${bold}Pulling translations${endbold}"
+mkdir -p $(git rev-parse --show-toplevel)/i18n
 tx pull -a
 echo "travis_fold:end:tx-pull"
