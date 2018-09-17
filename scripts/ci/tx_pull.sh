5c6df9033db97162f3f5c4a153b03b20a57f2e3b
Matthias Kuhn <matthias@opengis.ch>
2018-09-17 08:49:52 +0200

    Create translation list on build

--- scripts/ci/tx_pull.sh
+++ scripts/ci/tx_pull.sh
@@ -6,4 +6,16 @@ echo "travis_fold:start:tx-pull"
 echo "${bold}Pulling translations${endbold}"
 mkdir -p $(git rev-parse --show-toplevel)/i18n
 tx pull -a
+
+echo '<RCC>' > i18n/translations.qrc
+echo '    <qresource prefix="/">' >> i18n/translations.qrc
+for filename in i18n/*.ts; do
+  filename=$(basename -- "$filename")
+  filename="${filename%.*}"
+  echo "        <file>${filename}.qm</file>" >> i18n/translations.qrc
+done
+echo '' >> i18n/translations.qrc
+echo '    </qresource>' >> i18n/translations.qrc
+echo '</RCC>' >> i18n/translations.qrc
+
 echo "travis_fold:end:tx-pull"
