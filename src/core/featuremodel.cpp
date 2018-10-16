8f5f4aa9c0f6a8431dd5ee98947026b525e583b3
signedav <david@opengis.ch>
2018-10-16 15:37:42 +0200

    moved function

--- src/core/featuremodel.cpp
+++ src/core/featuremodel.cpp
@@ -27,7 +27,7 @@
 FeatureModel::FeatureModel( QObject *parent )
   : QAbstractListModel( parent )
 {
-  connect( this, &FeatureModel::modelReset, this, &FeatureModel::featureChanged ); 
+  connect( this, &FeatureModel::modelReset, this, &FeatureModel::featureChanged );
 }
 
 void FeatureModel::setFeature( const QgsFeature& feature )
@@ -39,11 +39,6 @@ void FeatureModel::setFeature( const QgsFeature& feature )
   mFeature = feature;
   endResetModel();
   emit featureChanged();
-}
-
-void FeatureModel::clearRememberings()
-{
-  mRememberings.clear();
 }
 
 void FeatureModel::setCurrentLayer( QgsVectorLayer* layer )
@@ -262,6 +257,11 @@ void FeatureModel::resetAttributes()
     }
   }
   endResetModel();
+}
+
+void FeatureModel::clearRememberings()
+{
+  mRememberings.clear();
 }
 
 void FeatureModel::applyGeometry()
