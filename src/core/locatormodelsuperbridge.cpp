e744517ff66282d32f597936aacc99c913d67bc6
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-11-08 19:59:33 -0400

    trigger result works

--- src/core/locatormodelsuperbridge.cpp
+++ src/core/locatormodelsuperbridge.cpp
@@ -3,14 +3,14 @@
 
 #include "locatormodelsuperbridge.h"
 
-LocatorModelSuperBridge::LocatorModelSuperBridge(QObject *parent )
-  : QgsLocatorModelBridge(parent)
+LocatorModelSuperBridge::LocatorModelSuperBridge( QObject *parent )
+  : QgsLocatorModelBridge( parent )
 {
 }
 
-void LocatorModelSuperBridge::triggerResultAtRow(int row)
+void LocatorModelSuperBridge::triggerResultAtRow( int row )
 {
-  const QModelIndex index = mLocatorModel->index(row,0);
-  if (index.isValid())
-    triggerResult(index);
+  const QModelIndex index = mProxyModel->index( row, 0 );
+  if ( index.isValid() )
+    triggerResult( index );
 }
