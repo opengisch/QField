84161f737ec181f3e389047e3366ef46df4e2c43
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-01-21 21:18:45 -0500

    missing header

--- src/core/qgsgeometrywrapper.cpp
+++ src/core/qgsgeometrywrapper.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+  qgsgeometrywrapper.cpp - QgsGeometryWrapper
+
+ ---------------------
+ begin                : 18.08.2018
+ copyright            : (C) 2018 by Denis Rouzaud
+ email                : denis@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "qgsgeometrywrapper.h"
 
