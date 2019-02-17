b53a9a3f0945c732f76b46ab0f11ffebf228856b
Matthias Kuhn <matthias@opengis.ch>
2019-02-17 19:21:50 +0100

    Fix include

--- src/qgsquick/qgsquickmapcanvasmap.cpp
+++ src/qgsquick/qgsquickmapcanvasmap.cpp
@@ -22,7 +22,7 @@
 #include <qgspallabeling.h>
 #include <qgsproject.h>
 #include <qgsvectorlayer.h>
-#include <qgsexpressioncontextutils.h>
+#include <qgsexpressioncontext.h>
 #include "qgis.h"
 
 #include "qgsquickmapcanvasmap.h"
