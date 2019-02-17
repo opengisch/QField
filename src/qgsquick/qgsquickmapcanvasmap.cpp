837445ea4648fae3df3bee11aa36735e1311e43e
Matthias Kuhn <matthias@opengis.ch>
2019-02-17 22:52:41 +0100

    qgsexpressioncontextutils.h needs to be included

--- src/qgsquick/qgsquickmapcanvasmap.cpp
+++ src/qgsquick/qgsquickmapcanvasmap.cpp
@@ -22,7 +22,7 @@
 #include <qgspallabeling.h>
 #include <qgsproject.h>
 #include <qgsvectorlayer.h>
-#include <qgsexpressioncontext.h>
+#include <qgsexpressioncontextutils.h>
 #include "qgis.h"
 
 #include "qgsquickmapcanvasmap.h"
