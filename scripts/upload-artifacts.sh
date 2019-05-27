d8786b8eff4cf36cd4da1e14baecae1398267e97
Matthias Kuhn <matthias@opengis.ch>
2019-05-27 08:52:37 +0200

    Android 5 builds

--- scripts/upload-artifacts.sh
+++ scripts/upload-artifacts.sh
@@ -9,11 +9,11 @@ set -e
 if [[ "${TRAVIS_SECURE_ENV_VARS}" = "true" ]];
 then
   echo -e "\e[31mAbout to upload build artifacts\e[0m"
-  sudo mv build-${ARCH}/out/build/outputs/apk/release/out-release-signed.apk /tmp/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-${ARCH}.apk
+  sudo mv build-${ARCH}/out/build/outputs/apk/release/out-release-signed.apk /tmp/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-${ARCH}-${ANDROID_VERSION:-X}.apk
   openssl aes-256-cbc -K $encrypted_dev_upload_key -iv $encrypted_dev_upload_iv -in .ci/id_rsa.enc -out .ci/id_rsa -d
   chmod 400 .ci/id_rsa
-  scp -i .ci/id_rsa -o "StrictHostKeyChecking no" /tmp/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-${ARCH}.apk ${DEV_UPLOAD_USER}@${DEV_UPLOAD_HOST}:~/download.opengis.ch/qfield/ci-builds
-  echo -e "\e[31Uploaded to https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-${ARCH}.apk \e[0m"
+  scp -i .ci/id_rsa -o "StrictHostKeyChecking no" /tmp/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-${ARCH}-${ANDROID_VERSION:-X}.apk ${DEV_UPLOAD_USER}@${DEV_UPLOAD_HOST}:~/download.opengis.ch/qfield/ci-builds
+  echo -e "\e[31Uploaded to https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-${ARCH}-${ANDROID_VERSION:-X}.apk \e[0m"
 else
   echo -e "Not uploading artifacts ..."
   if [ "${TRAVIS_SECURE_ENV_VARS}" != "true" ];
