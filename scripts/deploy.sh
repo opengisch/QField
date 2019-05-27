559a6a329af6ce3d78c0fa981b4d722b3ebe8fd8
Matthias Kuhn <matthias@opengis.ch>
2019-05-27 13:46:11 +0200

    Improve deployment infrastructure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,16 +11,16 @@ if [[ ${TRAVIS_SECURE_ENV_VARS} = true ]]; then
     echo -e "\e[93;1mStarting to deploy a new release\e[0m"
     openssl aes-256-cbc -K $encrypted_play_upload_key -iv $encrypted_play_upload_iv -in .ci/play_developer.p12.enc -out .ci/play_developer.p12 -d
     echo -e "\e[93m * Collecting apks to upload...\e[0m"
-    curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-armv7-X.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-armv7-X.apk
-    curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-x86-X.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-x86-X.apk
-    curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-armv7-5.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-armv7-5.apk
-    curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-x86-5.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-x86-5.apk
+    curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-armv7.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-armv7-X.apk
+    curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-x86.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-x86-X.apk
+    curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-armv7-Android-5.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-armv7-5.apk
+    curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-x86-Android-5.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-x86-5.apk
 
     echo -e "\e[93m * Deploying app to github release...\e[0m"
-    ./scripts/release-upload.py --release=${TRAVIS_TAG} --oauth-token=${GITHUB_API_TOKEN} /tmp/qfield-${TRAVIS_TAG}-armv7-X.apk /tmp/qfield-${TRAVIS_TAG}-x86-X.apk /tmp/qfield-${TRAVIS_TAG}-x86-5.apk /tmp/qfield-${TRAVIS_TAG}-x86-5.apk
+    ./scripts/release-upload.py --release=${TRAVIS_TAG} --oauth-token=${GITHUB_API_TOKEN} /tmp/qfield-${TRAVIS_TAG}-armv7.apk /tmp/qfield-${TRAVIS_TAG}-x86.apk /tmp/qfield-${TRAVIS_TAG}-x86-Android-5.apk /tmp/qfield-${TRAVIS_TAG}-x86-Android-5.apk
 
     echo -e "\e[93m * Deploying app to google play (beta)...\e[0m"
-    ./scripts/basic_upload_apks_service_account.py ch.opengis.qfield /tmp/qfield-${TRAVIS_TAG}-armv7-5.apk ch.opengis.qfield /tmp/qfield-${TRAVIS_TAG}-x86-5.apk /tmp/qfield-${TRAVIS_TAG}-armv7-X.apk ch.opengis.qfield /tmp/qfield-${TRAVIS_TAG}-x86-X.apk
+    ./scripts/basic_upload_apks_service_account.py ch.opengis.qfield /tmp/qfield-${TRAVIS_TAG}-armv7-Android-5.apk ch.opengis.qfield /tmp/qfield-${TRAVIS_TAG}-x86-Android-5.apk /tmp/qfield-${TRAVIS_TAG}-armv7.apk ch.opengis.qfield /tmp/qfield-${TRAVIS_TAG}-x86.apk
 
   elif [[ ${TRAVIS_BRANCH} = master ]]; then
     # we are on a standard commit on master branch
