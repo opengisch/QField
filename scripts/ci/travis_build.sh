ae8219595bb7a0dc38b17d10135369ce6c3754e6
Matthias Kuhn <matthias@opengis.ch>
2019-10-15 15:11:55 +0200

    Fix versioning

--- scripts/ci/travis_build.sh
+++ scripts/ci/travis_build.sh
@@ -4,16 +4,13 @@ if [ -n "${TRAVIS_TAG}" ]; then
   APP_NAME="QField"
   PKG_NAME="qfield"
   APP_ICON="qfield-logo.svg"
-  APP_VERSION="${TRAVIS_TAG}"
 elif [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   APP_NAME="QField Nightly"
   PKG_NAME="qfield_nightly"
   APP_ICON="qfield-testlogo.svg"
-  APP_VERSION="Nightly ${TRAVIS_COMMIT}"
 else
   APP_NAME="QField Beta ${TRAVIS_PULL_REQUEST}"
   PKG_NAME="qfield_beta"
   APP_ICON="qfield-testlogo.svg"
-  APP_VERSION="Pull Request ${TRAVIS_PULL_REQUEST}"
 fi
-docker run -v $(pwd):/usr/src/qfield -e "BUILD_FOLDER=build-${ARCH}" -e ARCH -e STOREPASS -e KEYNAME -e KEYPASS -e "VERSION=${APP_VERSION}" -e PKG_NAME -e APP_NAME -e APP_ICON opengisch/qfield-sdk:${QFIELD_SDK_VERSION} /usr/src/qfield/scripts/docker-build.sh
+docker run -v $(pwd):/usr/src/qfield -e "BUILD_FOLDER=build-${ARCH}" -e ARCH -e STOREPASS -e KEYNAME -e KEYPASS -e "VERSION=${TRAVIS_TAG}" -e PKG_NAME -e APP_NAME -e APP_ICON opengisch/qfield-sdk:${QFIELD_SDK_VERSION} /usr/src/qfield/scripts/docker-build.sh
