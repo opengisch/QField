a81ed3581e26558a81f6dc2ea2cb359fd14012fb
Matthias Kuhn <matthias@opengis.ch>
2019-10-19 01:33:20 +0200

    Use cmake for testing

--- .docker/testing/build-test.sh
+++ .docker/testing/build-test.sh
@@ -4,9 +4,8 @@ set -e
 
 mkdir /usr/src/build
 cd /usr/src/build
-cp /usr/src/config.pri.default /usr/src/config.pri
-qmake CONFIG+=debug ..
+cmake /usr/src
 make -j2
 echo "travis_fold:end:build"
 cd test
-LD_LIBRARY_PATH=/usr/src/build/src/core:/usr/src/build/src/qgsquick xvfb-run ./test
+xvfb-run ./test
