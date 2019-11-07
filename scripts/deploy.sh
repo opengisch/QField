beb09946f8da7d6d5d5db3f8930b51dd39f6dc57
Matthias Kuhn <matthias@opengis.ch>
2019-11-07 12:38:19 +0100

    Fix v8a name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ if [[ ${TRAVIS_SECURE_ENV_VARS} = true ]]; then
     openssl aes-256-cbc -K $encrypted_play_upload_key -iv $encrypted_play_upload_iv -in .ci/play_developer.p12.enc -out .ci/play_developer.p12 -d
     echo -e "\e[93m * Collecting apks to upload...\e[0m"
     curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-armv7.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-armv7.apk
-    curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-arm64_v8a.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-armv64-v8a.apk
+    curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-arm64_v8a.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-armv64_v8a.apk
     curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-x86.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-x86.apk
     curl -L -s -S -o /tmp/qfield-${TRAVIS_TAG}-x86_64.apk https://download.opengis.ch/qfield/ci-builds/qfield-dev-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-x86_64.apk
 
