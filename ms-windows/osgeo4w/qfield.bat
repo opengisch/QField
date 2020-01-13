47a6e0019bfa54c35cb349a334d900da1e407843
Matthias Kuhn <matthias@opengis.ch>
2020-01-13 15:34:51 +0100

    Debug++

--- ms-windows/osgeo4w/qfield.bat
+++ ms-windows/osgeo4w/qfield.bat
@@ -2,4 +2,5 @@ SET GDAL_DATA="%0\..\gdal" rem "C:\OSGeo4W\apps\gdal-dev\share\gdal"
 SET PROJ_LIB="%0\..\proj" rem "C:\OSGeo4W\apps\proj-dev\share\proj"
 SET QGIS_PREFIX_PATH="%0\..\qgis"  rem "C:\OSGeo4W\apps\qgis-dev" +/-
 
+ls %0\..\qfield.exe
 CALL %0\..\qfield.exe
