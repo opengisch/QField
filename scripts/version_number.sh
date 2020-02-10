ca03b1a3449390a1f2fea1e4f1d357bf82f90561
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-02-10 15:14:17 +0100

    Fix version code string (#814)
    * better documentation of deploy scripts
    * use extra quotes for the define
    
    see https://stackoverflow.com/a/48784987/1548052

--- scripts/version_number.sh
+++ scripts/version_number.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# GNU prefix command for mac os support (gsed, gsplit)
+GP=
+if [[ "$OSTYPE" =~ darwin* ]]; then
+  GP=g
+fi
+
 # give the ARCH name and it returns an integer as build number increment
 # arg1: ARCH
 arch_to_build_number() {
@@ -28,7 +34,7 @@ app_version_code() {
   VERSION_MAJOR=$(echo "${APP_VERSION}" | cut -f 2 -dv | cut -f1 -d.)
   VERSION_MINOR=$(echo "${APP_VERSION}" | cut -f 2 -d.)
   VERSION_FIX=$(echo "${APP_VERSION}" | cut -f 3 -d. | cut -f1 -d-)
-  VERSION_NUMBER=$(echo "${APP_VERSION}" | sed -r -e 's/^.*-rc([0-9]+)/\1/;t;d') # v.1.2.3-rc4 => 4, v1.2.3 => NULL
+  VERSION_NUMBER=$(echo "${APP_VERSION}" | ${GP}sed -r -e 's/^.*-rc([0-9]+)/\1/;t;d') # v.1.2.3-rc4 => 4, v1.2.3 => NULL
 
   APP_VERSION_CODE=$(printf "%02d%02d%02d%02d%01d" ${VERSION_MAJOR} ${VERSION_MINOR} ${VERSION_FIX} ${VERSION_NUMBER:-99} ${ARCH_BUILD_NUMBER})
   echo ${APP_VERSION_CODE}
@@ -39,5 +45,5 @@ app_version_code() {
 # returns: 1.2.3 or 1.2.3 RC4
 app_version_str() {
   APP_VERSION=$1
-  echo ${APP_VERSION} | sed -r 's/^v//; s/-(rc[0-9]+)/ \U\1/'
+  echo ${APP_VERSION} | ${GP}sed -r 's/^v//; s/-(rc[0-9]+)/ \U\1/'
 }
