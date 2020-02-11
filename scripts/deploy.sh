a699f81ff8632b161ebab2294d0e284c4161f1bc
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-02-11 13:51:29 +0100

    adapt deploy scripts to stable/master branches (#819)
    * adapt deploy scripts to stable/master branches
    
    * fix version str for PRs
    
    * be more explicit about version info
    
    * give increasing version codes to PR builds
    
    * Revert "give increasing version codes to PR builds"
    
    This reverts commit efbf5d4640548792f08f83a2d54c322015c24d98.
    
    * more comments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,8 +24,8 @@ if [[ ${TRAVIS_SECURE_ENV_VARS} = true ]]; then
     echo -e "\e[31mDeploying app to pull request\e[0m"
     echo "${BODY}" | curl -u m-kuhn:${GITHUB_API_TOKEN} -X POST --data @- https://api.github.com/repos/opengisch/QField/issues/${TRAVIS_PULL_REQUEST}/comments
 
-  elif [[ -n ${TRAVIS_TAG} ]]; then
-    # we are on a standard commit on master branch => push to ALPHA
+  elif [[ -n ${TRAVIS_TAG} ]] && [[ ${TRAVIS_BRANCH} = stable ]]; then
+    # we are on a tag and on stable branch
     echo -e "\e[93;1mStarting to deploy a new release\e[0m"
     openssl aes-256-cbc -K $encrypted_play_upload_key -iv $encrypted_play_upload_iv -in .ci/play_developer.p12.enc -out .ci/play_developer.p12 -d
 
@@ -33,13 +33,10 @@ if [[ ${TRAVIS_SECURE_ENV_VARS} = true ]]; then
     for ARCH in "${ARCHS[@]}"
     do
       ASSET_PATH=/tmp/qfield-${TRAVIS_TAG}-${ARCH}.apk
-
       echo -e "\e[93m * Collecting apks to upload...\e[0m"
       fetch_asset ${ARCH} ${ASSET_PATH}
-
       echo -e "\e[93m * Deploying app to github release...\e[0m"
       ./scripts/upload_release_asset.py ${ASSET_PATH} ${TRAVIS_TAG}
-
       ASSETS="${ASSETS} ${ASSET_PATH}"
     done
 
@@ -47,22 +44,25 @@ if [[ ${TRAVIS_SECURE_ENV_VARS} = true ]]; then
     echo -e "\e[93m * Deploying app to google play (release version)...\e[0m"
     ./scripts/basic_upload_apks_service_account.py ch.opengis.qfield internal ${RELEASE_URL} ${ASSETS}
 
-  elif [[ ${TRAVIS_BRANCH} = master ]]; then
-    # we on a tag and master => push to BETA
+  elif [[ ${TRAVIS_BRANCH} = master ]] || [[ ${TRAVIS_BRANCH} = stable ]]; then
+    # we are on a standard commit on master or stable branch
+    # write comment
     echo "${BODY}" | curl -u m-kuhn:${GITHUB_API_TOKEN} -X POST --data $@- https://api.github.com/repos/opengisch/QField/commits/${TRAVIS_COMMIT}/comments
-    openssl aes-256-cbc -K $encrypted_play_upload_key -iv $encrypted_play_upload_iv -in .ci/play_developer.p12.enc -out .ci/play_developer.p12 -d
 
-    ASSETS=""
-    for ARCH in "${ARCHS[@]}"
-    do
-      echo -e "\e[93m * Collecting apks to upload...\e[0m"
-      ASSET_PATH=/tmp/qfield-dev-${ARCH}.apk
-      fetch_asset ${ARCH} ${ASSET_PATH}
-      ASSETS="${ASSETS} ${ASSET_PATH}"
-    done
-
-    echo -e "\e[93m * Deploying app to google play (release version)...\e[0m"
-    RELEASE_URL="https://github.com/opengisch/QField/commit/${TRAVIS_COMMIT}"
-    ./scripts/basic_upload_apks_service_account.py ch.opengis.qfield_dev beta ${RELEASE_URL} ${ASSETS}
+    # only master builds are pushed to play store
+    if [[ ${TRAVIS_BRANCH} = master ]]; then
+      ASSETS=""
+      for ARCH in "${ARCHS[@]}"
+      do
+        echo -e "\e[93m * Collecting apks to upload...\e[0m"
+        ASSET_PATH=/tmp/qfield-dev-${ARCH}.apk
+        fetch_asset ${ARCH} ${ASSET_PATH}
+        ASSETS="${ASSETS} ${ASSET_PATH}"
+      done
+      openssl aes-256-cbc -K $encrypted_play_upload_key -iv $encrypted_play_upload_iv -in .ci/play_developer.p12.enc -out .ci/play_developer.p12 -d
+      echo -e "\e[93m * Deploying app to google play (release version)...\e[0m"
+      RELEASE_URL="https://github.com/opengisch/QField/commit/${TRAVIS_COMMIT}"
+      ./scripts/basic_upload_apks_service_account.py ch.opengis.qfield_dev beta ${RELEASE_URL} ${ASSETS}
+    fi
   fi
 fi
