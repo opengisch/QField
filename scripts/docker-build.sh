d44afc696e5f7ed46ac92fedcd8144f76803076d
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-02-19 10:44:22 +0100

    also change the icon in unpacking

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -41,6 +41,7 @@ if [[ -z ${APP_ICON} ]]; then
   # replace icon
   echo "Replacing icon with ${APP_ICON}"
   sed -i "s|<file alias=\"qfield_logo.svg\">icons/qfield_logo.svg</file>|<file alias=\"qfield_logo.svg\">icons/${APP_ICON}.svg</file>|" ${SOURCE_DIR}/images/images.qrc
+  sed -i "s|@drawable/qfield_logo|@drawable/${APP_ICON}|g" ${SOURCE_DIR}/android/res/layout/unpacking_dialog.xml
   sed -i "s|@drawable/qfield_logo|@drawable/${APP_ICON}|g" ${SOURCE_DIR}/android/AndroidManifest.xml
 fi
 if [[ "X${PKG_NAME}" != "Xqfield" ]]; then
