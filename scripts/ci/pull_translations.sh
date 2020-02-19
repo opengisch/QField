8e7b54a1e5cabaedef5beb8cfe6a881b7718278f
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-02-19 06:34:40 +0100

    fix pull translations

--- scripts/ci/pull_translations.sh
+++ scripts/ci/pull_translations.sh
@@ -2,11 +2,14 @@
 
 echo "travis_fold:start:tx-pull"
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../version_number.sh
 
 if [[ ${TRAVIS_BRANCH} = master ]]; then
   tx pull --all --force
 else
-  tx pull --all --force --branch
+  travis_to_release_branch
+  tx pull --all --force --branch ${RELEASE_BRANCH//_/-}
 fi
 
 for x in android/res/values-*_*;do mv $x $(echo $x | sed -e 's/_/-r/') ;done
