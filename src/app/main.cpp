c136afd07778357b1cc5f1d0fa507b71a8585f59
nirvn <nirvn.asia@gmail.com>
2020-04-30 15:49:13 +0700

    Forward Qt info/debug console (as well as qml console.log) messages to android's logcat

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -31,6 +31,48 @@
 #include <qmainwindow.h>
 #include <QApplication>
 
+#ifdef ANDROID
+#include <android/log.h>
+const char *const applicationName = "QField";
+void qfMessageHandler( QtMsgType type, const QMessageLogContext &context, const QString &msg )
+{
+  QString report = msg;
+  if ( context.file && !QString( context.file ).isEmpty() )
+  {
+    report += " in file ";
+    report += QString( context.file );
+    report += " line ";
+    report += QString::number( context.line );
+  }
+
+  if ( context.function && !QString( context.function ).isEmpty() )
+  {
+    report += +" function ";
+    report += QString( context.function );
+  }
+  const char *const local = report.toLocal8Bit().constData();
+  switch ( type )
+  {
+    case QtDebugMsg:
+      __android_log_write( ANDROID_LOG_DEBUG, applicationName, local );
+      break;
+    case QtInfoMsg:
+      __android_log_write( ANDROID_LOG_INFO, applicationName, local );
+      break;
+    case QtWarningMsg:
+      __android_log_write( ANDROID_LOG_WARN, applicationName, local );
+      break;
+    case QtCriticalMsg:
+      __android_log_write( ANDROID_LOG_ERROR, applicationName, local );
+      break;
+    case QtFatalMsg:
+    default:
+      __android_log_write( ANDROID_LOG_FATAL, applicationName, local );
+      abort();
+  }
+}
+#endif
+
 int main( int argc, char **argv )
 {
 #if 0
@@ -49,6 +91,8 @@ int main( int argc, char **argv )
 
 #ifdef ANDROID
   QgsApplication app( argc, argv, true, AndroidPlatformUtilities().packagePath() + QStringLiteral( "/resources" ) );
+  qInstallMessageHandler( qfMessageHandler );
+
   QSettings settings;
 
   app.setThemeName( settings.value( "/Themes", "default" ).toString() );
