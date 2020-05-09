12f3bbc54be69792c61b83741a55d38b1c16ff29
Matthias Kuhn <matthias@opengis.ch>
2020-05-09 16:17:33 +0200

    Fix license headers

--- src/core/qgssggeometry.cpp
+++ src/core/qgssggeometry.cpp
@@ -1,3 +1,17 @@
+/***************************************************************************
+    qgssggeometry.cpp
+    ---------------------
+    begin                : March 2020
+    copyright            : (C) 2020 by David Signer
+    email                : david at opengis dot ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "math.h"
 
 #include "qgssggeometry.h"
