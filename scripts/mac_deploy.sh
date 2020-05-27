43b5ba9970f0f7366eaeb74d1c1ee642cc9a44a9
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-05-27 21:40:26 +0200

    adapt mac-deploy script for cmake builds

--- scripts/mac_deploy.sh
+++ scripts/mac_deploy.sh
@@ -8,9 +8,15 @@
 QGIS_INSTALL_PATH=$1
 
 qgis_libs=( qgis_core qgis_analysis )
-for qgis_lib in "${qgis_libs[@]}"; do
-  install_name_tool -change @executable_path/../Frameworks/${qgis_lib}.framework/${qgis_lib} ${QGIS_INSTALL_PATH}/QGIS.app/Contents/Frameworks/${qgis_lib}.framework/${qgis_lib} src/qgsquick/libqgsquick.dylib
-  install_name_tool -change @executable_path/../Frameworks/${qgis_lib}.framework/${qgis_lib} ${QGIS_INSTALL_PATH}/QGIS.app/Contents/Frameworks/${qgis_lib}.framework/${qgis_lib} src/core/libcore.dylib
-  install_name_tool -change @executable_path/../Frameworks/${qgis_lib}.framework/${qgis_lib} ${QGIS_INSTALL_PATH}/QGIS.app/Contents/Frameworks/${qgis_lib}.framework/${qgis_lib} src/app/qfield.app/Contents/MacOS/qfield
-  # install_name_tool -change @executable_path/../Frameworks/${qgis_lib}.framework/${qgis_lib} ${QGIS_INSTALL_PATH}/QGIS.app/Contents/Frameworks/${qgis_lib}.framework/${qgis_lib} test/test.app/Contents/MacOS/test
+libs=( src/qgsquick/libqgsquick.dylib src/core/libcore.dylib src/app/qfield.app/Contents/MacOS/qfield output/bin/vertexmodeltest output/bin/qfield output/lib/libqfield_core.dylib output/lib/libqfield_qgsquick.dylib )
+for lib in "${libs[@]}"; do
+  if [[ ! -f  ${lib} ]]; then
+    echo "skipping missing ${lib}"
+    continue
+  else
+    echo "running install_name_tool on ${lib}"
+  fi
+  for qgis_lib in "${qgis_libs[@]}"; do
+    install_name_tool -change @executable_path/../Frameworks/${qgis_lib}.framework/${qgis_lib} ${QGIS_INSTALL_PATH}/QGIS.app/Contents/Frameworks/${qgis_lib}.framework/${qgis_lib} ${lib}
+  done
 done
