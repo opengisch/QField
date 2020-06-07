4d51e11f4e8e9061c5e318f926e2a5c0f3e23b2a
Matthias Kuhn <matthias@opengis.ch>
2020-06-08 00:12:20 +0200

    Fix create asset

--- cmake/CreateZip.cmake
+++ cmake/CreateZip.cmake
@@ -1,6 +1,6 @@
 function(create_zip output_file input_files working_dir)
     add_custom_command(
-        COMMAND ${CMAKE_COMMAND} -E tar "cf" "${output_file}" --format=zip -- ${input_files}
+        COMMAND ${CMAKE_COMMAND} -E tar "cf" "${output_file}" -- ${input_files}
         WORKING_DIRECTORY "${working_dir}"
         OUTPUT  "${output_file}"
         DEPENDS ${input_files}
