dba0012d5eca3ca8e80ce55a306e0338b798ae4a
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-07-21 16:07:04 +0300

    Test

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -4,9 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../version_number.sh
 
 lupdate QField.pro
+tx status
 if [[ ${CI_BRANCH} = master ]]; then
-  tx push --source
+  # tx push --source
 else
   ci_to_release_branch
-  tx push --source --branch ${RELEASE_BRANCH//_/-}
+  # tx push --source --branch ${RELEASE_BRANCH//_/-}
 fi
