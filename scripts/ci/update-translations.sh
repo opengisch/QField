0149306f1375bcf3f8a47bda5c71072dc011c439
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-07-21 16:19:40 +0300

    Bash is live, bash is love

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -4,10 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../version_number.sh
 
 lupdate QField.pro
-tx status
+
 if [[ ${CI_BRANCH} = master ]]; then
+  tx status
   # tx push --source
 else
+  tx status
   ci_to_release_branch
   # tx push --source --branch ${RELEASE_BRANCH//_/-}
 fi
