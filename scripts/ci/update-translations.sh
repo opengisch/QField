0ffdffb9c06b1624ec62468eba898ecee27c67e5
signedav <david@opengis.ch>
2020-09-16 11:15:41 +0200

    fix dir variable and use clearer one

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-source ${DIR}/../version_number.sh
+SOURCE_DIR=${DIR}/../..
+source ${SOURCE_DIR}/scripts/version_number.sh
 
-lupdate -recursive ${CI_BRANCH} -ts ${CI_BRANCH}/i18n/qfield_en.ts
+lupdate -recursive ${SOURCE_DIR} -ts ${SOURCE_DIR}/i18n/qfield_en.ts
 
 # release only if the branch is master
 if [[ ${CI_BRANCH} = master ]]; then
