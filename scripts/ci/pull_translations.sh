545d77a94ece5f968fe6a209bedacdefc38c63d6
Matthias Kuhn <matthias@opengis.ch>
2020-11-06 16:09:08 +0100

    Merge pull request #1388 from opengisch/fix_pull_translations
    Set TX_TOKEN in github workflow and assume it's set in pull_translations

--- scripts/ci/pull_translations.sh
+++ scripts/ci/pull_translations.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+set -e
+
 echo "::group::tx-pull"
 
-if [[ ${CI_SECURE_ENV_VARS} = true ]]; then
-  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-  source ${DIR}/../version_number.sh
-  
-  tx pull --all --force
-  
-  for x in android/res/values-*_*;do mv $x $(echo $x | sed -e 's/_/-r/') ;done
-  
-  find ${DIR}/i18n -type f -name "*.ts" -exec lrelease "{}" \;
-fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${DIR}/../version_number.sh
+
+tx pull --all --force
+
+for x in android/res/values-*_*;do mv $x $(echo $x | sed -e 's/_/-r/') ;done
+
+find ${DIR}/../../i18n -type f -name "*.ts" -exec lrelease "{}" \;
 
 echo "::endgroup::"
