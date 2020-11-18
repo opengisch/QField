dcc7d985d3349777a9ba7097f923d740c28bb7db
Matthias Kuhn <matthias@opengis.ch>
2020-11-18 07:39:58 +0100

    set-env is disabled
    https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-commands-for-github-actions#environment-files

--- scripts/ci/env_gh.sh
+++ scripts/ci/env_gh.sh
@@ -28,18 +28,21 @@ export CI_COMMIT_RANGE=${CI_COMMIT_RANGE:="$TMP_CI_COMMIT_BEFORE...$TMP_CI_COMMI
 export CI_REPO_SLUG=${CI_REPO_SLUG:=$GITHUB_REPOSITORY}
 export CI_UPLOAD_ARTIFACT_ID=${CI_UPLOAD_ARTIFACT_ID:=$( [[ ${CI_PULL_REQUEST} =~ false ]] && echo ${CI_TAG} || echo ${CI_PULL_REQUEST} )}
 
-# make sure ::set-env is on newlines 
+{
+    echo "CI_BUILD_DIR=$CI_BUILD_DIR"
+    echo "CI_COMMIT=$CI_COMMIT"
+    echo "CI_BRANCH=$CI_BRANCH"
+    echo "CI_TAG=$CI_TAG"
+    echo "CI_SECURE_ENV_VARS=$CI_SECURE_ENV_VARS"
+    echo "CI_PULL_REQUEST=$CI_PULL_REQUEST"
+    echo "CI_PULL_REQUEST_BRANCH=$CI_PULL_REQUEST_BRANCH"
+    echo "CI_COMMIT_RANGE=$CI_COMMIT_RANGE"
+    echo "CI_REPO_SLUG=$CI_REPO_SLUG"
+    echo "CI_UPLOAD_ARTIFACT_ID=$CI_UPLOAD_ARTIFACT_ID"
+} >> $GITHUB_ENV
+
+# make sure ::set-output is on newlines 
 echo ""
-echo "::set-env name=CI_BUILD_DIR::$CI_BUILD_DIR"
-echo "::set-env name=CI_COMMIT::$CI_COMMIT"
-echo "::set-env name=CI_BRANCH::$CI_BRANCH"
-echo "::set-env name=CI_TAG::$CI_TAG"
-echo "::set-env name=CI_SECURE_ENV_VARS::$CI_SECURE_ENV_VARS"
-echo "::set-env name=CI_PULL_REQUEST::$CI_PULL_REQUEST"
-echo "::set-env name=CI_PULL_REQUEST_BRANCH::$CI_PULL_REQUEST_BRANCH"
-echo "::set-env name=CI_COMMIT_RANGE::$CI_COMMIT_RANGE"
-echo "::set-env name=CI_REPO_SLUG::$CI_REPO_SLUG"
-echo "::set-env name=CI_UPLOAD_ARTIFACT_ID::$CI_UPLOAD_ARTIFACT_ID"
 echo "::set-output name=CI_BUILD_DIR::$CI_BUILD_DIR"
 echo "::set-output name=CI_COMMIT::$CI_COMMIT"
 echo "::set-output name=CI_BRANCH::$CI_BRANCH"
