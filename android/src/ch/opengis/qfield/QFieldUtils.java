6cbe63b1c337c21c9d33990f2d21c24fb6ebbc45
Mathieu Pellerin <nirvn.asia@gmail.com>
2020-12-06 17:24:35 +0700

    Add header license

--- android/src/ch/opengis/qfield/QFieldUtils.java
+++ android/src/ch/opengis/qfield/QFieldUtils.java
@@ -1,3 +1,20 @@
+/***************************************************************************
+                            QFieldUtils.java
+                            -------------------
+              begin                : December 6, 2020
+              copyright            : (C) 2020 by Mathieu Pellerin
+              email                : nirvn dot asia at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 package ch.opengis.qfield;
 
 import android.content.Context;
