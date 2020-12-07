1588e352dcfcfd841c83c48da10e89f35e7663d9
signedav <david@opengis.ch>
2020-12-07 09:55:59 +0100

    license headers

--- src/core/qgsgnsspositioninformation.cpp
+++ src/core/qgsgnsspositioninformation.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+  qgsgnsspositioninformation.cpp - QgsGnssPositionInformation
+ ---------------------
+ begin                : 1.12.2020
+ copyright            : (C) 2020 by David Signer
+ email                : david (at) opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgsgnsspositioninformation.h"
 
 #include <QCoreApplication>
