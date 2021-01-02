d507a2aa9b4dcf7f3ff4ad04051b7eee1557f131
Matthias Kuhn <matthias@opengis.ch>
2021-01-02 10:52:01 +0100

    Patch permissions on sdk folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ export APP_VERSION_STR=${APP_VERSION_STR:-dev}
 
 ARCH=${ARCH:-arm64_v8a}
 
-docker run \
+docker run --rm \
   -v "$SRC_DIR":/usr/src/qfield \
   $(if [ -n "$CACHE_DIR" ]; then echo "-v $CACHE_DIR:/io/data"; fi) \
   -e "BUILD_FOLDER=build-${ARCH}" \
