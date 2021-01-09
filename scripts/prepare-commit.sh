c88586c57bbfdb34acbad6282a9711a6c7667394
Matthias Kuhn <matthias@opengis.ch>
2021-01-09 10:51:54 +0100

    No sip in qfield

--- scripts/prepare-commit.sh
+++ scripts/prepare-commit.sh
@@ -75,7 +75,7 @@ for f in $MODIFIED; do
   (( i++ )) || true
 
   case "$f" in
-  *.cpp|*.c|*.h|*.cxx|*.hxx|*.c++|*.h++|*.cc|*.hh|*.C|*.H|*.sip|*.py|*.mm)
+  *.cpp|*.c|*.h|*.cxx|*.hxx|*.c++|*.h++|*.cc|*.hh|*.C|*.H|*.py|*.mm)
     ;;
 
   *)
@@ -104,47 +104,6 @@ if [ -s "$ASTYLEDIFF" ]; then
   FILES_CHANGED=1
 else
   rm "$ASTYLEDIFF"
-fi
-
-
-# verify SIP files
-SIPIFYDIFF=sipify.$REV.diff
-true > "$SIPIFYDIFF"
-for f in $MODIFIED; do
-  # if cpp header
-  if [[ $f =~ ^src\/(core|gui|analysis|server|3d)\/.*\.h$ ]]; then
-    # look if corresponding SIP file
-    sip_file=$(${GP}sed -r 's@^src/(core|gui|analysis|server|3d)/@@; s@\.h$@.sip@' <<<"$f" )
-    pyfile=$(${GP}sed -E 's@([^\/]+\/)*([^\/]+)\.sip@\2.py@;' <<< "$sip_file")
-    module=$(${GP}sed -r 's@src/(core|gui|analysis|server|3d)/.*$@\1@' <<<"$f" )
-    if grep -Fq "$sip_file" "${TOPLEVEL}"/python/"${module}"/"${module}"_auto.sip; then
-      sip_file=$(${GP}sed -r 's@^src/(core|gui|analysis|server|3d)@\1/auto_generated@; s@\.h$@.sip.in@' <<<"$f" )
-      m=python/$sip_file.$REV.prepare
-      if [ ! -f python/"$sip_file" ]; then
-        touch python/"$sip_file"
-      fi
-      cp python/"$sip_file" "$m"
-      "${TOPLEVEL}"/scripts/sipify.pl -s $m -p python/"${module}"/auto_additions/"${pyfile}" "$f"
-      # only replace sip files if they have changed
-      if ! diff -u python/"$sip_file" "$m" >>"$SIPIFYDIFF"; then
-        echo "python/$sip_file is not up to date"
-        cp "$m" python/"$sip_file"
-      fi
-      rm "$m"
-    fi
-  fi
-done
-if [[ -s "$SIPIFYDIFF" ]]; then
-  if tty -s; then
-    # review astyle changes
-    colordiff <"$SIPIFYDIFF" | less -r
-    rm "$SIPIFYDIFF"
-  else
-    echo "Files changed (see $ASTYLEDIFF)"
-  fi
-  FILES_CHANGED=1
-else
-  rm "$SIPIFYDIFF"
 fi
 
 exit $FILES_CHANGED
