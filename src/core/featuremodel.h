602c933880cfdd070620812dfe8d658b8100434d
nirvn <nirvn.asia@gmail.com>
2021-04-13 16:24:21 +0700

    Fix and improve remembered values

--- src/core/featuremodel.h
+++ src/core/featuremodel.h
@@ -18,15 +18,17 @@
 #ifndef FEATUREMODEL_H
 #define FEATUREMODEL_H
 
+#include "snappingresult.h"
+#include "gnsspositioninformation.h"
+#include "geometry.h"
+
+#include <qgsfeature.h>
+
+#include <QMutex>
 #include <QAbstractListModel>
 #include <QtPositioning/QGeoPositionInfoSource>
 #include <qgsrelationmanager.h>
 #include <memory>
-#include <qgsfeature.h>
-#include "snappingresult.h"
-#include "gnsspositioninformation.h"
-
-#include "geometry.h"
 
 class VertexModel;
 
@@ -46,6 +48,8 @@ class FeatureModel : public QAbstractListModel
     Q_PROPERTY( SnappingResult topSnappingResult READ topSnappingResult WRITE setTopSnappingResult NOTIFY topSnappingResultChanged )
     Q_PROPERTY( bool positionLocked READ positionLocked WRITE setPositionLocked NOTIFY positionLockedChanged )
 
+  public:
+
     //! keeping the information what attributes are remembered and the last edited feature
     struct RememberValues
     {
@@ -53,7 +57,6 @@ class FeatureModel : public QAbstractListModel
       QVector<bool> rememberedAttributes;
     };
 
-  public:
     enum ModelModes
     {
       SingleFeatureModel = 1,
@@ -259,8 +262,9 @@ class FeatureModel : public QAbstractListModel
     GnssPositionInformation mPositionInformation;
     SnappingResult mTopSnappingResult;
     QString mTempName;
-    QMap<QgsVectorLayer *, RememberValues> mRememberings;
     bool mPositionLocked = false;
+
+    static QMutex sMutex;
 };
 
 #endif // FEATUREMODEL_H
