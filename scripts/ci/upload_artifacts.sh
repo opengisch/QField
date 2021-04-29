7d038051e7a889ab9515e496ca336088d2ee3330
Matthias Kuhn <matthias@opengis.ch>
2021-04-29 11:10:54 +0200

    Remove acl command

--- scripts/ci/upload_artifacts.sh
+++ scripts/ci/upload_artifacts.sh
@@ -18,8 +18,8 @@ then
   echo "${S3CFG}" > ~/.s3cfg
   s3cmd put --acl-public /tmp/${FILENAME} s3://qfieldapks/ci-builds/${FILENAME}
   echo -e "\e[31Uploaded to https://sos-ch-dk-2.exo.io/qfieldapks/ci-builds/${FILENAME} \e[0m"
-  s3cmd modify --add-header=content-type:application/vnd.android.package-archive s3://qfieldapks/ci-builds/${FILENAME}
-  echo -e "\e[31ACL modified \e[0m"
+#  s3cmd modify --add-header=content-type:application/vnd.android.package-archive s3://qfieldapks/ci-builds/${FILENAME}
+#  echo -e "\e[31ACL modified \e[0m"
 else
   echo -e "Not uploading artifacts ..."
 fi
