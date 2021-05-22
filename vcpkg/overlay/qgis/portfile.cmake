49d4c12feb527543f3a78aed258a2a6b26dd714c
Matthias Kuhn <matthias@opengis.ch>
2021-05-22 13:07:22 +0200

    Use vcpkg overlay instead of custom repository (#1915)
    * vcpkg overlay
    
    * Remove triplet spec
    
    * disable patches
    
    * Bump libspatialindex to 1.9.3
    
    * vcpkg cleanup
    
    * Update portfile.cmake
    
    * spatialindex 1.9.3

---
+++ vcpkg/overlay/qgis/portfile.cmake
@@ -0,0 +1,341 @@
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+
+set(QGIS_REF 5c212369aef5174fee3723c0c2dcea99f8270f7a)
+set(QGIS_SHA512 d2400458fc9ee2d00dac5c1bb91abb100e243eb6d4c892a36aabcc25cc73b71707a2931d889c4d0687f18e9164e5e2db6c3f8382f7c4f85fb123e0cb2a1b84a8)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO qgis/QGIS
+    REF ${QGIS_REF}
+    SHA512   ${QGIS_SHA512}
+    HEAD_REF master
+    PATCHES
+        # Make qgis support python's debug library
+        qgspython.patch
+)
+
+vcpkg_find_acquire_program(FLEX)
+vcpkg_find_acquire_program(BISON)
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON3_PATH ${PYTHON3} DIRECTORY)
+vcpkg_add_to_path(${PYTHON3_PATH})
+vcpkg_add_to_path(${PYTHON3_PATH}/Scripts)
+set(PYTHON_EXECUTABLE ${PYTHON3})
+
+list(APPEND QGIS_OPTIONS -DENABLE_TESTS:BOOL=OFF)
+list(APPEND QGIS_OPTIONS -DWITH_QTWEBKIT:BOOL=OFF)
+list(APPEND QGIS_OPTIONS -DWITH_GRASS7:BOOL=OFF)
+list(APPEND QGIS_OPTIONS -DWITH_QSPATIALITE:BOOL=ON)
+
+if("bindings" IN_LIST FEATURES)
+    list(APPEND QGIS_OPTIONS -DWITH_BINDINGS:BOOL=ON)
+else()
+    list(APPEND QGIS_OPTIONS -DWITH_BINDINGS:BOOL=OFF)
+endif()
+
+if("gui" IN_LIST FEATURES)
+    list(APPEND QGIS_OPTIONS -DWITH_GUI:BOOL=ON)
+else()
+    if("desktop" IN_LIST FEATURES OR "customwidgets" IN_LIST FEATURES)
+        message(FATAL_ERROR "If QGIS is built without gui, desktop and customwidgets cannot be built. Enable gui or disable customwidgets and desktop.")
+    endif()
+    list(APPEND QGIS_OPTIONS -DWITH_GUI:BOOL=OFF)
+endif()
+
+if("desktop" IN_LIST FEATURES)
+    list(APPEND QGIS_OPTIONS -DWITH_DESKTOP:BOOL=ON)
+else()
+    list(APPEND QGIS_OPTIONS -DWITH_DESKTOP:BOOL=OFF)
+endif()
+
+if("customwidgets" IN_LIST FEATURES)
+    list(APPEND QGIS_OPTIONS -DWITH_CUSTOM_WIDGETS:BOOL=ON)
+else()
+    list(APPEND QGIS_OPTIONS -DWITH_CUSTOM_WIDGETS:BOOL=OFF)
+endif()
+
+if("server" IN_LIST FEATURES)
+    list(APPEND QGIS_OPTIONS -DWITH_SERVER:BOOL=ON)
+    if("bindings" IN_LIST FEATURES)
+        list(APPEND QGIS_OPTIONS -DWITH_SERVER_PLUGINS:BOOL=ON)
+    else()
+        list(APPEND QGIS_OPTIONS -DWITH_SERVER_PLUGINS:BOOL=OFF)
+    endif()
+else()
+    list(APPEND QGIS_OPTIONS -DWITH_SERVER:BOOL=OFF)
+    list(APPEND QGIS_OPTIONS -DWITH_SERVER_PLUGINS:BOOL=OFF)
+endif()
+
+if("process" IN_LIST FEATURES)
+    list(APPEND QGIS_OPTIONS -DWITH_QGIS_PROCESS:BOOL=ON)
+else()
+    list(APPEND QGIS_OPTIONS -DWITH_QGIS_PROCESS:BOOL=OFF)
+endif()
+
+if("3d" IN_LIST FEATURES)
+    list(APPEND QGIS_OPTIONS -DWITH_3D:BOOL=ON)
+else()
+    list(APPEND QGIS_OPTIONS -DWITH_3D:BOOL=OFF)
+endif()
+
+# Configure debug and release library paths
+macro(FIND_LIB_OPTIONS basename relname debname suffix libsuffix)
+   file(TO_NATIVE_PATH "${CURRENT_INSTALLED_DIR}/lib/${VCPKG_TARGET_IMPORT_LIBRARY_PREFIX}${relname}${libsuffix}" ${basename}_LIBRARY_RELEASE)
+   file(TO_NATIVE_PATH "${CURRENT_INSTALLED_DIR}/debug/lib/${VCPKG_TARGET_IMPORT_LIBRARY_PREFIX}${debname}${libsuffix}" ${basename}_LIBRARY_DEBUG)
+   if( ${basename}_LIBRARY_DEBUG AND ${basename}_LIBRARY_RELEASE AND NOT ${basename}_LIBRARY_DEBUG STREQUAL ${basename}_LIBRARY_RELEASE )
+        list(APPEND QGIS_OPTIONS_RELEASE -D${basename}_${suffix}:FILEPATH=${${basename}_LIBRARY_RELEASE})
+        list(APPEND QGIS_OPTIONS_DEBUG -D${basename}_${suffix}:FILEPATH=${${basename}_LIBRARY_DEBUG})
+   elseif( ${basename}_LIBRARY_RELEASE )
+        list(APPEND QGIS_OPTIONS -D${basename}_${suffix}:FILEPATH=${${basename}_LIBRARY_RELEASE})
+   elseif( ${basename}_LIBRARY_DEBUG )
+        list(APPEND QGIS_OPTIONS -D${basename}_${suffix}:FILEPATH=${${basename}_LIBRARY_DEBUG})
+   endif()
+endmacro()
+
+if(VCPKG_TARGET_IS_WINDOWS)
+    ##############################################################################
+    #Install pip
+    if(NOT EXISTS "${PYTHON3_PATH}/Scripts/pip.exe")
+        MESSAGE(STATUS  "Install pip for Python Begin ...")
+        vcpkg_download_distfile(
+            GET_PIP_PATH
+            URLS https://bootstrap.pypa.io/pip/3.4/get-pip.py
+            FILENAME get-pip.py
+            SHA512  2accfa705be5ca38bb2c7851292cf985e9c1eaa8b8a0871bdbf37672c0af796d4f976fa8caf7764d8ddfce46f6396d546e5d446812ef9e2f6411dc30b40764ed
+        )
+
+        vcpkg_execute_required_process(
+            COMMAND "${PYTHON_EXECUTABLE}" "${GET_PIP_PATH}"
+            WORKING_DIRECTORY ${PYTHON3_PATH}
+            LOGNAME pip
+        )
+
+        vcpkg_execute_required_process(
+            COMMAND "${PYTHON_EXECUTABLE}" -m pip install --upgrade pip
+            WORKING_DIRECTORY ${PYTHON3_PATH}
+            LOGNAME pip
+        )
+        MESSAGE(STATUS  "Install pip for Python End")
+    endif (NOT EXISTS "${PYTHON3_PATH}/Scripts/pip.exe")
+    ##############################################################################
+
+    list(APPEND QGIS_OPTIONS -DBISON_EXECUTABLE="${BISON}")
+    list(APPEND QGIS_OPTIONS -DFLEX_EXECUTABLE="${FLEX}")
+
+    list(APPEND QGIS_OPTIONS -DPYUIC_PROGRAM=${PYTHON3_PATH}/Scripts/pyuic5.exe)
+    list(APPEND QGIS_OPTIONS -DPYRCC_PROGRAM=${PYTHON3_PATH}/Scripts/pyrcc5.exe)
+    list(APPEND QGIS_OPTIONS -DQT_LRELEASE_EXECUTABLE=${CURRENT_INSTALLED_DIR}/tools/qt5-tools/bin/lrelease.exe)
+
+    # qgis_gui depends on Qt5UiTools, and Qt5UiTools is a static library.
+    # If Qt5_EXCLUDE_STATIC_DEPENDENCIES is not set, it will add the QT release library that it depends on.
+    # so that in debug mode, it will reference both the qt debug library and the release library.
+    # In Debug mode, add Qt5_EXCLUDE_STATIC_DEPENDENCIES to avoid this bug
+    list(APPEND QGIS_OPTIONS_DEBUG -DQt5_EXCLUDE_STATIC_DEPENDENCIES:BOOL=ON)
+
+    FIND_LIB_OPTIONS(GDAL gdal gdald LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(GEOS geos_c geos_cd LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(GSL gsl gsld LIB ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(GSLCBLAS gslcblas gslcblasd LIB ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(POSTGRES libpq libpq LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(PROJ proj proj_d LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(PYTHON python39 python39_d LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(QCA qca qcad LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(QWT qwt qwtd LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(QTKEYCHAIN qt5keychain qt5keychaind LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(QSCINTILLA qscintilla2_qt5 qscintilla2_qt5d LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+    if("server" IN_LIST FEATURES)
+        FIND_LIB_OPTIONS(FCGI libfcgi libfcgi LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+        list(APPEND QGIS_OPTIONS -DFCGI_INCLUDE_DIR="${CURRENT_INSTALLED_DIR}/include/fastcgi")
+    endif()
+
+    set(SPATIALINDEX_LIB_NAME spatialindex)
+    if( VCPKG_TARGET_ARCHITECTURE STREQUAL "x64" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64" )
+        set( SPATIALINDEX_LIB_NAME "spatialindex-64" )
+    else()
+        set( SPATIALINDEX_LIB_NAME "spatialindex-32" )
+    endif()
+    FIND_LIB_OPTIONS(SPATIALINDEX ${SPATIALINDEX_LIB_NAME} ${SPATIALINDEX_LIB_NAME}d LIBRARY ${VCPKG_TARGET_IMPORT_LIBRARY_SUFFIX})
+elseif(VCPKG_TARGET_IS_LINUX OR VCPKG_TARGET_IS_OSX) # Build in UNIX
+    macro(INSTALL_PROGRAM program)
+        if(VCPKG_TARGET_IS_OSX)
+            message(STATUS "brew install ${program}")
+            vcpkg_execute_required_process(
+              COMMAND brew install ${program}
+              WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+            )
+        else()
+            message(STATUS "sudo apt-get install ${program}")
+            vcpkg_execute_required_process(
+              COMMAND sudo apt-get install -y ${program}
+              WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+            )
+        endif()
+    endmacro()
+
+    find_program(PIP3 pip3)
+    if (NOT PIP3)
+        INSTALL_PROGRAM(python3-pip)
+    endif()
+
+    vcpkg_execute_required_process(
+        COMMAND "${PYTHON_EXECUTABLE}" -m pip install --upgrade pip
+        WORKING_DIRECTORY ${PYTHON3_PATH}
+        LOGNAME pip
+    )
+
+    find_program(PYUIC5 pyuic5)
+    if (NOT PYUIC5)
+        INSTALL_PROGRAM(pyqt5-dev-tools)
+    endif()
+
+    find_program(PYRCC5 pyrcc5)
+
+    list(APPEND QGIS_OPTIONS_DEBUG -DQT_INSTALL_LIBS:PATH=${CURRENT_INSTALLED_DIR}/debug/lib)
+    list(APPEND QGIS_OPTIONS_RELEASE -DQT_INSTALL_LIBS:PATH=${CURRENT_INSTALLED_DIR}/lib)
+    list(APPEND QGIS_OPTIONS -DGDAL_CONFIG=" ")
+    list(APPEND QGIS_OPTIONS -DGDAL_INCLUDE_DIR:PATH=${CURRENT_INSTALLED_DIR}/include)
+    FIND_LIB_OPTIONS(GDAL gdal gdal LIBRARY ${VCPKG_TARGET_SHARED_LIBRARY_SUFFIX})
+    list(APPEND QGIS_OPTIONS -DGEOS_CONFIG=" ")
+    FIND_LIB_OPTIONS(GEOS geos_c geos_cd LIBRARY ${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX})
+    list(APPEND QGIS_OPTIONS -DGSL_CONFIG=" ")
+    list(APPEND QGIS_OPTIONS -DGSL_INCLUDE_DIR:PATH=${CURRENT_INSTALLED_DIR}/include)
+    list(APPEND QGIS_OPTIONS_DEBUG -DGSL_LIBRARIES:FILEPATH=${CURRENT_INSTALLED_DIR}/debug/lib/${VCPKG_TARGET_STATIC_LIBRARY_PREFIX}gsld${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX};${CURRENT_INSTALLED_DIR}/debug/lib/${VCPKG_TARGET_STATIC_LIBRARY_PREFIX}gslcblasd${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX})
+    list(APPEND QGIS_OPTIONS_RELEASE -DGSL_LIBRARIES:FILEPATH="${CURRENT_INSTALLED_DIR}/lib/${VCPKG_TARGET_STATIC_LIBRARY_PREFIX}gsl${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX} ${CURRENT_INSTALLED_DIR}/lib/${VCPKG_TARGET_STATIC_LIBRARY_PREFIX}gslcblas${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX}")
+    list(APPEND QGIS_OPTIONS -DPYTHON_INCLUDE_PATH:PATH=${CURRENT_INSTALLED_DIR}/include/python3.9m)
+    FIND_LIB_OPTIONS(PYTHON python3.9m python3.9dm LIBRARY ${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX})
+    FIND_LIB_OPTIONS(QTKEYCHAIN qt5keychain qt5keychaind LIBRARY  ${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX})
+    if("server" IN_LIST FEATURES)
+        FIND_LIB_OPTIONS(FCGI fcgi fcgi LIBRARY ${VCPKG_TARGET_SHARED_LIBRARY_SUFFIX})
+        list(APPEND QGIS_OPTIONS -DFCGI_INCLUDE_DIR="${CURRENT_INSTALLED_DIR}/include/fastcgi")
+    endif()
+
+    FIND_LIB_OPTIONS(SPATIALINDEX spatialindex spatialindexd LIBRARY ${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX})
+else() # Other build system
+  message(FATAL_ERROR "Unsupport build system.")
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    #PREFER_NINJA
+    OPTIONS ${QGIS_OPTIONS} 
+    OPTIONS_DEBUG ${QGIS_OPTIONS_DEBUG}
+    OPTIONS_RELEASE ${QGIS_OPTIONS_RELEASE}
+)
+
+vcpkg_install_cmake()
+
+# handle qgis tools and plugins
+function(copy_path basepath)
+    file(GLOB ${basepath}_PATH ${CURRENT_PACKAGES_DIR}/${basepath}/*)
+    if( ${basepath}_PATH )
+        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/${PORT}/${basepath})
+        file(COPY ${${basepath}_PATH} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT}/${basepath})
+    endif()
+
+    if(EXISTS "${CURRENT_PACKAGES_DIR}/${basepath}/")
+        file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/${basepath}/)
+    endif()
+
+    if("debug-tools" IN_LIST FEATURES)
+        file(GLOB ${basepath}_DEBUG_PATH ${CURRENT_PACKAGES_DIR}/debug/${basepath}/*)
+        if( ${basepath}_DEBUG_PATH )
+            file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/${basepath})
+            file(COPY ${${basepath}_DEBUG_PATH} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/${basepath})
+        endif()
+    endif()
+
+    if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/${basepath}/")
+        file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/${basepath}/)
+    endif()
+endfunction()
+
+file(GLOB QGIS_CMAKE_PATH ${CURRENT_PACKAGES_DIR}/*.cmake)
+if(QGIS_CMAKE_PATH)
+    file(COPY ${QGIS_CMAKE_PATH} DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmake/${PORT})
+    file(REMOVE_RECURSE ${QGIS_CMAKE_PATH})
+endif()
+file(GLOB QGIS_CMAKE_PATH_DEBUG ${CURRENT_PACKAGES_DIR}/debug/*.cmake)
+if( QGIS_CMAKE_PATH_DEBUG )
+    file(REMOVE_RECURSE ${QGIS_CMAKE_PATH_DEBUG})
+endif()
+
+file(GLOB QGIS_TOOL_PATH ${CURRENT_PACKAGES_DIR}/bin/*${VCPKG_TARGET_EXECUTABLE_SUFFIX} ${CURRENT_PACKAGES_DIR}/*${VCPKG_TARGET_EXECUTABLE_SUFFIX})
+if(QGIS_TOOL_PATH)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin)
+    file(COPY ${QGIS_TOOL_PATH} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin)
+    file(REMOVE_RECURSE ${QGIS_TOOL_PATH})
+    file(GLOB QGIS_TOOL_PATH ${CURRENT_PACKAGES_DIR}/bin/* )
+    file(COPY ${QGIS_TOOL_PATH} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin)
+endif()
+
+file(GLOB QGIS_TOOL_PATH_DEBUG ${CURRENT_PACKAGES_DIR}/debug/bin/*${VCPKG_TARGET_EXECUTABLE_SUFFIX} ${CURRENT_PACKAGES_DIR}/debug/*${VCPKG_TARGET_EXECUTABLE_SUFFIX})
+if(QGIS_TOOL_PATH_DEBUG)
+    if("debug-tools" IN_LIST FEATURES)
+        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/bin)
+        file(COPY ${QGIS_TOOL_PATH_DEBUG} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/bin)
+        file(REMOVE_RECURSE ${QGIS_TOOL_PATH_DEBUG})
+        file(GLOB QGIS_TOOL_PATH_DEBUG ${CURRENT_PACKAGES_DIR}/debug/bin/* )
+        file(COPY ${QGIS_TOOL_PATH_DEBUG} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/bin)
+    else()
+        file(REMOVE_RECURSE ${QGIS_TOOL_PATH_DEBUG})
+    endif()
+endif()
+
+copy_path(doc)
+copy_path(i18n)
+copy_path(icons)
+copy_path(images)
+copy_path(plugins)
+copy_path(python)
+copy_path(resources)
+if("server" IN_LIST FEATURES)
+    copy_path(server)
+endif()
+copy_path(svg)
+
+if(VCPKG_TARGET_IS_WINDOWS)
+    # Extend vcpkg_copy_tool_dependencies to support the export of dll and exe dependencies in different directories to the same directory,
+    # and support the copy of debug dependencies
+    function(vcpkg_copy_tool_dependencies_ex TOOL_DIR OUTPUT_DIR SEARCH_DIR)
+        find_program(PS_EXE powershell PATHS ${DOWNLOADS}/tool)
+        if (PS_EXE-NOTFOUND)
+            message(FATAL_ERROR "Could not find powershell in vcpkg tools, please open an issue to report this.")
+        endif()
+        macro(search_for_dependencies PATH_TO_SEARCH)
+            file(GLOB TOOLS ${TOOL_DIR}/*.exe ${TOOL_DIR}/*.dll)
+            foreach(TOOL ${TOOLS})
+                vcpkg_execute_required_process(
+                    COMMAND ${PS_EXE} -noprofile -executionpolicy Bypass -nologo
+                        -file ${CMAKE_CURRENT_LIST_DIR}/applocal.ps1
+                        -targetBinary ${TOOL}
+                        -installedDir ${PATH_TO_SEARCH}
+                        -outputDir    ${OUTPUT_DIR}
+                    WORKING_DIRECTORY ${VCPKG_ROOT_DIR}
+                    LOGNAME copy-tool-dependencies
+                )
+            endforeach()
+        endmacro()
+        search_for_dependencies(${CURRENT_PACKAGES_DIR}/${SEARCH_DIR})
+        search_for_dependencies(${CURRENT_INSTALLED_DIR}/${SEARCH_DIR})
+    endfunction()
+
+    vcpkg_copy_tool_dependencies_ex(${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin ${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin bin)
+    vcpkg_copy_tool_dependencies_ex(${CURRENT_PACKAGES_DIR}/tools/${PORT}/plugins ${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin bin)
+    if("debug-tools" IN_LIST FEATURES)
+        vcpkg_copy_tool_dependencies_ex(${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/bin ${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/bin debug/bin)
+        vcpkg_copy_tool_dependencies_ex(${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/plugins ${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/bin debug/bin)
+    endif()
+    if("server" IN_LIST FEATURES)
+        vcpkg_copy_tool_dependencies_ex(${CURRENT_PACKAGES_DIR}/tools/${PORT}/server ${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin bin)
+        if("debug-tools" IN_LIST FEATURES)
+            vcpkg_copy_tool_dependencies_ex(${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/server ${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/bin debug/bin)
+        endif()
+    endif()
+endif()
+
+file(REMOVE_RECURSE
+    ${CURRENT_PACKAGES_DIR}/debug/include
+)
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
