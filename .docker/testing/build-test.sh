1ed1250f9cbcbb3e362b64344f93198a3c646437
Matthias Kuhn <matthias@opengis.ch>
2021-05-29 18:57:15 +0200

    spix for linux

--- .docker/testing/build-test.sh
+++ .docker/testing/build-test.sh
@@ -2,11 +2,14 @@
 
 set -e
 
+/usr/src/vcpkg/base/bootstrap-vcpkg.sh
+/usr/src/vcpkg/base/vcpkg install --overlay-ports=/usr/src/vcpkg/overlay spix
+
 mkdir /usr/src/build
 cd /usr/src/build
 
 echo ::group::cmake
-cmake -GNinja /usr/src
+cmake -GNinja -DSpix_DIR:PATH=/usr/src/vcpkg/base/installed/x64-linux/share/Spix/cmake -DAnyRPC_DIR:PATH=/usr/src/vcpkg/base/installed/x64-linux/share/anyrpc -DWITH_SPIX=TRUE /usr/src
 echo ::endgroup::
 
 echo ::group::make
