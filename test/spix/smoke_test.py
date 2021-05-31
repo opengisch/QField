e86f3f648fed6b6d0a7d77ff4cc06393a718d22f
Matthias Kuhn <matthias@opengis.ch>
2021-05-31 07:52:55 +0200

    Modularize test code

--- test/spix/smoke_test.py
+++ test/spix/smoke_test.py
@@ -9,42 +9,69 @@ import time
 import os
 
 
-# TODO: use setup and teardown for this infrastructure work
-def get_test_app():
-    process = subprocess.Popen('./output/bin/qfield', stdout=subprocess.PIPE)
+@pytest.fixture
+def app():
+    process = subprocess.Popen(
+        "./output/bin/qfield", stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    app = xmlrpc.client.ServerProxy("http://localhost:9000")
 
-    s = xmlrpc.client.ServerProxy('http://localhost:9000')
-    
     start = time.time()
     while True:
         try:
-            s.existsAndVisible('mainWindow')
-            return process, s
+            exit_code = process.poll()
+            if exit_code is not None:
+                output, errors = process.communicate()
+                print("== OUTPUT ==")
+                print(output.decode("utf-8"))
+                print("== ERRORS ==")
+                print(errors.decode("utf-8"))
+
+                assert exit_code is None
+            app.existsAndVisible("mainWindow")
+            yield app
+            break
         except (ConnectionRefusedError, OSError) as e:
             if time.time() - start > 30:
                 output, errors = process.communicate()
                 print(output)
                 print(errors)
-                assert False # Could not start app after 30 seconds
+                assert False  # Could not start app after 30 seconds
             print(str(e))
             time.sleep(0.2)
+        except Exception as e:
+            output, errors = process.communicate()
+            assert False  # Unexcpected exception while starting up app
 
-    output, errors = process.communicate()
-    assert False # Test app could not be started
+    app.quit()
+    app.quit()
+    try:
+        process.wait(1)
+    except subprocess.TimeoutExpired:
+        print("Process did not quit after 1 second. Killing it.")
+        process.kill()
 
-def test_load_project():
-    process, app = get_test_app()
-    assert app.existsAndVisible('mainWindow')
 
-    assert app.existsAndVisible('mainWindow/welcomeScreen')
-    first_title = app.getStringProperty('mainWindow/welcomeScreen/loadProjectItem_1', 'title')
-    app.mouseClick('mainWindow/welcomeScreen/loadProjectItem_1')
-#    time.sleep(3)
+def test_load_project(app):
+    assert app.existsAndVisible("mainWindow")
+
+    assert app.existsAndVisible("mainWindow/welcomeScreen")
+
+    first_title = app.getStringProperty(
+        "mainWindow/welcomeScreen/loadProjectItem_1", "title"
+    )
+    app.mouseClick("mainWindow/welcomeScreen/loadProjectItem_1")
+    app.takeScreenshot(
+        "mainWindow", os.path.join(os.environ["QFIELD_SCREENSHOT_PATH"], "startup.png")
+    )
+
+
+# @pytest.mark.parametrize('project', ['some_project.qgz'])
+# def test_load_project(project):
+#   app.mouseClick() ...
+#   time.sleep(3)
 #
-#    assert first_title == 'Test'
-
-    process.terminate()
-    process.terminate()
+#   assert first_title == 'Test'
 
 
 if __name__ == "__main__":
