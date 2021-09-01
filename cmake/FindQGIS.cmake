8b55e93f588f0748b27cb2bc76309f578d456895
Hieu Van <hieu.van@posteo.net>
2021-09-01 07:48:16 +0700

    Improve CMake custom modules

--- cmake/FindQGIS.cmake
+++ cmake/FindQGIS.cmake
@@ -80,219 +80,237 @@ IF(WIN32)
         "$ENV{PROGRAMFILES}/QGIS/lib"
     )
   ENDIF (MSVC)
-ELSE(WIN32)
-  IF(UNIX)
-    #MESSAGE("Searching for QGIS in /usr/bin; /usr/local/bin")
-    FIND_PATH(QGIS_INCLUDE_DIR
-      NAMES qgis.h
-      PATHS
-        ${CMAKE_PREFIX_PATH}
-        ${QGIS_BUILD_PATH}/output/lib/qgis_core.framework/Headers
-        ${QGIS_MAC_PATH}/Frameworks/qgis_core.framework/Headers
-        ${QGIS_PREFIX_PATH}/include/qgis
-        /usr/include/qgis
-        /usr/local/include/qgis
-        /Library/Frameworks/qgis_core.framework/Headers
-        "$ENV{LIB_DIR}/include/qgis"
-      PATH_SUFFIXES QGIS.app/Contents/Frameworks/qgis_core.framework/Headers
-    )
 
-    # also get other frameworks' headers folders on OS X
-    IF (APPLE AND USE_MAC_BUNDLING)
-      FIND_PATH(QGIS_ANALYSIS_INCLUDE_DIR
-        NAMES qgsanalysis.h
-        PATHS
-          ${CMAKE_PREFIX_PATH}
-          ${QGIS_BUILD_PATH}/output/lib
-          ${QGIS_MAC_PATH}/Frameworks
-          /Library/Frameworks
-        PATH_SUFFIXES
-          qgis_analysis.framework/Headers
-          QGIS.app/Contents/Frameworks/qgis_analysis.framework/Headers
-      )
-      SET(QGIS_INCLUDE_DIR
-        ${QGIS_INCLUDE_DIR}
-        ${QGIS_ANALYSIS_INCLUDE_DIR}
-      )
+elseif(ANDROID)
+  find_path(
+    QGIS_INCLUDE_DIR
+    NAMES qgis.h
+    HINTS ${OSGEO4A_STAGE_DIR}/${ANDROID_ABI}
+    PATH_SUFFIXES /include/qgis
+  )
 
-      # qgis_core is an archive without extensions for ios
-      SET(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES} "")
-    ENDIF (APPLE AND USE_MAC_BUNDLING)
+  find_library(
+    QGIS_CORE_LIBRARY
+    qgis_core_${ANDROID_ABI}
+    HINTS ${OSGEO4A_STAGE_DIR}/${ANDROID_ABI}
+    PATH_SUFFIXES /lib
+  )
+  find_library(
+    QGIS_ANALYSIS_LIBRARY
+    qgis_analysis_${ANDROID_ABI}
+    HINTS ${OSGEO4A_STAGE_DIR}/${ANDROID_ABI}
+    PATH_SUFFIXES /lib
+  )
 
+ELSEIF(UNIX)
+  #MESSAGE("Searching for QGIS in /usr/bin; /usr/local/bin")
+  FIND_PATH(QGIS_INCLUDE_DIR
+    NAMES qgis.h
+    PATHS
+      ${CMAKE_PREFIX_PATH}
+      ${QGIS_BUILD_PATH}/output/lib/qgis_core.framework/Headers
+      ${QGIS_MAC_PATH}/Frameworks/qgis_core.framework/Headers
+      ${QGIS_PREFIX_PATH}/include/qgis
+      /usr/include/qgis
+      /usr/local/include/qgis
+      /Library/Frameworks/qgis_core.framework/Headers
+      "$ENV{LIB_DIR}/include/qgis"
+    PATH_SUFFIXES QGIS.app/Contents/Frameworks/qgis_core.framework/Headers
+  )
 
-    FIND_LIBRARY(QGIS_CORE_LIBRARY
-      NAMES qgis_core
-      PATHS
-        ${CMAKE_PREFIX_PATH}
-        ${QGIS_BUILD_PATH}/output/lib
-        ${QGIS_MAC_PATH}/Frameworks
-        ${QGIS_MAC_PATH}/lib
-        ${QGIS_PREFIX_PATH}/lib/
-        /usr/lib64
-        /usr/lib
-        /usr/local/lib
-        /Library/Frameworks
-        "$ENV{LIB_DIR}/lib/"
-      PATH_SUFFIXES
-        QGIS.app/Contents/Frameworks/qgis_core.framework
-    )
-    FIND_LIBRARY(QGIS_ANALYSIS_LIBRARY
-      NAMES qgis_analysis
+  # also get other frameworks' headers folders on OS X
+  IF (APPLE AND USE_MAC_BUNDLING)
+    FIND_PATH(QGIS_ANALYSIS_INCLUDE_DIR
+      NAMES qgsanalysis.h
       PATHS
         ${CMAKE_PREFIX_PATH}
         ${QGIS_BUILD_PATH}/output/lib
         ${QGIS_MAC_PATH}/Frameworks
-        ${QGIS_MAC_PATH}/lib
-        ${QGIS_PREFIX_PATH}/lib/
-        /usr/lib64
-        /usr/lib
-        /usr/local/lib
         /Library/Frameworks
-        "$ENV{LIB_DIR}/lib/"
       PATH_SUFFIXES
-        QGIS.app/Contents/Frameworks/qgis_analysis.framework
+        qgis_analysis.framework/Headers
+        QGIS.app/Contents/Frameworks/qgis_analysis.framework/Headers
+    )
+    SET(QGIS_INCLUDE_DIR
+      ${QGIS_INCLUDE_DIR}
+      ${QGIS_ANALYSIS_INCLUDE_DIR}
     )
 
-    IF (APPLE AND USE_MAC_BUNDLING)
-      FIND_PATH(QGIS_APPLE_RESOURCES_DIR
-        NAMES FindQGIS.cmake
-        PATHS
-          ${CMAKE_PREFIX_PATH}
-          ${QGIS_BUILD_PATH}/output/bin
-          ${QGIS_MAC_PATH}/Frameworks
-          /Library/Frameworks
-        PATH_SUFFIXES
-          QGIS.app/Contents/Resources
-        REQUIRED
-      )
-    ENDIF()
+    # qgis_core is an archive without extensions for ios
+    SET(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES} "")
+  ENDIF (APPLE AND USE_MAC_BUNDLING)
 
-    # Not sure how this affect other build, so let's keep that only for iOs for now
-    IF(${CMAKE_SYSTEM_NAME} STREQUAL "iOS")
+  FIND_LIBRARY(QGIS_CORE_LIBRARY
+    NAMES qgis_core
+    PATHS
+      ${CMAKE_PREFIX_PATH}
+      ${QGIS_BUILD_PATH}/output/lib
+      ${QGIS_MAC_PATH}/Frameworks
+      ${QGIS_MAC_PATH}/lib
+      ${QGIS_PREFIX_PATH}/lib/
+      /usr/lib64
+      /usr/lib
+      /usr/local/lib
+      /Library/Frameworks
+      "$ENV{LIB_DIR}/lib/"
+    PATH_SUFFIXES
+      QGIS.app/Contents/Frameworks/qgis_core.framework
+  )
+  FIND_LIBRARY(QGIS_ANALYSIS_LIBRARY
+    NAMES qgis_analysis
+    PATHS
+      ${CMAKE_PREFIX_PATH}
+      ${QGIS_BUILD_PATH}/output/lib
+      ${QGIS_MAC_PATH}/Frameworks
+      ${QGIS_MAC_PATH}/lib
+      ${QGIS_PREFIX_PATH}/lib/
+      /usr/lib64
+      /usr/lib
+      /usr/local/lib
+      /Library/Frameworks
+      "$ENV{LIB_DIR}/lib/"
+    PATH_SUFFIXES
+      QGIS.app/Contents/Frameworks/qgis_analysis.framework
+  )
 
-      function(find_and_link_library TARGET LIBRARY FORCE_SDK)
-        if(FORCE_SDK)
-          set(ADDITIONAL_ARGS "NO_CMAKE_FIND_ROOT_PATH")
-        endif()
-        find_library(${LIBRARY}-LIBRARY ${LIBRARY} ${ADDITIONAL_ARGS})
-        if(${LIBRARY}-LIBRARY)
-          message(STATUS "Link ${${LIBRARY}-LIBRARY} interface to ${TARGET}")
-          target_link_libraries(${TARGET} INTERFACE ${${LIBRARY}-LIBRARY})
-        else()
-          message(FATAL_ERROR "Fail to find library ${LIBRARY}. Make sure it is present in CMAKE_PREFIX_PATH")
-        endif()
-      endfunction()
+  IF (APPLE AND USE_MAC_BUNDLING)
+    FIND_PATH(QGIS_APPLE_RESOURCES_DIR
+      NAMES FindQGIS.cmake
+      PATHS
+        ${CMAKE_PREFIX_PATH}
+        ${QGIS_BUILD_PATH}/output/bin
+        ${QGIS_MAC_PATH}/Frameworks
+        /Library/Frameworks
+      PATH_SUFFIXES
+        QGIS.app/Contents/Resources
+      REQUIRED
+    )
+  ENDIF()
 
-      # Create an interface library for qgis_core & qgis_analysis
-      # In the best world, this should have been properly handled inside qgis
-      # qgis should use cpack, and generate its correct find cmake scripts.
-      # If this is implemented in QGIS in the future, this whole won't have a reason to exist.
+  # Not sure how this affect other build, so let's keep that only for iOs for now
+  IF(${CMAKE_SYSTEM_NAME} STREQUAL "iOS")
 
-      add_library(qgis_core INTERFACE)
-      target_link_libraries(qgis_core INTERFACE ${QGIS_CORE_LIBRARY})
-
-      function(qgis_core_link_library LIBRARY FORCE_SDK)
-        find_and_link_library(qgis_core ${LIBRARY} ${FORCE_SDK})
-      endfunction()
-
-      # qgis_core is built using bz2, xml2 from sysroot
-      qgis_core_link_library(bz2 FALSE)
-      qgis_core_link_library(z FALSE)
-      qgis_core_link_library(xml2 FALSE)
-      qgis_core_link_library(iconv FALSE)
-      qgis_core_link_library(charset TRUE)
-      qgis_core_link_library(expat TRUE)
-      qgis_core_link_library(freexl TRUE)
-      qgis_core_link_library(gdal TRUE)
-      qgis_core_link_library(geos TRUE)
-      qgis_core_link_library(geos_c TRUE)
-      qgis_core_link_library(pq TRUE)
-      qgis_core_link_library(proj TRUE)
-      # Be careful, protobuf/protobuf-lite are also in sysroot. But we want to use the one from our sdk
-      qgis_core_link_library(protobuf-lite TRUE)
-      qgis_core_link_library(protobuf TRUE)
-      qgis_core_link_library(protoc TRUE)
-      qgis_core_link_library(qt5keychain TRUE)
-      qgis_core_link_library(qca-qt5 TRUE)
-      qgis_core_link_library(spatialindex TRUE)
-      qgis_core_link_library(spatialite TRUE)
-      qgis_core_link_library(tasn1 TRUE)
-      qgis_core_link_library(zip TRUE)
-
-      find_library(QGIS_WSMPROVIDER_PLUGIN_LIBRARY wmsprovider_a
-        PATH_SUFFIXES
-         QGIS.app/Contents/PlugIns/qgis/
-	 qgis/plugins
-      )
-      if(QGIS_WSMPROVIDER_PLUGIN_LIBRARY)
-        message(STATUS "Link ${QGIS_WSMPROVIDER_PLUGIN_LIBRARY} interface to qgis_core")
-        target_link_libraries(qgis_core INTERFACE ${QGIS_WSMPROVIDER_PLUGIN_LIBRARY})
+    function(find_and_link_library TARGET LIBRARY FORCE_SDK)
+      if(FORCE_SDK)
+        set(ADDITIONAL_ARGS "NO_CMAKE_FIND_ROOT_PATH")
+      endif()
+      find_library(${LIBRARY}-LIBRARY ${LIBRARY} ${ADDITIONAL_ARGS})
+      if(${LIBRARY}-LIBRARY)
+        message(STATUS "Link ${${LIBRARY}-LIBRARY} interface to ${TARGET}")
+        target_link_libraries(${TARGET} INTERFACE ${${LIBRARY}-LIBRARY})
       else()
-        message(FATAL_ERROR "Fail to find library wmsprovider_a. Make sure it is present in CMAKE_PREFIX_PATH/QGIS.app/Contents/PlugIns/qgis/")
+        message(FATAL_ERROR "Fail to find library ${LIBRARY}. Make sure it is present in CMAKE_PREFIX_PATH")
       endif()
+    endfunction()
 
-      find_library(QGIS_POSTGRESPROVIDER_PLUGIN_LIBRARY postgresprovider_a
-        PATH_SUFFIXES
-         QGIS.app/Contents/PlugIns/qgis/
-	 qgis/plugins
-      )
-      if(QGIS_POSTGRESPROVIDER_PLUGIN_LIBRARY)
-        message(STATUS "Link ${QGIS_POSTGRESPROVIDER_PLUGIN_LIBRARY} interface to qgis_core")
-        target_link_libraries(qgis_core INTERFACE ${QGIS_POSTGRESPROVIDER_PLUGIN_LIBRARY})
-      else()
-        message(FATAL_ERROR "Fail to find library postgresprovider_a. Make sure it is present in CMAKE_PREFIX_PATH/QGIS.app/Contents/PlugIns/qgis/")
-      endif()
+    # Create an interface library for qgis_core & qgis_analysis
+    # In the best world, this should have been properly handled inside qgis
+    # qgis should use cpack, and generate its correct find cmake scripts.
+    # If this is implemented in QGIS in the future, this whole won't have a reason to exist.
 
-      find_library(QGIS_POSTGRESRASTERPROVIDER_PLUGIN_LIBRARY postgresrasterprovider_a
-        PATH_SUFFIXES
-         QGIS.app/Contents/PlugIns/qgis/
-	 qgis/plugins
-      )
-      if(QGIS_POSTGRESRASTERPROVIDER_PLUGIN_LIBRARY)
-        message(STATUS "Link ${QGIS_POSTGRESRASTERPROVIDER_PLUGIN_LIBRARY} interface to qgis_core")
-        target_link_libraries(qgis_core INTERFACE ${QGIS_POSTGRESRASTERPROVIDER_PLUGIN_LIBRARY})
-      else()
-        message(FATAL_ERROR "Fail to find library postgresrasterprovider_a. Make sure it is present in CMAKE_PREFIX_PATH/QGIS.app/Contents/PlugIns/qgis/")
-      endif()
+    add_library(qgis_core INTERFACE)
+    target_link_libraries(qgis_core INTERFACE ${QGIS_CORE_LIBRARY})
 
-      target_include_directories(qgis_core INTERFACE ${QGIS_INCLUDE_DIR})
+    function(qgis_core_link_library LIBRARY FORCE_SDK)
+      find_and_link_library(qgis_core ${LIBRARY} ${FORCE_SDK})
+    endfunction()
 
-      # Versionless tagging of Qt, only supported from Qt5.15
-      if(NOT TARGET Qt::PrintSupport)
-        target_compile_definitions(qgis_core INTERFACE "-DQT_NO_PRINTER")
-      endif()
+    # qgis_core is built using bz2, xml2 from sysroot
+    qgis_core_link_library(bz2 FALSE)
+    qgis_core_link_library(z FALSE)
+    qgis_core_link_library(xml2 FALSE)
+    qgis_core_link_library(iconv FALSE)
+    qgis_core_link_library(charset TRUE)
+    qgis_core_link_library(expat TRUE)
+    qgis_core_link_library(freexl TRUE)
+    qgis_core_link_library(gdal TRUE)
+    qgis_core_link_library(geos TRUE)
+    qgis_core_link_library(geos_c TRUE)
+    qgis_core_link_library(pq TRUE)
+    qgis_core_link_library(proj TRUE)
+    # Be careful, protobuf/protobuf-lite are also in sysroot. But we want to use the one from our sdk
+    qgis_core_link_library(protobuf-lite TRUE)
+    qgis_core_link_library(protobuf TRUE)
+    qgis_core_link_library(protoc TRUE)
+    qgis_core_link_library(qt5keychain TRUE)
+    qgis_core_link_library(qca-qt5 TRUE)
+    qgis_core_link_library(spatialindex TRUE)
+    qgis_core_link_library(spatialite TRUE)
+    qgis_core_link_library(tasn1 TRUE)
+    qgis_core_link_library(zip TRUE)
 
-      function(qgis_analysis_link_library LIBRARY FORCE_SDK)
-        find_and_link_library(qgis_analysis ${LIBRARY} ${FORCE_SDK})
-      endfunction()
+    find_library(QGIS_WSMPROVIDER_PLUGIN_LIBRARY wmsprovider_a
+      PATH_SUFFIXES
+        QGIS.app/Contents/PlugIns/qgis/
+        qgis/plugins
+    )
+    if(QGIS_WSMPROVIDER_PLUGIN_LIBRARY)
+      message(STATUS "Link ${QGIS_WSMPROVIDER_PLUGIN_LIBRARY} interface to qgis_core")
+      target_link_libraries(qgis_core INTERFACE ${QGIS_WSMPROVIDER_PLUGIN_LIBRARY})
+    else()
+      message(FATAL_ERROR "Fail to find library wmsprovider_a. Make sure it is present in CMAKE_PREFIX_PATH/QGIS.app/Contents/PlugIns/qgis/")
+    endif()
 
-      add_library(qgis_analysis INTERFACE)
-      target_link_libraries(qgis_analysis INTERFACE ${QGIS_ANALYSIS_LIBRARY} qgis_core)
+    find_library(QGIS_POSTGRESPROVIDER_PLUGIN_LIBRARY postgresprovider_a
+      PATH_SUFFIXES
+        QGIS.app/Contents/PlugIns/qgis/
+        qgis/plugins
+    )
+    if(QGIS_POSTGRESPROVIDER_PLUGIN_LIBRARY)
+      message(STATUS "Link ${QGIS_POSTGRESPROVIDER_PLUGIN_LIBRARY} interface to qgis_core")
+      target_link_libraries(qgis_core INTERFACE ${QGIS_POSTGRESPROVIDER_PLUGIN_LIBRARY})
+    else()
+      message(FATAL_ERROR "Fail to find library postgresprovider_a. Make sure it is present in CMAKE_PREFIX_PATH/QGIS.app/Contents/PlugIns/qgis/")
+    endif()
 
-      qgis_analysis_link_library(exiv2 TRUE)
-      qgis_analysis_link_library(exiv2-xmp TRUE)
+    find_library(QGIS_POSTGRESRASTERPROVIDER_PLUGIN_LIBRARY postgresrasterprovider_a
+      PATH_SUFFIXES
+        QGIS.app/Contents/PlugIns/qgis/
+        qgis/plugins
+    )
+    if(QGIS_POSTGRESRASTERPROVIDER_PLUGIN_LIBRARY)
+      message(STATUS "Link ${QGIS_POSTGRESRASTERPROVIDER_PLUGIN_LIBRARY} interface to qgis_core")
+      target_link_libraries(qgis_core INTERFACE ${QGIS_POSTGRESRASTERPROVIDER_PLUGIN_LIBRARY})
+    else()
+      message(FATAL_ERROR "Fail to find library postgresrasterprovider_a. Make sure it is present in CMAKE_PREFIX_PATH/QGIS.app/Contents/PlugIns/qgis/")
+    endif()
+
+    target_include_directories(qgis_core INTERFACE ${QGIS_INCLUDE_DIR})
+
+    # Versionless tagging of Qt, only supported from Qt5.15
+    if(NOT TARGET Qt::PrintSupport)
+      target_compile_definitions(qgis_core INTERFACE "-DQT_NO_PRINTER")
+    endif()
+
+    function(qgis_analysis_link_library LIBRARY FORCE_SDK)
+      find_and_link_library(qgis_analysis ${LIBRARY} ${FORCE_SDK})
+    endfunction()
+
+    add_library(qgis_analysis INTERFACE)
+    target_link_libraries(qgis_analysis INTERFACE ${QGIS_ANALYSIS_LIBRARY} qgis_core)
+
+    qgis_analysis_link_library(exiv2 TRUE)
+    qgis_analysis_link_library(exiv2-xmp TRUE)
 
 
-      target_include_directories(qgis_analysis INTERFACE ${QGIS_ANALYSIS_INCLUDE_DIR})
+    target_include_directories(qgis_analysis INTERFACE ${QGIS_ANALYSIS_INCLUDE_DIR})
 
-      # Overwrite the variable to point to the interface library
-      set(QGIS_CORE_LIBRARY qgis_core)
-      set(QGIS_ANALYSIS_LIBRARY qgis_analysis)
+    # Overwrite the variable to point to the interface library
+    set(QGIS_CORE_LIBRARY qgis_core)
+    set(QGIS_ANALYSIS_LIBRARY qgis_analysis)
 
-      find_file(PROJ_DB proj.db PATHS ${CMAKE_PREFIX_PATH} PATH_SUFFIXES share/proj share/proj4 REQUIRED)
-      get_filename_component(QFIELD_PROJ_DIR ${PROJ_DB} DIRECTORY)
-      message("Found Proj DB directory: " ${QFIELD_PROJ_DIR})
+    find_file(PROJ_DB proj.db PATHS ${CMAKE_PREFIX_PATH} PATH_SUFFIXES share/proj share/proj4 REQUIRED)
+    get_filename_component(QFIELD_PROJ_DIR ${PROJ_DB} DIRECTORY)
+    message("Found Proj DB directory: " ${QFIELD_PROJ_DIR})
 
-      FIND_PATH(PROJ_INCLUDE_DIR proj_api.h NO_CMAKE_FIND_ROOT_PATH)
-      MESSAGE("Found Proj include dir: " ${PROJ_INCLUDE_DIR})
+    FIND_PATH(PROJ_INCLUDE_DIR proj_api.h NO_CMAKE_FIND_ROOT_PATH)
+    MESSAGE("Found Proj include dir: " ${PROJ_INCLUDE_DIR})
 
-      FIND_PATH(GDAL_INCLUDE_DIR gdal.h  NO_CMAKE_FIND_ROOT_PATH)
-      MESSAGE("Found GDAL include dir: " ${GDAL_INCLUDE_DIR})
+    FIND_PATH(GDAL_INCLUDE_DIR gdal.h  NO_CMAKE_FIND_ROOT_PATH)
+    MESSAGE("Found GDAL include dir: " ${GDAL_INCLUDE_DIR})
 
-    ENDIF() # iOs
-
-  ENDIF(UNIX)
-ENDIF(WIN32)
+  ENDIF() # iOs
+ENDIF()
 
 IF (QGIS_INCLUDE_DIR)
   SET(QGIS_VERSION QGIS_VERSION-NOTFOUND)
--- cmake/FindSqlite3.cmake
+++ cmake/FindSqlite3.cmake
@@ -17,46 +17,52 @@
 # FIND_* is invoked first with specified paths and NO_DEFAULT_PATH
 # and then again with no specified paths to search the default
 # locations. When an earlier FIND_* succeeds, subsequent FIND_*s
-# searching for the same item do nothing. 
-
-# try to use framework on mac
-# want clean framework path, not unix compatibility path
-IF (APPLE AND NOT QGIS_MAC_DEPS_DIR)
-  IF (CMAKE_FIND_FRAMEWORK MATCHES "FIRST"
-      OR CMAKE_FRAMEWORK_PATH MATCHES "ONLY"
-      OR NOT CMAKE_FIND_FRAMEWORK)
-    SET (CMAKE_FIND_FRAMEWORK_save ${CMAKE_FIND_FRAMEWORK} CACHE STRING "" FORCE)
-    SET (CMAKE_FIND_FRAMEWORK "ONLY" CACHE STRING "" FORCE)
-    #FIND_PATH(SQLITE3_INCLUDE_DIR SQLite3/sqlite3.h)
-    FIND_LIBRARY(SQLITE3_LIBRARY SQLite3)
-    IF (SQLITE3_LIBRARY)
-      # FIND_PATH doesn't add "Headers" for a framework
-      SET (SQLITE3_INCLUDE_DIR ${SQLITE3_LIBRARY}/Headers CACHE PATH "Path to a file.")
-    ENDIF (SQLITE3_LIBRARY)
-    SET (CMAKE_FIND_FRAMEWORK ${CMAKE_FIND_FRAMEWORK_save} CACHE STRING "" FORCE)
-  ENDIF ()
-ENDIF (APPLE AND NOT QGIS_MAC_DEPS_DIR)
-
-# FIND_PATH and FIND_LIBRARY normally search standard locations
-# before the specified paths. To search non-standard paths first,
-# FIND_* is invoked first with specified paths and NO_DEFAULT_PATH
-# and then again with no specified paths to search the default
-# locations. When an earlier FIND_* succeeds, subsequent FIND_*s
 # searching for the same item do nothing.
-FIND_PATH(SQLITE3_INCLUDE_DIR sqlite3.h
-  "$ENV{LIB_DIR}/include"
-  "$ENV{LIB_DIR}/include/sqlite"
-  "$ENV{INCLUDE}"
-  NO_DEFAULT_PATH
-)
-FIND_PATH(SQLITE3_INCLUDE_DIR sqlite3.h)
 
-FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3_i sqlite3 PATHS
-  "$ENV{LIB_DIR}/lib"
-  "$ENV{LIB}/lib"
-  NO_DEFAULT_PATH
-)
-FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3_i sqlite3)
+if(ANDROID)
+  set(SQLITE3_INCLUDE_DIR ${OSGEO4A_STAGE_DIR}/${ANDROID_ABI}/include CACHE PATH "")
+  set(SQLITE3_LIBRARY ${OSGEO4A_STAGE_DIR}/${ANDROID_ABI}/lib/libsqlite3.so CACHE PATH "")
+
+else()
+  # try to use framework on mac
+  # want clean framework path, not unix compatibility path
+  IF (APPLE AND NOT QGIS_MAC_DEPS_DIR)
+    IF (CMAKE_FIND_FRAMEWORK MATCHES "FIRST"
+        OR CMAKE_FRAMEWORK_PATH MATCHES "ONLY"
+        OR NOT CMAKE_FIND_FRAMEWORK)
+      SET (CMAKE_FIND_FRAMEWORK_save ${CMAKE_FIND_FRAMEWORK} CACHE STRING "" FORCE)
+      SET (CMAKE_FIND_FRAMEWORK "ONLY" CACHE STRING "" FORCE)
+      #FIND_PATH(SQLITE3_INCLUDE_DIR SQLite3/sqlite3.h)
+      FIND_LIBRARY(SQLITE3_LIBRARY SQLite3)
+      IF (SQLITE3_LIBRARY)
+        # FIND_PATH doesn't add "Headers" for a framework
+        SET (SQLITE3_INCLUDE_DIR ${SQLITE3_LIBRARY}/Headers CACHE PATH "Path to a file.")
+      ENDIF (SQLITE3_LIBRARY)
+      SET (CMAKE_FIND_FRAMEWORK ${CMAKE_FIND_FRAMEWORK_save} CACHE STRING "" FORCE)
+    ENDIF ()
+  ENDIF (APPLE AND NOT QGIS_MAC_DEPS_DIR)
+
+  # FIND_PATH and FIND_LIBRARY normally search standard locations
+  # before the specified paths. To search non-standard paths first,
+  # FIND_* is invoked first with specified paths and NO_DEFAULT_PATH
+  # and then again with no specified paths to search the default
+  # locations. When an earlier FIND_* succeeds, subsequent FIND_*s
+  # searching for the same item do nothing.
+  FIND_PATH(SQLITE3_INCLUDE_DIR sqlite3.h
+    "$ENV{LIB_DIR}/include"
+    "$ENV{LIB_DIR}/include/sqlite"
+    "$ENV{INCLUDE}"
+    NO_DEFAULT_PATH
+  )
+  FIND_PATH(SQLITE3_INCLUDE_DIR sqlite3.h)
+
+  FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3_i sqlite3 PATHS
+    "$ENV{LIB_DIR}/lib"
+    "$ENV{LIB}/lib"
+    NO_DEFAULT_PATH
+  )
+  FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3_i sqlite3)
+endif()
 
 IF (SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARY)
    SET(SQLITE3_FOUND TRUE)
