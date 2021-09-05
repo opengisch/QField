00cc6852ddd4d3200afea7ff547ffbc4c87bff19
Matthias Kuhn <matthias@opengis.ch>
2021-09-05 10:26:48 +0200

    Translation update

--- docs/scripts/update-tx.py
+++ docs/scripts/update-tx.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
 import pathlib
+import os
 from slugify import slugify
 
 with open('.tx/config', 'w') as f:
     f.write('''[main]
 host = https://www.transifex.com
 lang_map = zh-Hant: zh_TW''')
-    for md_file in list(pathlib.Path('docs/en').glob('**/*.md')):
-        relpath = md_file.relative_to('docs/en')
-
+    for md_file in list(pathlib.Path('docs').glob('**/*.md')):
+        relpath = md_file.relative_to('docs')
+        relbasename, _ = os.path.splitext(relpath)
 
         f.write(f'''
 [qfdocstest.{slugify(str(relpath))}]
-file_filter = docs/<lang>/{relpath}
+file_filter = docs/{relbasename}.<lang>.md
 minimum_perc = 0
-source_file = docs/en/{relpath}
+source_file = docs/{relbasename}.md
 source_lang = en
 type = GITHUBMARKDOWN
 ''')
