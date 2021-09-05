aecee2982e12f6f224c1c9cd4cc0da7d258caf7c
Matthias Kuhn <matthias@opengis.ch>
2021-09-05 10:46:51 +0200

    translation

--- docs/migrate.py
+++ docs/migrate.py
@@ -17,16 +17,16 @@ print(pathlib.Path(args.source))
 for rst_file in list(pathlib.Path(args.source).glob('**/*.rst')):
     rst_file_relpath = os.path.join('docs', rst_file.relative_to(args.source))
     basename, _ = os.path.splitext(rst_file_relpath)
-    if basename.endswith('index') and basename != 'docs/en/index':
+    if basename.endswith('index') and basename != 'docs/index':
         continue
     basedir = pathlib.Path(basename).parent
-    md_file_relpath = os.path.join(basename + '.md')
+    md_file_relpath = os.path.join('docs', pathlib.Path(os.path.join(basename + '.md')).relative_to('docs/en/'))
 
-    pathlib.Path(basedir).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(os.path.join('docs', basedir.relative_to('docs/en'))).mkdir(parents=True, exist_ok=True)
 
     subprocess.run(['pandoc', '-s', '-o', md_file_relpath, rst_file])
     subprocess.run(['sed', '-i', 's/{width=.*}//g', md_file_relpath])
-    subprocess.run(['sed', '-i', r's|\(\](\)\.*/image|\1../../assets/image|g', md_file_relpath])
+    subprocess.run(['sed', '-i', r's|\(\](\)\.*/image|\1../assets/image|g', md_file_relpath])
 
 copy_tree(os.path.join(args.source, 'en/images'), 'docs/assets/images')
 
