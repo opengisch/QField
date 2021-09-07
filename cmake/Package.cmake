c68e13fc97743c09bc7fd2e7c5b522e2a715b751
Matthias Kuhn <matthias@opengis.ch>
2021-09-08 00:18:59 +0200

    NSIS

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -1,10 +1,50 @@
-
-
-######################################################################################################################
-# Packaging
-######################################################################################################################
-
 find_program(LINUXDEPLOY_EXECUTABLE linuxdeploy linuxdeploy-x86_64.AppImage)
+
+function(windeployqt target)
+    # Bundle Library Files
+    if(CMAKE_BUILD_TYPE_UPPER STREQUAL "DEBUG")
+        set(WINDEPLOYQT_ARGS --debug)
+    else()
+        set(WINDEPLOYQT_ARGS --release)
+    endif()
+
+    add_custom_command(TARGET ${target} POST_BUILD
+                       COMMAND "${CMAKE_COMMAND}" -E remove_directory "${CMAKE_CURRENT_BINARY_DIR}/winqt/"
+                       COMMAND "${CMAKE_COMMAND}" -E
+                               env PATH="${_qt_bin_dir}" "${WINDEPLOYQT_EXECUTABLE}"
+                               ${WINDEPLOYQT_ARGS}
+                               --verbose 0
+                               --no-compiler-runtime
+                               --no-angle
+                               --no-opengl-sw
+                               --dir "${CMAKE_CURRENT_BINARY_DIR}/winqt/"
+                               $<TARGET_FILE:${target}>
+                       COMMENT "Deploying Qt..."
+    )
+    install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/winqt/" DESTINATION bin)
+    set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)
+    include(InstallRequiredSystemLibraries)
+endfunction()
+
+if(WIN32)
+    # NSIS windows installer
+    find_program(NSIS_PATH nsis PATH_SUFFIXES nsis)
+    if(NSIS_PATH)
+        list(APPEND CPACK_GENERATOR NSIS)
+        message(STATUS "   + NSIS                                 YES ")
+
+        set(CPACK_NSIS_DISPLAY_NAME ${CPACK_PACKAGE_NAME})
+        # Icon of the installer
+        file(TO_NATIVE_PATH "${PACKAGE_ICON}" CPACK_NSIS_MUI_ICON)
+        file(TO_NATIVE_PATH "${PACKAGE_ICON}" CPACK_NSIS_MUI_HEADERIMAGE_BITMAP)
+        set(CPACK_NSIS_CONTACT "${CPACK_PACKAGE_CONTACT}")
+        set(CPACK_NSIS_MODIFY_PATH ON)
+    else()
+        message(STATUS "   + NSIS                                 NO ")
+    endif()
+
+    windeployqt(qfield)
+endif()
 
 get_target_property(qmake_executable Qt5::qmake IMPORTED_LOCATION)
 
