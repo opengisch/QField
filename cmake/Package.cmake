7c0977014a860e7af05cae894a633418a3987e8a
Matthias Kuhn <matthias@opengis.ch>
2021-09-08 08:11:05 +0200

    fixes

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -43,7 +43,7 @@ if(WIN32)
         message(STATUS "   + NSIS                                 NO ")
     endif()
 
-    windeployqt(qfield)
+    windeployqt(QField)
 endif()
 
 get_target_property(qmake_executable Qt5::qmake IMPORTED_LOCATION)
