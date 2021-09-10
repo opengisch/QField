3aa77faa55c3f069be2be4f297b18579c2e6f4c4
Matthias Kuhn <matthias@opengis.ch>
2021-09-11 00:41:54 +0200

    wix

--- Package.cmake
+++ Package.cmake
@@ -3,32 +3,6 @@ set(CPACK_IFW_ROOT $ENV{HOME}/Qt/QtIFW-3.0.6/ CACHE PATH "Qt Installer Framework
 find_program(BINARYCREATOR_EXECUTABLE binarycreator HINTS "${_qt_bin_dir}" ${CPACK_IFW_ROOT}/bin)
 find_program(MACDEPLOYQT_EXECUTABLE macdeployqt HINTS "${_qt_bin_dir}")
 
-function(windeployqt target)
-    # Bundle Library Files
-    if(CMAKE_BUILD_TYPE_UPPER STREQUAL "DEBUG")
-        set(WINDEPLOYQT_ARGS --debug)
-    else()
-        set(WINDEPLOYQT_ARGS --release)
-    endif()
-
-    add_custom_command(TARGET ${target} POST_BUILD
-                       COMMAND "${CMAKE_COMMAND}" -E remove_directory "${CMAKE_CURRENT_BINARY_DIR}/winqt/"
-                       COMMAND "${CMAKE_COMMAND}" -E
-                               env PATH="${_qt_bin_dir}" "${WINDEPLOYQT_EXECUTABLE}"
-                               ${WINDEPLOYQT_ARGS}
-                               --verbose 0
-                               --no-compiler-runtime
-                               --no-angle
-                               --no-opengl-sw
-                               --dir "${CMAKE_CURRENT_BINARY_DIR}/winqt/"
-                               $<TARGET_FILE:${target}>
-                       COMMENT "Deploying Qt..."
-    )
-    install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/winqt/" DESTINATION bin)
-    set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)
-    include(InstallRequiredSystemLibraries)
-endfunction()
-
 function(macdeployqt bundle targetdir _PACKAGER)
     file(GENERATE OUTPUT ${CMAKE_BINARY_DIR}/CPackMacDeployQt-${_PACKAGER}.cmake
                   CONTENT "execute_process(COMMAND \"${MACDEPLOYQT_EXECUTABLE}\" \"${CPACK_PACKAGE_DIRECTORY}/_CPack_Packages/Darwin/${_PACKAGER}/${targetdir}/${bundle}\" -always-overwrite)")
@@ -37,23 +11,8 @@ function(macdeployqt bundle targetdir _PACKAGER)
 endfunction()
 
 if(WIN32)
-    # NSIS windows installer
-    find_program(NSIS_PATH nsis PATH_SUFFIXES nsis)
-    if(NSIS_PATH)
-        list(APPEND CPACK_GENERATOR NSIS)
-        message(STATUS "   + NSIS                                 YES ")
-
-        set(CPACK_NSIS_DISPLAY_NAME ${CPACK_PACKAGE_NAME})
-        # Icon of the installer
-        file(TO_NATIVE_PATH "${PACKAGE_ICON}" CPACK_NSIS_MUI_ICON)
-        file(TO_NATIVE_PATH "${PACKAGE_ICON}" CPACK_NSIS_MUI_HEADERIMAGE_BITMAP)
-        set(CPACK_NSIS_CONTACT "${CPACK_PACKAGE_CONTACT}")
-        set(CPACK_NSIS_MODIFY_PATH ON)
-    else()
-        message(STATUS "   + NSIS                                 NO ")
-    endif()
-
-    windeployqt(qfield)
+    message(STATUS "   + WIX                             YES ")
+    set(CPACK_GENERATOR "WIX")
 endif()
 
 get_target_property(qmake_executable Qt5::qmake IMPORTED_LOCATION)
