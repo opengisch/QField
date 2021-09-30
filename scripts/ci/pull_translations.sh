0eb33ed3dfc0995ebbc113179c782d46af86d712
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-09-30 11:10:45 +0200

    better folding on CI

--- scripts/ci/pull_translations.sh
+++ scripts/ci/pull_translations.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-echo "::group::tx-pull"
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../version_number.sh
 
+
+echo "::group::tx-pull"
 tx pull --all --force
+echo "::endgroup::"
 
+echo "::group::android specific translations"
 for x in android/res/values-*_*;do mv $x $(echo $x | sed -e 's/_/-r/') ;done
+echo "::endgroup::"
 
+echo "::group::lrelease"
 find ${DIR}/../../i18n -type f -name "*.ts" | xargs lrelease
-
 echo "::endgroup::"
