f2e48ccb95e9f98485ada333d4b5b28389bffb02
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-09-30 15:06:29 +0200

    Merge pull request #2168 from opengisch/better-sh
    use accolades in var names in shell scripts

--- scripts/version_number.sh
+++ scripts/version_number.sh
@@ -2,7 +2,7 @@
 
 # GNU prefix command for mac os support (gsed, gsplit)
 GP=
-if [[ "$OSTYPE" =~ darwin* ]]; then
+if [[ "${OSTYPE}" =~ darwin* ]]; then
   GP=g
 fi
 
@@ -10,13 +10,13 @@ fi
 # arg1: ARCH
 arch_to_build_number() {
   ARCH=$1
-  if [[ $ARCH == armv7 ]]; then
+  if [[ ${ARCH} == armv7 ]]; then
     echo 1
-  elif [[ $ARCH == arm64_v8a ]]; then
+  elif [[ ${ARCH} == arm64_v8a ]]; then
     echo 2
-  elif [[ $ARCH == x86 ]]; then
+  elif [[ ${ARCH} == x86 ]]; then
     echo 3
-  elif [[ $ARCH == x86_64 ]]; then
+  elif [[ ${ARCH} == x86_64 ]]; then
     echo 4
   else
     echo 0
