8affe5581751b2e44ef111d81747d9355eb5a48a
Mathieu Pellerin <nirvn.asia@gmail.com>
2021-11-12 06:48:09 +0700

    AAB packages + SDK 30 + Signature v2 for APKs (#2288)

--- scripts/ci/upload_artifacts.sh
+++ scripts/ci/upload_artifacts.sh
@@ -9,16 +9,25 @@ then
 
   if [[ "${CI_TAG}" ]];
   then
-    FILENAME="qfield-${CI_TAG}-${ARCH}.apk"
+    FILENAME_AAB="qfield-${CI_TAG}-${ARCH}.aab"
+    FILENAME_APK="qfield-${CI_TAG}-${ARCH}.apk"
   else
-    FILENAME="qfield-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${ARCH}.apk"
+    FILENAME_AAB="qfield-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${ARCH}.aab"
+    FILENAME_APK="qfield-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${ARCH}.apk"
   fi
 
-  mv ${CMAKE_BUILD_DIR}/android-build/build/outputs/apk/release/android-build-release-signed.apk /tmp/${FILENAME}
+  mv ${CMAKE_BUILD_DIR}/android-build/build/outputs/bundle/release/android-build-release.aab /tmp/${FILENAME_AAB}
   echo "${S3CFG}" > ~/.s3cfg
-  s3cmd put --acl-public /tmp/${FILENAME} s3://qfieldapks/ci-builds/${FILENAME}
-  echo -e "\e[31mUploaded to https://sos-ch-dk-2.exo.io/qfieldapks/ci-builds/${FILENAME} \e[0m"
-  s3cmd modify --add-header=content-type:application/vnd.android.package-archive s3://qfieldapks/ci-builds/${FILENAME}
+  s3cmd put --acl-public /tmp/${FILENAME_AAB} s3://qfieldapks/ci-builds/${FILENAME_AAB}
+  echo -e "\e[31mUploaded to https://sos-ch-dk-2.exo.io/qfieldapks/ci-builds/${FILENAME_AAB} \e[0m"
+  s3cmd modify --add-header=content-type:application/octet-stream s3://qfieldapks/ci-builds/${FILENAME_AAB}
+  echo -e "\e[31mcontent-type modified \e[0m"
+
+  mv ${CMAKE_BUILD_DIR}/android-build/build/outputs/apk/release/android-build-release-signed.apk /tmp/${FILENAME_APK}
+  echo "${S3CFG}" > ~/.s3cfg
+  s3cmd put --acl-public /tmp/${FILENAME_APK} s3://qfieldapks/ci-builds/${FILENAME_APK}
+  echo -e "\e[31mUploaded to https://sos-ch-dk-2.exo.io/qfieldapks/ci-builds/${FILENAME_APK} \e[0m"
+  s3cmd modify --add-header=content-type:application/vnd.android.package-archive s3://qfieldapks/ci-builds/${FILENAME_APK}
   echo -e "\e[31mcontent-type modified \e[0m"
 else
   echo -e "Not uploading artifacts ..."
