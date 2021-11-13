c870a8da09ccd38c5c06ee9020e6bd793baa0cb0
Matthias Kuhn <matthias@opengis.ch>
2021-11-13 11:53:45 +0000

    Use triplet instead of arch for versioncode (#2295)

--- test/test_version_number.sh
+++ test/test_version_number.sh
@@ -5,8 +5,8 @@ source ${DIR}/../scripts/version_number.sh
 
 set -v
 
-[[ $(apk_version_code v1.2.3 x86) != 010203993 ]] && echo "=> failed" && exit 1 || echo "OK"
-[[ $(apk_version_code v1.2.3-rc4 arm64_v8a) != 010203042 ]] && echo "=> failed" && exit 1 || echo "OK"
+[[ $(apk_version_code v1.2.3 x86-android) != 010203993 ]] && echo "=> failed" && exit 1 || echo "OK"
+[[ $(apk_version_code v1.2.3-rc4 arm64-android) != 010203042 ]] && echo "=> failed" && exit 1 || echo "OK"
 
 [[ $(app_version_str v1.2.3) != "1.2.3" ]] && echo "=> failed" && exit 1 || echo "OK"
 [[ $(app_version_str v1.2.3-rc4) != "1.2.3 RC4" ]] && echo "=> failed" && exit 1 || echo "OK"
