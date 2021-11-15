d8edc4c698b300eb194e42e0edcb91a9903b6c6a
nirvn <nirvn.asia@gmail.com>
2021-11-15 14:34:44 +0700

    Replace 3rdparty tessellator with QgsTesselator

--- src/core/sgrubberband.cpp
+++ src/core/sgrubberband.cpp
@@ -12,10 +12,15 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
+
 #include "sgrubberband.h"
-extern "C" {
-#include "tessellate.h"
-}
+
+#include <qgstessellator.h>
+#include <qgsgeometry.h>
+#include <qgspolygon.h>
+#include <qgslinestring.h>
+#include <qgssurface.h>
+#include <qgscurvepolygon.h>
 
 SGRubberband::SGRubberband( const QVector<QgsPoint> &points, QgsWkbTypes::GeometryType type, const QColor &color, qreal width )
   : QSGNode()
@@ -73,38 +78,35 @@ QSGGeometryNode *SGRubberband::createLineGeometry( const QVector<QgsPoint> &poin
 
 QSGGeometryNode *SGRubberband::createPolygonGeometry( const QVector<QgsPoint> &points )
 {
-  double *coordinates_out;
-  int *tris_out;
-  int nverts, ntris;
-
-  double *vertices_in = ( double * ) malloc( points.size() * 2 * sizeof( double ) );
-  const double *contours_array[] = { vertices_in, vertices_in + points.size() * 2 };
-  int i = 0;
-
-  for ( const QgsPoint &pt : points )
-  {
-    vertices_in[i++] = pt.x();
-    vertices_in[i++] = pt.y();
-  }
-
-  tessellate( &coordinates_out, &nverts,
-              &tris_out, &ntris,
-              contours_array, contours_array + 2 );
+  QgsPolygon *polygon = new QgsPolygon( new QgsLineString( points ) );
+  QgsTessellator t( 0, 0, false, false, false, true );
+  if ( points.size() > 2 )
+    t.addPolygon( *polygon, 0 );
 
   QSGGeometryNode *node = new QSGGeometryNode;
-  QSGGeometry *sgGeom = new QSGGeometry( QSGGeometry::defaultAttributes_Point2D(), ntris * 3 );
+  QSGGeometry *sgGeom = new QSGGeometry( QSGGeometry::defaultAttributes_Point2D(), t.dataVerticesCount() );
 
   QSGGeometry::Point2D *vertices = sgGeom->vertexDataAsPoint2D();
 
-  for ( int j = 0; j < ntris * 3; j++ )
+  const QVector<float> triangleData = t.data();
+  int currentVertex = 0;
+  for ( auto it = triangleData.constBegin(); it != triangleData.constEnd(); )
   {
-    vertices[j].x = coordinates_out[tris_out[j] * 2];
-    vertices[j].y = coordinates_out[tris_out[j] * 2 + 1];
-  }
+    vertices[currentVertex].x = ( *it++ );
+    ( void )it++; // z
+    vertices[currentVertex].y = -( *it++ );
+    currentVertex++;
 
-  free( vertices_in );
-  free( coordinates_out );
-  free( tris_out );
+    vertices[currentVertex].x = ( *it++ );
+    ( void )it++; // z
+    vertices[currentVertex].y = -( *it++ );
+    currentVertex++;
+
+    vertices[currentVertex].x = ( *it++ );
+    ( void )it++; // z
+    vertices[currentVertex].y = -( *it++ );
+    currentVertex++;
+  }
 
   sgGeom->setDrawingMode( GL_TRIANGLES );
   node->setGeometry( sgGeom );
