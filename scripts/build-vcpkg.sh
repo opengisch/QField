26554569d5c34cc0ba6725926758d98db117e566
Matthias Kuhn <matthias@opengis.ch>
2021-11-28 16:37:32 +0100

    Reinstate local build scripts

---
+++ scripts/build-vcpkg.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "building for ${triplet}"
+
+cd /usr/src/qfield || exit
+./vcpkg/base/bootstrap-vcpkg.sh
+pip3 install aqtinstall
+/home/devel/.local/bin/aqt install-qt linux android 5.14.2 -m qtcharts
+
+export ANDROID_NDK_HOME=/home/devel/android/ndk/21.4.7075529/
+export Qt5_DIR=/usr/src/qfield/5.14.2/android/
+export ANDROID_PLATFORM=21
+export ANDROID_TARGET_PLATFORM=30
+
+[[ -z ${APP_NAME} ]] && APP_NAME="QField"
+[[ -z ${APP_PACKAGE_NAME} ]] && APP_PACKAGE_NAME="qfield"
+[[ -z ${APP_ICON} ]] && APP_ICON="qfield_logo"
+
+echo "Package name ${APP_PACKAGE_NAME}"
+
+# This should be factored out, we also need it in .github/workflows/android.yaml
+case ${triplet} in
+  arm64-android)
+    export ARCH=arm64_v8a
+    export ABI=arm64-v8a
+    ;;
+  arm-android)
+    export ARCH=armv7
+    export ABI=armeabi-v7a
+    ;;
+  x64-android)
+    export ARCH=x86_64
+    export ABI=x86_64
+    ;;
+  x86-android)
+    export ARCH=x86
+    export ABI=x86
+    ;;
+esac
+
+cmake -S "${SOURCE_DIR}" \
+      -B "${CMAKE_BUILD_DIR}" \
+      -G Ninja \
+      -DCMAKE_TOOLCHAIN_FILE=/usr/src/qfield/vcpkg/base/scripts/buildsystems/vcpkg.cmake \
+      -DVCPKG_OVERLAY_PORTS=/usr/src/qfield/vcpkg/overlay_system_qt\;/usr/src/qfield/vcpkg/overlay \
+      -DVCPKG_TARGET_TRIPLET="${triplet}" \
+      -DWITH_VCPKG=ON \
+      -DWITH_SPIX=OFF \
+      -DAPP_VERSION="${APP_VERSION}" \
+      -DAPK_VERSION_CODE="${APK_VERSION_CODE}" \
+      -DAPP_VERSION_STR="${APP_VERSION_STR}" \
+      -DAPP_PACKAGE_NAME="${APP_PACKAGE_NAME}" \
+      -DAPP_ICON="${APP_ICON}" \
+      -DAPP_NAME="${APP_NAME}" \
+      -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake \
+      -DANDROID_ABI=${ABI} \
+      -DANDROID_PLATFORM=${ANDROID_PLATFORM} \
+      -DANDROID_TARGET_PLATFORM=${ANDROID_TARGET_PLATFORM}
+
+ninja
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,18 @@
 
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"/..
 
-QFIELD_SDK_VERSION=$(awk -F "=" '/osgeo4a_version/{print $2}' sdk.conf)
 export APK_VERSION_CODE=${APK_VERSION_CODE:-1}
 export APP_VERSION_STR=${APP_VERSION_STR:-dev}
 
-ARCH=${ARCH:-arm64_v8a}
+triplet=${triplet:-arm64-android}
+
+docker build ${SRC_DIR}/.docker/android_dev -t qfield_and_dev
 
 docker run --rm \
   -v "$SRC_DIR":/usr/src/qfield:Z \
   $(if [ -n "$CACHE_DIR" ]; then echo "-v $CACHE_DIR:/io/data"; fi) \
-  -e "BUILD_FOLDER=build-${ARCH}" \
-  -e ARCH \
+  -e "BUILD_FOLDER=build-${triplet}" \
+  -e triplet=${triplet} \
   -e STOREPASS \
   -e KEYNAME \
   -e KEYPASS \
@@ -22,5 +23,5 @@ docker run --rm \
   -e APP_VERSION \
   -e APP_VERSION_STR \
   -e APK_VERSION_CODE \
-  opengisch/qfield-sdk:${QFIELD_SDK_VERSION} \
-  /usr/src/qfield/scripts/docker-build-wrapper.sh
+  qfield_and_dev \
+  /usr/src/qfield/scripts/build-vcpkg.sh
