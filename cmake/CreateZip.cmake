e7754376184f628018e818bee764a574c10e438f
Matthias Kuhn <matthias@opengis.ch>
2021-11-28 22:21:37 +0100

    Remove CreateZip, we no longer zip assets

--- cmake/CreateZip.cmake
+++
@@ -1,9 +0,0 @@
-function(create_zip output_file input_files working_dir)
-    add_custom_command(
-        COMMAND ${CMAKE_COMMAND} -E tar "cf" "${output_file}" --format=zip -- ${input_files}
-        WORKING_DIRECTORY "${working_dir}"
-        OUTPUT  "${output_file}"
-        DEPENDS ${input_files}
-        COMMENT "Zipping to ${output_file}."
-    )
-endfunction()
