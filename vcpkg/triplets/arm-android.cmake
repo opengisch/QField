60e0ef7ac0b7e1b18b8d8479b54e0a1a7621b28c
Matthias Kuhn <matthias@opengis.ch>
2022-03-28 18:52:50 +0200

    Dynamic linkage

--- vcpkg/triplets/arm-android.cmake
+++ vcpkg/triplets/arm-android.cmake
@@ -1,8 +1,5 @@
 set(VCPKG_TARGET_ARCHITECTURE arm)
-set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
-
-if(PORT STREQUAL "openssl")
-  set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
+set(VCPKG_BUILD_TYPE release)
