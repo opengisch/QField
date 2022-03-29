dc2c8e17bfb9e538ec3edb30e82a33cff52c75ba
Matthias Kuhn <matthias@opengis.ch>
2022-03-29 09:41:27 +0200

    Update arm-android.cmake

--- vcpkg/triplets/arm-android.cmake
+++ vcpkg/triplets/arm-android.cmake
@@ -3,3 +3,5 @@ set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 # set(VCPKG_BUILD_TYPE release)
+
+set(ENV{VCPKG_ANDROID_NATIVE_API_LEVEL} "detect")
