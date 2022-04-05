942e0341ed5c6c71f060c44e4964b0b2ff8afd99
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-04-05 16:11:28 +0700

    Triplets harmony!

--- vcpkg/triplets/x86-android.cmake
+++ vcpkg/triplets/x86-android.cmake
@@ -1,9 +1,10 @@
 set(VCPKG_TARGET_ARCHITECTURE x86)
-set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
-
 set(VCPKG_BUILD_TYPE release)
 
 set(ENV{VCPKG_ANDROID_NATIVE_API_LEVEL} "detect")
+set(ENV{CXXFLAGS} "-fstack-protector-strong")
+set(ENV{CFLAGS} "-fstack-protector-strong")
 
