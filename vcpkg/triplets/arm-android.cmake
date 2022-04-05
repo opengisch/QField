58768e77c5d3321f386c068747843749dfe2bf32
Matthias Kuhn <matthias@opengis.ch>
2022-04-05 13:49:21 +0200

    Triplets (#2667)
    This makes builds faster by building libs only once, (not twice).
    Adds stack protection for better stack traces on android.
    Builds against shared c++ lib on android

--- vcpkg/triplets/arm-android.cmake
+++ vcpkg/triplets/arm-android.cmake
@@ -1,8 +1,9 @@
 set(VCPKG_TARGET_ARCHITECTURE arm)
-set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
+set(VCPKG_BUILD_TYPE release)
 
-if(PORT STREQUAL "openssl")
-  set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
+set(ENV{VCPKG_ANDROID_NATIVE_API_LEVEL} "detect")
+set(ENV{CXXFLAGS} "-fstack-protector-strong")
+set(ENV{CFLAGS} "-fstack-protector-strong")
