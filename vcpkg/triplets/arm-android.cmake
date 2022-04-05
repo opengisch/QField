7cda92aef5ac8ddda8a7f98a12b78ea114cad2ea
Matthias Kuhn <matthias@opengis.ch>
2022-04-05 14:36:51 +0200

    Debug

--- vcpkg/triplets/arm-android.cmake
+++ vcpkg/triplets/arm-android.cmake
@@ -2,7 +2,7 @@ set(VCPKG_TARGET_ARCHITECTURE arm)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
-set(VCPKG_BUILD_TYPE release)
+set(VCPKG_BUILD_TYPE debug)
 
 set(ENV{VCPKG_ANDROID_NATIVE_API_LEVEL} "detect")
 set(ENV{CXXFLAGS} "-fstack-protector-strong")
