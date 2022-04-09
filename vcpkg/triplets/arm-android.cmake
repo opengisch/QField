e13ef389c7b5ff8c51c5e9813d8841a7a698d1a9
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-04-09 17:20:34 +0700

    Throwing all in

--- vcpkg/triplets/arm-android.cmake
+++ vcpkg/triplets/arm-android.cmake
@@ -13,6 +13,15 @@ set(ENV{CFLAGS} "-fstack-protector-strong -fsanitize=address -fno-omit-frame-poi
 set(ENV{CFLAGS_RELEASE} "-fstack-protector-strong -fsanitize=address -fno-omit-frame-pointer")
 set(ENV{LDFLAGS} "-fsanitize=address")
 set(ENV{LDFLAGS_RELEASE} "-fsanitize=address")
+
+set(VCPKG_CXX_FLAGS "-fstack-protector-strong -fsanitize=address -fno-omit-frame-pointer")
+set(VCPKG_CXX_FLAGS_RELEASE "-fstack-protector-strong -fsanitize=address -fno-omit-frame-pointer")
+set(VCPKG_C_FLAGS "-fstack-protector-strong -fsanitize=address -fno-omit-frame-pointer")
+set(VCPKG_C_FLAGS_RELEASE "-fstack-protector-strong -fsanitize=address -fno-omit-frame-pointer")
+set(CMAKE_STATIC_LINKER_FLAGS "-fsanitize=address")
+set(CMAKE_STATIC_LINKER_FLAGS_RELEASE "-fsanitize=address")
+set(VCPKG_DETECTED_CMAKE_STATIC_LINKER_FLAGS "-fsanitize=address")
+set(VCPKG_DETECTED_CMAKE_STATIC_LINKER_FLAGS_RELEASE "-fsanitize=address")
 #endif()
 
 set(ENV{VCPKG_ANDROID_NATIVE_API_LEVEL} "27")
