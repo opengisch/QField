157cb6c925750b1103670c3656e7d2eb387ef79d
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-04-09 10:03:41 +0700

    More tweaking

--- vcpkg/triplets/arm-android.cmake
+++ vcpkg/triplets/arm-android.cmake
@@ -4,12 +4,14 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
 
+
 set(ENV{VCPKG_ANDROID_NATIVE_API_LEVEL} "detect")
 
-#if(PORT STREQUAL "proj4" OR PORT STREQUAL "proj")
+if(PORT STREQUAL "proj4" OR PORT STREQUAL "proj")
 set(ENV{CXXFLAGS} "-fstack-protector-strong -fsanitize=address -fno-omit-frame-pointer")
 set(ENV{CFLAGS} "-fstack-protector-strong -fsanitize=address -fno-omit-frame-pointer")
 set(ENV{LDFLAGS} "-fsanitize=address")
-#endif()
+endif()
+
 set(ENV{VCPKG_ANDROID_NATIVE_API_LEVEL} "27")
 
