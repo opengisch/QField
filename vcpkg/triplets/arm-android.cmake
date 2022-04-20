6c20ea3e370f8b5df278950b73dd0e0dd9672586
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-04-20 08:54:10 +0700

    ANDROID_STL shared

--- vcpkg/triplets/arm-android.cmake
+++ vcpkg/triplets/arm-android.cmake
@@ -1,10 +1,13 @@
 set(VCPKG_TARGET_ARCHITECTURE arm)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
 
 set(ENV{VCPKG_ANDROID_NATIVE_API_LEVEL} "24")
+set(VCPKG_ANDROID_NATIVE_API_LEVEL "24")
+set(ANDROID_STL c++_shared)
+set(ANDROID_ARM_NEON FALSE)
 
 set(VCPKG_CXX_FLAGS "-fstack-protector-strong")
 set(VCPKG_C_FLAGS "-fstack-protector-strong")
