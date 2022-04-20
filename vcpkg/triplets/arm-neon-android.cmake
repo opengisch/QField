c6ffbbd322070f7229a4a1fef939c3cdf8ff5888
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-04-21 00:07:54 +0700

    U-turn

--- vcpkg/triplets/arm-neon-android.cmake
+++ vcpkg/triplets/arm-neon-android.cmake
@@ -1,12 +1,12 @@
 set(VCPKG_TARGET_ARCHITECTURE arm)
-set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
 
 set(ENV{VCPKG_ANDROID_NATIVE_API_LEVEL} "24")
 set(VCPKG_ANDROID_NATIVE_API_LEVEL "24")
-set(ANDROID_STL c++_shared)
+set(ANDROID_STL c++_static)
 set(ANDROID_ARM_NEON TRUE)
 
 set(VCPKG_CXX_FLAGS "-fstack-protector-strong")
