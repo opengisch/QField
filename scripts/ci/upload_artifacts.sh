f0e1b1e7dccf6c27389ef068e5830093d77b9487
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-05-11 10:54:14 +0700

    Fix artifacts, disable AAB upload

--- scripts/ci/upload_artifacts.sh
+++ scripts/ci/upload_artifacts.sh
@@ -10,28 +10,29 @@ then
   if [[ "${CI_TAG}" ]];
   then
     if [[ ${ALL_FILES_ACCESS} == "ON" ]]; then
-      FILENAME_AAB="qfield-${CI_TAG}-${TRIPLET}.aab"
+      #FILENAME_AAB="qfield-${CI_TAG}-${TRIPLET}.aab"
       FILENAME_APK="qfield-${CI_TAG}-${TRIPLET}.apk"
     else
-      FILENAME_AAB="qfield-${CI_TAG}-${TRIPLET}.aab"
+      #FILENAME_AAB="qfield-${CI_TAG}-${TRIPLET}.aab"
       FILENAME_APK="qfield-${CI_TAG}-${TRIPLET}.apk"
     fi
   else
     if [[ ${ALL_FILES_ACCESS} == "ON" ]]; then
-      FILENAME_AAB="qfield-plus-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${TRIPLET}.aab"
+      #FILENAME_AAB="qfield-plus-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${TRIPLET}.aab"
       FILENAME_APK="qfield-plus-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${TRIPLET}.apk"
     else
-      FILENAME_AAB="qfield-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${TRIPLET}.aab"
+      #FILENAME_AAB="qfield-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${TRIPLET}.aab"
       FILENAME_APK="qfield-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${TRIPLET}.apk"
     fi
   fi
 
-  mv ${CMAKE_BUILD_DIR}/android-build/build/outputs/bundle/release/android-build-release.aab /tmp/${FILENAME_AAB}
-  echo "${S3CFG}" > ~/.s3cfg
-  s3cmd put --acl-public /tmp/${FILENAME_AAB} s3://qfieldapks/ci-builds/${FILENAME_AAB}
-  echo -e "\e[31mUploaded to https://sos-ch-dk-2.exo.io/qfieldapks/ci-builds/${FILENAME_AAB} \e[0m"
-  s3cmd modify --add-header=content-type:application/octet-stream s3://qfieldapks/ci-builds/${FILENAME_AAB}
-  echo -e "\e[31mcontent-type modified \e[0m"
+  # skip AAB upload until we need it
+  #mv ${CMAKE_BUILD_DIR}/android-build/build/outputs/bundle/release/android-build-release.aab /tmp/${FILENAME_AAB}
+  #echo "${S3CFG}" > ~/.s3cfg
+  #s3cmd put --acl-public /tmp/${FILENAME_AAB} s3://qfieldapks/ci-builds/${FILENAME_AAB}
+  #echo -e "\e[31mUploaded to https://sos-ch-dk-2.exo.io/qfieldapks/ci-builds/${FILENAME_AAB} \e[0m"
+  #s3cmd modify --add-header=content-type:application/octet-stream s3://qfieldapks/ci-builds/${FILENAME_AAB}
+  #echo -e "\e[31mcontent-type modified \e[0m"
 
   mv ${CMAKE_BUILD_DIR}/android-build/build/outputs/apk/release/android-build-release-signed.apk /tmp/${FILENAME_APK}
   echo "${S3CFG}" > ~/.s3cfg
