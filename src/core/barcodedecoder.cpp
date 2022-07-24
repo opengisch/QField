f7cc08115211f92146f3049c21b9dda2cb3f2971
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-07-24 14:37:31 +0700

    Plug in ZXing decoding into the barcode decodero object

--- src/core/barcodedecoder.cpp
+++ src/core/barcodedecoder.cpp
@@ -16,6 +16,8 @@
 
 #include "barcodedecoder.h"
 
+#include <ZXing/ReadBarcode.h>
+
 BarcodeDecoder::BarcodeDecoder( QObject *parent )
   : QObject( parent )
 {
@@ -35,6 +37,45 @@ void BarcodeDecoder::setDecodedString( const QString &decodedString )
 
 void BarcodeDecoder::decodeImage( const QImage &image )
 {
+  auto imageFormatFromQImage = []( const QImage &img ) {
+    switch ( img.format() )
+    {
+      case QImage::Format_ARGB32:
+      case QImage::Format_RGB32:
+#if Q_BYTE_ORDER == Q_LITTLE_ENDIAN
+        return ZXing::ImageFormat::BGRX;
+#else
+        return ImageFormat::XRGB;
+#endif
+      case QImage::Format_RGB888:
+        return ZXing::ImageFormat::RGB;
+      case QImage::Format_RGBX8888:
+      case QImage::Format_RGBA8888:
+        return ZXing::ImageFormat::RGBX;
+      case QImage::Format_Grayscale8:
+        return ZXing::ImageFormat::Lum;
+      default:
+        return ZXing::ImageFormat::None;
+    }
+  };
+
+  ZXing::ImageFormat imageFormat = imageFormatFromQImage( image );
+  if ( imageFormat != ZXing::ImageFormat::None )
+  {
+    ZXing::ImageView imageView( image.bits(), image.width(), image.height(), imageFormat, static_cast<int>( image.bytesPerLine() ) );
+
+    ZXing::DecodeHints hints;
+    hints.setFormats( ZXing::BarcodeFormat::Any );
+    hints.setTryRotate( false );
+
+    ZXing::Result result = ZXing::ReadBarcode( imageView, hints );
+    QString resultText = QString::fromWCharArray( result.text().c_str() );
+    if ( !resultText.isEmpty() )
+    {
+      setDecodedString( resultText );
+    }
+  }
+
   return;
 }
 
