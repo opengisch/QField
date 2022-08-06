92f328cd9825cdfe7fff923031663e9ac72b46e5
Matthias Kuhn <matthias@opengis.ch>
2022-08-06 20:45:21 +0200

    License headers

--- src/sentry/sentry_classic.cpp
+++ src/sentry/sentry_classic.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+  sentry_classic.cpp
+
+ ---------------------
+ begin                : August 2022
+ copyright            : (C) 2022 by Matthias Kuhn, OPENGIS.ch
+ email                : matthias@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "sentry_config.h"
 #include "sentry_wrapper.h"
 
--- src/sentry/sentry_wrapper.h
+++ src/sentry/sentry_wrapper.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+  sentry_wrapper.h
+
+ ---------------------
+ begin                : August 2022
+ copyright            : (C) 2022 by Matthias Kuhn, OPENGIS.ch
+ email                : matthias@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 namespace sentry_wrapper
 {
   void init();
