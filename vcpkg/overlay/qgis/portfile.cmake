c3232e57844cdea6307653af110d751f4917f9c7
Matthias Kuhn <matthias@opengis.ch>
2022-08-11 17:57:13 +0200

    Fix poly2tri target

--- vcpkg/overlay/qgis/portfile.cmake
+++ vcpkg/overlay/qgis/portfile.cmake
@@ -203,6 +203,8 @@ if(VCPKG_TARGET_IS_WINDOWS)
         list(APPEND QGIS_OPTIONS -DPoly2Tri_INCLUDE_DIR:PATH=${CMAKE_CURRENT_LIST_DIR}/poly2tri)
         list(APPEND QGIS_OPTIONS_DEBUG -DPoly2Tri_LIBRARY:PATH=${CURRENT_INSTALLED_DIR}/debug/lib/qt_poly2tri_debug.lib) # static qt only
         list(APPEND QGIS_OPTIONS_RELEASE -DPoly2Tri_LIBRARY:PATH=${CURRENT_INSTALLED_DIR}/lib/qt_poly2tri.lib) # static qt only
+    else()
+        list(APPEND -DPoly2Tri_LIBRARY=poly2tri::poly2tri)
     endif()
 else() # Build in UNIX
     list(APPEND QGIS_OPTIONS -DCMAKE_FIND_ROOT_PATH=$ENV{Qt5_DIR}) # for building with system Qt. Should find a nicer solution.
@@ -236,6 +238,8 @@ else() # Build in UNIX
         list(APPEND QGIS_OPTIONS -DPoly2Tri_INCLUDE_DIR:PATH=${CMAKE_CURRENT_LIST_DIR}/poly2tri)
         list(APPEND QGIS_OPTIONS_DEBUG -DPoly2Tri_LIBRARY:PATH=${QT_POLY2TRI_DIR_DEBUG}/debug/lib/libqt_poly2tri_debug.a) # static qt only
         list(APPEND QGIS_OPTIONS_RELEASE -DPoly2Tri_LIBRARY:PATH=${QT_POLY2TRI_DIR_RELEASE}/lib/libqt_poly2tri.a) # static qt only
+    else()
+        list(APPEND -DPoly2Tri_LIBRARY=poly2tri::poly2tri)
     endif()
 endif()
 
