4853173551afedc9731985bf834da40126e64e71
Matthias Kuhn <matthias@opengis.ch>
2022-09-18 12:57:35 +0200

    Add triplets

--- vcpkg/triplets/arm64-android.cmake
+++ vcpkg/triplets/arm64-android.cmake
@@ -3,6 +3,7 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
 
 set(VCPKG_CXX_FLAGS "-fstack-protector-strong")
 set(VCPKG_C_FLAGS "-fstack-protector-strong")
