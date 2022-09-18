42f27c897f78d0dbf36408050a529ecfcdad2bea
Matthias Kuhn <matthias@opengis.ch>
2022-09-18 12:54:28 +0200

    Baseline update

--- vcpkg/triplets/arm64-android.cmake
+++ vcpkg/triplets/arm64-android.cmake
@@ -4,6 +4,5 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
 
-
 set(VCPKG_CXX_FLAGS "-fstack-protector-strong")
 set(VCPKG_C_FLAGS "-fstack-protector-strong")
