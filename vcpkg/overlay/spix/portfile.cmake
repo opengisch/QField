1244e44e0f7d0d17b607f94af2c0af7881aca380
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-09-25 09:45:50 +0700

    Update spix to latest master revision to gain ability to invoke QML functions

--- vcpkg/overlay/spix/portfile.cmake
+++ vcpkg/overlay/spix/portfile.cmake
@@ -3,18 +3,15 @@ vcpkg_from_github(
     REPO faaxm/spix
     REF 4de44072d76b49ceaae9c57046f2260948c065a4
     SHA512 07555c17f1836e1297ff91cc5dd05f7171a2a23dc6896be4bb6ab88b9b8d96ff0908b41758b4effcc719257a9b7eacff258a7fece896d5075082afcbfe534fc0
-
     HEAD_REF master
 )
-
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ANYRPC_LIB_BUILD_SHARED)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DSPIX_BUILD_EXAMPLES=OFF
         -DSPIX_BUILD_TESTS=OFF
-        -DSPIX_QT_MAJOR=6
+        -DSPIX_QT_MAJOR=5
 )
 
 vcpkg_cmake_install()
