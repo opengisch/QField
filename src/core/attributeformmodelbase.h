431808a49260c373dd7bdce12dc770228f8c5763
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-10-18 15:21:16 +0700

    Additional guard against endless update loop

--- src/core/attributeformmodelbase.h
+++ src/core/attributeformmodelbase.h
@@ -81,7 +81,7 @@ class AttributeFormModelBase : public QStandardItemModel
 
     void flatten( QgsAttributeEditorContainer *container, QStandardItem *parent, const QString &parentVisibilityExpressions, QVector<QStandardItem *> &items, int currentTabIndex = 0, const QColor &color = QColor() );
 
-    void updateDefaultValues( int fieldIndex = -1 );
+    void updateDefaultValues( int fieldIndex = -1, QVector<int> updatedFields = QVector<int>() );
 
     void updateVisibilityAndConstraints( int fieldIndex = -1 );
 
