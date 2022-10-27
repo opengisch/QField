360a74d13688697ba5143d9d3b544c2b004f34c4
Matthias Kuhn <matthias@opengis.ch>
2022-10-27 08:28:40 +0200

    Adjust ANDROID_SDK_ROOT var

--- vcpkg/triplets/arm64-android.cmake
+++ vcpkg/triplets/arm64-android.cmake
@@ -4,7 +4,7 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
 set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
-set(ANDROID_SDK_HOME $ENV{ANDROID_SDK_ROOT})
+set(ANDROID_SDK_ROOT $ENV{ANDROID_SDK_ROOT})
 
 set(VCPKG_CXX_FLAGS "-fstack-protector-strong")
 set(VCPKG_C_FLAGS "-fstack-protector-strong")
