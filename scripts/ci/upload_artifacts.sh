5ab32b629281732b41fe208b14f8e47d858a7e63
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-10-28 13:52:56 +0700

    Final cleanups

--- scripts/ci/upload_artifacts.sh
+++ scripts/ci/upload_artifacts.sh
@@ -7,14 +7,8 @@ if [[ "${S3CFG}" ]];
 then
   echo -e "\e[31mAbout to upload build artifacts\e[0m"
 
-  if [[ "${CI_TAG}" ]];
-  then
-    #FILENAME_AAB="${APP_PACKAGE_NAME}-${CI_TAG}-${TRIPLET}.aab"
-    FILENAME_APK="${APP_PACKAGE_NAME}-${CI_TAG}-${TRIPLET}.apk"
-  else
-    #FILENAME_AAB="${APP_PACKAGE_NAME}-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${TRIPLET}.aab"
-    FILENAME_APK="${APP_PACKAGE_NAME}-dev-${CI_UPLOAD_ARTIFACT_ID}-${CI_COMMIT}-${TRIPLET}.apk"
-  fi
+  #FILENAME_AAB="${APP_PACKAGE_NAME}-${CI_PACKAGE_FILE_SUFFIX}-${TRIPLET}.aab"
+  FILENAME_APK="${APP_PACKAGE_NAME}-${CI_PACKAGE_FILE_SUFFIX}-${TRIPLET}.apk"
 
   # skip AAB upload until we need it
   #mv ${CMAKE_BUILD_DIR}/android-build/build/outputs/bundle/release/android-build-release.aab /tmp/${FILENAME_AAB}
