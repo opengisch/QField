3e2fd5445b9f7dbe10f68d393986f8abc0cc63a9
Matthias Kuhn <matthias@opengis.ch>
2022-10-29 15:40:36 +0200

    Update ccache.cmake

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -1,4 +1,45 @@
-Makefiles do. Handle both cases.\n"
+# Based on https://crascit.com/2016/04/09/using-ccache-with-cmake/
+cmake_minimum_required(VERSION 3.4)
+
+find_program(CCACHE_PROGRAM ccache)
+if(CCACHE_PROGRAM)
+    # Set up wrapper scripts
+    set(C_LAUNCHER   "${CCACHE_PROGRAM}")
+    set(CXX_LAUNCHER "${CCACHE_PROGRAM}")
+    set(CUDA_LAUNCHER "${CCACHE_PROGRAM}")
+
+    file(WRITE "${CMAKE_BINARY_DIR}/launch-c" ""
+        "#!/usr/bin/env sh\n"
+        "\n"
+        "# Xcode generator doesn't include the compiler as the\n"
+        "# first argument, Ninja and Makefiles do. Handle both cases.\n"
+        "if [ \"$1\" = \"${CMAKE_C_COMPILER}\" ] ; then\n"
+        "    shift\n"
+        "fi\n"
+        "\n"
+        "export CCACHE_CPP2=true\n"
+        "exec \"${C_LAUNCHER}\" \"${CMAKE_C_COMPILER}\" \"$@\"\n"
+        )
+
+    file(WRITE "${CMAKE_BINARY_DIR}/launch-cxx" ""
+        "#!/usr/bin/env sh\n"
+        "\n"
+        "# Xcode generator doesn't include the compiler as the\n"
+        "# first argument, Ninja and Makefiles do. Handle both cases.\n"
+        "if [ \"$1\" = \"${CMAKE_CXX_COMPILER}\" ] ; then\n"
+        "    shift\n"
+        "fi\n"
+        "\n"
+        "export CCACHE_CPP2=true\n"
+        "exec \"${CXX_LAUNCHER}\" \"${CMAKE_CXX_COMPILER}\" \"$@\"\n"
+        )
+
+    # Cuda support only added in CMake 3.10
+    file(WRITE "${CMAKE_BINARY_DIR}/launch-cuda" ""
+        "#!/usr/bin/env sh\n"
+        "\n"
+        "# Xcode generator doesn't include the compiler as the\n"
+        "# first argument, Ninja and Makefiles do. Handle both cases.\n"
         "if [ \"$1\" = \"${CMAKE_CUDA_COMPILER}\" ] ; then\n"
         "    shift\n"
         "fi\n"
