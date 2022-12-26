70d30fcd7d11a05695993c0a8d1fde9202f05abc
Mathieu Pellerin <nirvn.asia@gmail.com>
2022-12-26 13:03:54 +0700

    Add udp type to the nmea server

--- test/nmea_server/nmeaserver.py
+++ test/nmea_server/nmeaserver.py
@@ -12,13 +12,21 @@ desc = """\
         nmeaserver.py
         --------------------------------
             This application loads a nmea log file and streams it
-            via a tcp server
+            via a tcp or udp server
         """
 parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter, description=desc
 )
 
 parser.add_argument("logfile", help="Path to the NMEA log file", type=pathlib.Path)
+parser.add_argument(
+    "-t",
+    "--type",
+    help="Server type to operate on",
+    choices=["tcp", "udp"],
+    default="tcp",
+    type=str,
+)
 parser.add_argument("-p", "--port", help="Port to operate on", default=1958, type=int)
 parser.add_argument(
     "-i", "--interval", help="Pause between sending sentences", default=0.1, type=float
@@ -28,7 +36,6 @@ args = vars(parser.parse_args())
 
 host = ""  # localhost
 port = args["port"]
-PACKET_SIZE = 1024
 
 log = os.path.join(args["logfile"])
 with open(log, "rb") as f:
@@ -41,14 +48,31 @@ def work_thread(client):
         i = (i + 1) % len(lines)
         data = lines[i]
         client.sendall(data)
+        print(data)
         time.sleep(args["interval"])
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((host, port))
-    sock.listen(5)  # we should never have more than one client
-    print(f"Listening on port {port}")
+if args["type"] == "tcp":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((host, port))
+        sock.listen(5)  # we should never have more than one client
+        print(f"Listening on port {port}")
 
-    client, addr = sock.accept()  # your script will block here waiting for a connection
-    t = threading.Thread(target=work_thread, args=(client,))
-    t.start()
+        (
+            client,
+            addr,
+        ) = sock.accept()  # your script will block here waiting for a connection
+        t = threading.Thread(target=work_thread, args=(client,))
+        t.start()
+else:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.settimeout(0.2)
+    i = 0
+    while True:
+        i = (i + 1) % len(lines)
+        data = lines[i]
+        sock.sendto(data, ("<broadcast>", port))
+        print(data)
+        time.sleep(args["interval"])
