626f83b867f0169365ad9c5b7733c1a0b5951ad4
Mathieu Pellerin <nirvn.asia@gmail.com>
2023-02-20 16:03:25 +0700

    Add AudioRecorder class documentation

--- src/core/audiorecorder.h
+++ src/core/audiorecorder.h
@@ -33,13 +33,38 @@ class AudioRecorder : public QAudioRecorder
   public:
     explicit AudioRecorder( QObject *parent = nullptr );
 
+    /**
+     * Returns TRUE when audio is being recorded.
+     */
     bool recording() const;
+
+    /**
+     * Returns TRUE when audio level/amplitude is available during recording.
+     * \note this reflects whether a given platform supports QAudioProbe
+     * \see level()
+     */
     bool hasLevel() const;
+
+    /**
+     * Returns the current level (0.0 to 1.0) of the audio being recorded.
+     * \see hasLevel()
+     */
     double level() const;
 
   signals:
+    /**
+     * Emitted when the recording state has changed.
+     */
     void recordingChanged();
+
+    /**
+     * Emitted upon successfully setting up the audio probe used to return audio levels.
+     */
     void hasLevelChanged();
+
+    /**
+     * Emitted when the current audio level being recorded has changed.
+     */
     void levelChanged();
 
   private slots:
