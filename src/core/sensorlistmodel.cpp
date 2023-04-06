541c2c048d2e79dae4a747f50ab27a71f961e9bc
Mathieu Pellerin <nirvn.asia@gmail.com>
2023-04-06 10:14:38 +0700

    Fix crash on QField exit

--- src/core/sensorlistmodel.cpp
+++ src/core/sensorlistmodel.cpp
@@ -54,7 +54,7 @@ void SensorListModel::setProject( QgsProject *project )
   {
     mSensorModel->deleteLater();
   }
-  mSensorModel = new QgsSensorModel( mProject->sensorManager() );
+  mSensorModel = mProject ? new QgsSensorModel( mProject->sensorManager() ) : nullptr;
   setSourceModel( mSensorModel );
 }
 
