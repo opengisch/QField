d0aa351c5ea3284545a0e79a7c5b140f31698b8b
Matthias Kuhn <matthias@opengis.ch>
2023-05-07 17:26:09 +0000

    "Document" opengl linux installation

--- scripts/build-for-linux.sh
+++ scripts/build-for-linux.sh
@@ -5,6 +5,7 @@ QT_VERSION=6.5.0
 # pip install aqt
 # aqt install linux desktop ${QT_VERSION} ${QT_PREFIX} -m [...] <- check android.yaml
 # make sure flex and bison are installed (use `apt` or similar)
+# sudo apt install mesa-common-dev libgl1-mesa-dev (makes QtGui/OpenGL happy)
 
 export Qt6_DIR=${QT_PREFIX}/${QT_VERSION}
 cmake -S . -B $(git rev-parse --show-toplevel)/build-x64-linux -DSYSTEM_QT=ON -DBUILD_WITH_QT6=ON -GNinja -DWITH_VCPKG=ON -DQt6_ROOT=${Qt6_DIR}
