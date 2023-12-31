c65f9288c4c610cdc1b72c4e0a81481e95534dcb
Mathieu Pellerin <nirvn.asia@gmail.com>
2023-12-31 18:01:44 +0700

    Cleanup android triplets

--- vcpkg/triplets/arm64-android.cmake
+++ vcpkg/triplets/arm64-android.cmake
@@ -1,52 +1,13 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
-# set(DYNAMIC_PORTS
-#   "zlib"
-#   "egl-registry"
-#   "bzip2"
-#   "pthreads"
-#   "opengl-registry"
-#   "libpng"
-#   "gettext-libintl"
-#   "brotli"
-#   "getopt"
-#   "expat"
-#   "pthread"
-#   "openssl"
-#   "opengl"
-#   "lz4"
-#   "libuuid"
-#   "freetype"
-#   "dirent"
-#   "libiconv"
-#   "gettext"
-#   "gperf"
-#   "double-conversion"
-#   "egl"
-#   "sqlite3"
-#   "zstd"
-#   "pcre2"
-#   "fontconfig"
-#   "libpq"
-#   "libjpeg-turbo"
-#   "libb2"
-#   "icu"
-#   "harfbuzz"
-# )
 
-include("${CMAKE_CURRENT_LIST_DIR}/stat.cmake")
-message(STATUS "Building static: ${STATIC_PORTS}")
-# set(STATIC_PORTS
-#     catch2
-#     libiconv
-#     freexl
-#     libspatialite
-# )
+include("${CMAKE_CURRENT_LIST_DIR}/static-ports-android.cmake")
 if (PORT IN_LIST STATIC_PORTS)
     set(VCPKG_LIBRARY_LINKAGE static)
 else()
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
+
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
 set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
--- vcpkg/triplets/x64-android.cmake
+++ vcpkg/triplets/x64-android.cmake
@@ -1,9 +1,13 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
-if(PORT MATCHES "^qt")
+
+include("${CMAKE_CURRENT_LIST_DIR}/static-ports-android.cmake")
+if (PORT IN_LIST STATIC_PORTS)
+    set(VCPKG_LIBRARY_LINKAGE static)
+else()
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
+
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
 set(VCPKG_MAKE_BUILD_TRIPLET "--host=x86_64-linux-android")
