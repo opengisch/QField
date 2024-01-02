3e5dde98e55b8f4ba5eb0a25b5f373d81ac6b2ac
Mathieu Pellerin <nirvn.asia@gmail.com>
2024-01-02 14:21:06 +0700

    Switch linux (local + github workflow) to building Qt

--- scripts/build-for-linux.sh
+++ scripts/build-for-linux.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-QT_PREFIX=${HOME}/.qt
-QT_VERSION=6.6.1
 
-set -e
-
-# pip install --user aqtinstall
-# $HOME/.local/bin/aqt install-qt linux desktop ${QT_VERSION} gcc_64 -O ${QT_PREFIX} -m qt5compat qtcharts qtpositioning qtserialport qtconnectivity qtmultimedia qtwebview qtsensors # Modules come from .github/workflows/android.yaml
-# make sure flex, curl and bison are installed (use `apt` or similar)
-# sudo apt install mesa-common-dev libgl1-mesa-dev (makes QtGui/OpenGL happy)
-
-export Qt6_DIR=${QT_PREFIX}/${QT_VERSION}/gcc_64
-cmake -S . -B $(git rev-parse --show-toplevel)/build-x64-linux -DSYSTEM_QT=ON -GNinja -DWITH_VCPKG=ON -DQt6_ROOT=${Qt6_DIR} -DENABLE_TESTS=ON
+cmake -S . -B $(git rev-parse --show-toplevel)/build-x64-linux -GNinja -DWITH_VCPKG=ON -DENABLE_TESTS=ON
 cmake --build $(git rev-parse --show-toplevel)/build-x64-linux
