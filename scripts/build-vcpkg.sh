72459ddb24313141502e16ef4ad93f8233e00fac
Mathieu Pellerin <nirvn.asia@gmail.com>
2024-01-02 13:33:58 +0700

    Fix camera crash, take X
    * Build Qt for android
    
    * Add qtbase overlay
    
    * Enable egl support for qtbase/android
    
    * Install required deps
    
    * Diable default feature egl for linux
    
    * We only need qttools on host
    
    * Working local docker Android builds with self-built Qt
    
    * Implicitly set QT_HOST_PATH
    
    * Fix androiddeployqt path discovery
    
    * openssl dynamic
    
    * First vcpkg-enabled Qt patch: android camera
    
    * Bye aqtinstall
    
    * Remove unneeded cmake definition
    
    * Only set paths after find_package(Qt)
    
    * cleanup
    
    * Build openssl statically
    
    * Build for android on 22.04
    
    * Reclaim disk space
    
    * Retire x86 android builds
    
    * Add missing image formats (needed for WMS tiff rendering), add qtmultimedia ffmpeg plugin
    
    * Add required components to build qtmultimedia ffmpeg plugin, adjust overlay, enable for linux only
    
    * More dynamic test
    
    * more hacking
    
    * we do not need android openssl
    
    * temporarily suppress some error
    
    * Reduce qttools host footprint
    
    * Fix exiv2 target
    
    * Opt out of hdf5
    
    * Reduce qgis portfile contents
    
    * Portfile update
    
    * Do not crash qtmultimedia for no reason
    
    * qgis is still static
    
    * Enable ffmpeg plugin for android
    
    * Static build for qgis and ffmpeg
    
    * Cleanup android triplets
    
    * Update .github/workflows/android.yml
    
    Co-authored-by: Matthias Kuhn <matthias@opengis.ch>
    
    * Build qca in static mode
    
    * Fix QGIS build on linux/windows/macos
    
    * Bump qca to latest master to allow building without openssl legacy provider
    
    * Disable qtbase[icu]
    
    * Remove more qtbase deps
    
    * Remove unused dir
    
    * Restore x86 android triplet
    
    * Cleanup
    
    ---------
    
    Co-authored-by: Matthias Kuhn <matthias@opengis.ch>

--- scripts/build-vcpkg.sh
+++ scripts/build-vcpkg.sh
@@ -6,8 +6,6 @@ export SOURCE_DIR=/usr/src/qfield
 
 CMAKE_BUILD_DIR=/usr/src/qfield/build-${triplet}
 
-export Qt6_DIR=/home/devel/${install_qt_version}/${install_qt_arch}
-
 [[ -z ${APP_NAME} ]] && APP_NAME="QField Home"
 [[ -z ${APP_PACKAGE_NAME} ]] && APP_PACKAGE_NAME="qfield_home"
 [[ -z ${APP_ICON} ]] && APP_ICON="qfield_logo_beta"
@@ -18,11 +16,8 @@ echo "Package name ${APP_PACKAGE_NAME}"
 cmake -S "${SOURCE_DIR}" \
 	-B "${CMAKE_BUILD_DIR}" \
 	-G Ninja \
-	-D CMAKE_PREFIX_PATH=${Qt6_DIR} \
-	-D QT_HOST_PATH=/home/devel/${install_qt_version}/gcc_64 \
-	-D QT_HOST_PATH_CMAKE_DIR=/home/devel/${install_qt_version}/gcc_64/lib/cmake \
 	-D VCPKG_TARGET_TRIPLET="${triplet}" \
-	-D SYSTEM_QT=ON \
+	-D SYSTEM_QT=OFF \
 	-D ANDROID_NDK_VERSION="${ANDROID_NDK_VERSION}" \
 	-D ANDROID_SDK=/opt/android-sdk-linux \
 	-D ANDROID_SDK_ROOT=/opt/android-sdk-linux \
---
+++ vcpkg/triplets/static-ports-android.cmake
@@ -0,0 +1,14 @@
+set(STATIC_PORTS
+    gdal
+    catch2
+    libiconv
+    freexl
+    libspatialite
+    qhull
+    harfbuzz
+    libb2
+    freeglut
+    qca-qt6 # OSSL plugin not discovered unless static
+    qgis-qt6
+    ffmpeg
+)
