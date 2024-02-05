cd9e2d0a8e66cc4bcb32a98a01478d402e9eb001
Matthias Kuhn <matthias@opengis.ch>
2024-02-05 17:46:19 +0100

    Set min osx version to 10.15

--- vcpkg/triplets/arm64-osx.cmake
+++ vcpkg/triplets/arm64-osx.cmake
@@ -5,3 +5,7 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES arm64)
 set(VCPKG_BUILD_TYPE release)
+
+set(VCPKG_OSX_DEPLOYMENT_TARGET 10.15)
+set(VCPKG_C_FLAGS -mmacosx-version-min=10.15)
+set(VCPKG_CXX_FLAGS -mmacosx-version-min=10.15)
