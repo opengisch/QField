5f15bd22e8edd1e58e5b022708bf7e4da6d1d3fb
Mathieu Pellerin <nirvn.asia@gmail.com>
2024-06-19 14:33:49 +0700

    Fix extra empty item added to the value map combobox

--- src/core/valuemapmodelbase.cpp
+++ src/core/valuemapmodelbase.cpp
@@ -33,11 +33,11 @@ QVariant ValueMapModelBase::map() const
 void ValueMapModelBase::setMap( const QVariant &map )
 {
   mMap.clear();
-  // QGIS 3
+
   const QVariantList list = map.toList();
-  if ( !list.empty() )
+  if ( !list.empty() ) // QGIS 3
   {
-    beginInsertRows( QModelIndex(), 0, list.size() );
+    beginInsertRows( QModelIndex(), 0, list.size() - 1 );
 
     for ( const QVariant &item : list )
     {
@@ -55,7 +55,7 @@ void ValueMapModelBase::setMap( const QVariant &map )
     const QVariantMap valueMap = map.toMap();
     if ( !valueMap.empty() )
     {
-      beginInsertRows( QModelIndex(), 0, valueMap.size() );
+      beginInsertRows( QModelIndex(), 0, valueMap.size() - 1 );
 
       QMapIterator<QString, QVariant> i( valueMap );
       while ( i.hasNext() )
