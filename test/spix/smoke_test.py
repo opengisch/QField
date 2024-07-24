db3a72e928c947490c642ac5fa6547e2e87eac47
mohsen <36326627+mohsenD98@users.noreply.github.com>
2024-07-24 06:07:02 +0330

    Update smoke_test.py
    Update object id.

--- test/spix/smoke_test.py
+++ test/spix/smoke_test.py
@@ -110,7 +110,7 @@ def test_wms_layer(app, screenshot_path, screenshot_check, extra, process_alive)
     """
     assert app.existsAndVisible("mainWindow")
 
-    app.invokeMethod("mainWindow/QFieldGuideStartupTour", "blockGuide", [])
+    app.invokeMethod("mainWindow/QFieldGuideMapCanvasTour", "blockGuide", [])
 
     # Arbitrary wait period to insure project fully loaded and rendered
     time.sleep(4)
