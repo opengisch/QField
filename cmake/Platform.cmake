a73b01da513d421259c55c783311c40058516bdb
mohsen <36326627+mohsenD98@users.noreply.github.com>
2024-08-22 19:36:28 +0330

    Bump minimum Android version to 23 (Android 6)
    Minimum requirement imposed by Qt

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -22,7 +22,7 @@ endif()
 if(ANDROID_ABI)
   set(ANDROID_BUILD_TOOLS_VERSION "34.0.0" CACHE STRING "Android build-tools version")
   set(ANDROID_TARGET_PLATFORM 34 CACHE INT "Target Android platform SDK version")
-  set(ANDROID_PLATFORM 21 CACHE INT "Minimum Android platform SDK version")
-  set(ANDROID_PLATFORM_INT 21 CACHE INT "Minimum Android platform SDK version") # Used in build.gradle.in
+  set(ANDROID_PLATFORM 23 CACHE INT "Minimum Android platform SDK version")
+  set(ANDROID_PLATFORM_INT 23 CACHE INT "Minimum Android platform SDK version") # Used in build.gradle.in
   set(ANDROID_CPP_FEATURES "rtti exceptions")
 endif()
