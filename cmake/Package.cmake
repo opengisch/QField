ec2100e23793818183acbae921641ade1bad41df
Mathieu Pellerin <nirvn.asia@gmail.com>
2024-09-07 14:27:37 +0700

    Add file association for .qgs/.qgz on Windows

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -35,6 +35,7 @@ if(WIN32)
     message(STATUS "   + NSIS                             YES ")
     set(CPACK_NSIS_EXECUTABLES_DIRECTORY "usr\\\\bin")
     set(CPACK_NSIS_DISPLAY_NAME "QField")
+
     list(APPEND CPACK_GENERATOR "NSIS")
 endif()
 
