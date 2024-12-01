1e3a81d6a95636a52258653ac1a2f7da130d5ee1
Mathieu Pellerin <nirvn.asia@gmail.com>
2024-12-01 11:33:05 +0700

    Rework handling of pg_service.conf, rely on a newly created pg_service.conf in the app system directory, support SSL keys authentication

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -201,6 +201,60 @@ int main( int argc, char **argv )
     delete[] newPaths;
   }
 
+#if defined( Q_OS_ANDROID ) || defined( Q_OS_IOS )
+  for ( const QString &dataDir : dataDirs )
+  {
+    const QFileInfo pgServiceFileInfo( QStringLiteral( "%1/pg_service.conf" ).arg( dataDir ) );
+    if ( pgServiceFileInfo.exists() && pgServiceFileInfo.isReadable() )
+    {
+      const QString systemLocalDataPath = platformUtils->systemLocalDataLocation( QString() );
+
+      QFile pgServiceFile( QStringLiteral( "%1/pg_service.conf" ).arg( dataDir ) );
+      pgServiceFile.open( QFile::ReadOnly | QFile::Text );
+      QTextStream textStream( &pgServiceFile );
+      QString psServiceFileContent = textStream.readAll();
+      pgServiceFile.close();
+
+      const QStringList keys = QStringList() << QStringLiteral( "sslrootcert" ) << QStringLiteral( "sslcert" ) << QStringLiteral( "sslkey" );
+      for ( const QString &key : keys )
+      {
+        const QRegularExpression rx( QStringLiteral( "%1=(.*)" ).arg( key ) );
+        QRegularExpressionMatchIterator matchIt = rx.globalMatch( psServiceFileContent );
+        while ( matchIt.hasNext() )
+        {
+          const QRegularExpressionMatch match = matchIt.next();
+          const QString fileName = match.captured( 1 ).trimmed();
+
+          // Check if the file is relative to the pg_service.conf, in which case copy to user-owned location, use absolute path, and change permissions
+          const QString filePath = QStringLiteral( "%1/%2" ).arg( dataDir, fileName );
+          const QFileInfo fileInfo( filePath );
+          if ( QFileInfo::exists( filePath ) )
+          {
+            const QString newFilePath = QStringLiteral( "%1/%2" ).arg( systemLocalDataPath, fileName );
+            if ( QFileInfo::exists( newFilePath ) )
+            {
+              QFile newFile( newFilePath );
+              newFile.remove();
+            }
+            QFile::copy( filePath, newFilePath );
+            QFile::setPermissions( newFilePath, QFileDevice::ReadOwner | QFileDevice::WriteOwner );
+            psServiceFileContent.replace( QStringLiteral( "%1=%2" ).arg( key, match.captured( 1 ) ), QStringLiteral( "%1=%2" ).arg( key, newFilePath ) );
+          }
+        }
+      }
+
+      const QString localPgServiceFileName = QStringLiteral( "%1/pg_service.conf" ).arg( systemLocalDataPath );
+      QFile localPgServiceFile( localPgServiceFileName );
+      localPgServiceFile.open( QFile::WriteOnly );
+      localPgServiceFile.write( psServiceFileContent.toUtf8() );
+      localPgServiceFile.close();
+
+      setenv( "PGSYSCONFDIR", systemLocalDataPath.toUtf8(), true );
+      break;
+    }
+  }
+#endif
+
 #if WITH_SENTRY
   sentry_wrapper::install_message_handler();
 #endif
