08c74ea540b24f3d2b1f7c73e13c989a04826105
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-03-12 15:43:11 +0700

    Backport crucial Qt 6.8.3 fixes to our overlay Qt ports

--- vcpkg/ports/qtbase/portfile.cmake
+++ vcpkg/ports/qtbase/portfile.cmake
@@ -23,6 +23,8 @@ set(${PORT}_PATCHES
         fix_deploy_windows.patch
         fix-link-lib-discovery.patch
         macdeployqt-symlinks.patch
+        bluetooth_mouse_fix.patch
+        android_cursor_location.patch
 )
  
 if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
