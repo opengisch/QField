6aeffa176038b20257be8c16b3dd44bc5d68c24e
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-03-13 09:54:27 +0700

    Sort app-wide plugins list in the plugin manager

--- src/core/pluginmanager.cpp
+++ src/core/pluginmanager.cpp
@@ -272,7 +272,11 @@ void PluginManager::refreshAppPlugins()
 
 QList<PluginInformation> PluginManager::availableAppPlugins() const
 {
-  return mAvailableAppPlugins.values();
+  QList<PluginInformation> plugins = mAvailableAppPlugins.values();
+  std::sort( plugins.begin(), plugins.end(), []( const PluginInformation &plugin1, const PluginInformation &plugin2 ) {
+    return plugin1.name().toLower() < plugin2.name().toLower();
+  } );
+  return std::move( plugins );
 }
 
 void PluginManager::enableAppPlugin( const QString &uuid )
