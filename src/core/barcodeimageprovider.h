61f231a710b7e641f748fcbfb138c4a60695bdd0
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-04-27 13:16:41 +0700

    Add an image provider dedicated to generating QR codes

---
+++ src/core/barcodeimageprovider.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+  barcodeimageprovider.h - BarcodeImageProvider
+
+ ---------------------
+ begin                : 26.04.2025
+ copyright            : (C) 2025 by Mathieu Pellerin
+ email                : mathieu at opengis dot ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef BARCODEIMAGEPROVIDER_H
+#define BARCODEIMAGEPROVIDER_H
+
+#include <QQuickImageProvider>
+
+/**
+ * \ingroup core
+ */
+class BarcodeImageProvider : public QQuickImageProvider
+{
+  public:
+    explicit BarcodeImageProvider();
+
+    QQmlImageProviderBase::ImageType imageType() const override { return QQmlImageProviderBase::Image; }
+    QImage requestImage( const QString &id, QSize *size, const QSize &requestedSize ) override;
+};
+
+#endif // BARCODEIMAGEPROVIDER_H
