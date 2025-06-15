c0d85d1a4b318202353ad6c54548bae4d0a10416
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-06-15 14:55:00 +0700

    Force build windows

--- vcpkg/ports/crashpad/portfile.cmake
+++ vcpkg/ports/crashpad/portfile.cmake
@@ -5,7 +5,6 @@ vcpkg_from_git(
     URL https://chromium.googlesource.com/crashpad/crashpad
     REF 7e0af1d4d45b526f01677e74a56f4a951b70517d
     PATCHES
-        fix-linux.patch
         fix-lib-name-conflict.patch
         cast_void.patch
 )
