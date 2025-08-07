8b5d340ae3c2a246bed475cedb534de5027663ba
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-08-07 17:04:19 +0700

    Fix undo/redo of feature deletion against vector layer containing joins

--- src/core/featurehistory.h
+++ src/core/featurehistory.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+                        featurehistory.h
+                        ----------------
+  begin                : Dec 2023
+  copyright            : (C) 2023 by Ivan Ivanov
+  email                : ivan@opengis.ch
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef FEATUREHISTORY_H
 #define FEATUREHISTORY_H
 
@@ -5,7 +22,7 @@
 #include <QTimer>
 #include <qgsproject.h>
 
-#include <trackingmodel.h>
+class TrackingModel;
 
 typedef QPair<QgsFeature, QgsFeature> OldNewFeaturePair;
 
