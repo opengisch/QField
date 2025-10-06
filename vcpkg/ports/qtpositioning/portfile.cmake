d1dfa0b65836ae4a93bd074542d356ea91c1e494
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-10-06 07:40:26 +0700

    Update vcpkg to catch Qt 6.9.1 in preparation for 6.9.2 (#6687)
    * Update vcpkg to catch Qt 6.9.1 in preparation for 6.9.2
    
    * Add exiv2 port with a fixed dependencies patch
    
    * Add poppler to list of ports built statically
    
    * Add libspatialite overlay to fix libspatialite on ios

--- vcpkg/ports/qtpositioning/portfile.cmake
+++ vcpkg/ports/qtpositioning/portfile.cmake
@@ -18,8 +18,6 @@ list(APPEND FEATURE_OPTIONS "-DCMAKE_DISABLE_FIND_PACKAGE_Gypsy=ON"
                             "-DCMAKE_DISABLE_FIND_PACKAGE_Gconf=ON"
 )
 
-
-
 qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      CONFIGURE_OPTIONS ${FEATURE_OPTIONS}
                      CONFIGURE_OPTIONS_RELEASE
