f24bf9d1897a1675b342f0cfc734c05638508173
Matthias Kuhn <matthias@opengis.ch>
2025-10-25 23:02:36 +0200

    Add static condition

--- cmake/qgis-cmake-wrapper.cmake
+++ cmake/qgis-cmake-wrapper.cmake
@@ -63,7 +63,7 @@ endfunction()
 #  set(CMAKE_USE_PTHREADS_INIT 1)
 #endif()
 
-if(TRUE) # Should possibly have a "static only" check
+if(VCPKG_TARGET_TRIPLET MATCHES "-static")
   find_package(PkgConfig QUIET)
 
   _find_and_link_library(authmethod_basic_a QGIS::Core)
