6cd23ec0212e50d54fd2208b2a73a09d2286526e
Matthias Kuhn <matthias@opengis.ch>
2025-10-27 21:03:39 +0100

    A mixed bag of static/dynamic for windows

--- vcpkg/triplets/x64-windows.cmake
+++ vcpkg/triplets/x64-windows.cmake
@@ -1,5 +1,12 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+include("${CMAKE_CURRENT_LIST_DIR}/static-ports.cmake")
+
+if (PORT IN_LIST STATIC_PORTS)
+    set(VCPKG_LIBRARY_LINKAGE static)
+else()
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
 
 set(VCPKG_BUILD_TYPE release)
