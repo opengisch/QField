06869fbc0fb3b20c0e6868e4a92f4ade450ffbde
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-10-31 11:16:27 +0700

    Add Qt 6.9.2 overlay ports

--- vcpkg/ports/qtpositioning/portfile.cmake
+++ vcpkg/ports/qtpositioning/portfile.cmake
@@ -4,7 +4,6 @@ include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 set(${PORT}_PATCHES
     devendor-poly2tri.patch
     foregroundservice.patch
-    androidaltitude.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
