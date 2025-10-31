36b0de19b2b9725905781b509e89061644be319a
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-10-31 11:29:52 +0700

    Update Qt 6.9.2 to 6.9.3, add missing qtwebsocket port

--- vcpkg/ports/qttools/port.data.cmake
+++ vcpkg/ports/qttools/port.data.cmake
@@ -1,3 +1,3 @@
-set(qttools_HASH "95f2e666accbb474d2a7bb0ac88b4fb006e14a419301272fdb1f168b79e9437cad224b678298a355faba243853e9314c5f2b5e5192e56f4772a373e41d74dce0")
-set(qttools_URL "https://download.qt.io/archive/qt/6.9/6.9.2/submodules/qttools-everywhere-src-6.9.2.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.9/6.9.2/submodules/qttools-everywhere-src-6.9.2.tar.xz")
-set(qttools_FILENAME "qttools-everywhere-src-6.9.2.tar.xz")
+set(qttools_HASH "6e94ef96865c668c139b71583aaf40db4137824708663b59ad49a42c9c74a4ad4f36b228909b31a4434be69ae3e693502f2dbee1593dd9891a109e8e665253f0")
+set(qttools_URL "https://download.qt.io/archive/qt/6.9/6.9.3/submodules/qttools-everywhere-src-6.9.3.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.9/6.9.3/submodules/qttools-everywhere-src-6.9.3.tar.xz")
+set(qttools_FILENAME "qttools-everywhere-src-6.9.3.tar.xz")
