53f0b88ca1ba21754388dd38991520064d58f68a
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-10-31 14:25:58 +0700

    Fix build

--- vcpkg/ports/qttools/port.data.cmake
+++ vcpkg/ports/qttools/port.data.cmake
@@ -1,3 +1,3 @@
-set(qttools_HASH "6e94ef96865c668c139b71583aaf40db4137824708663b59ad49a42c9c74a4ad4f36b228909b31a4434be69ae3e693502f2dbee1593dd9891a109e8e665253f0")
+set(qttools_HASH "529aae86f9d4ba52b6ec1f72d19c611ce17c8e6644974cb1f202a5eef54f83167b072dfae9d94d29f0ab2269ba97a99bd7744b573b3dbd1aa2e40a55729242c0")
 set(qttools_URL "https://download.qt.io/archive/qt/6.9/6.9.3/submodules/qttools-everywhere-src-6.9.3.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.9/6.9.3/submodules/qttools-everywhere-src-6.9.3.tar.xz")
 set(qttools_FILENAME "qttools-everywhere-src-6.9.3.tar.xz")
