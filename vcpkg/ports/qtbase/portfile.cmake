4d2df61d7151cf4ec5f397532ca53adad72c147f
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-11-12 08:43:15 +0700

    Add patch to fix Android virtual keyboard

--- vcpkg/ports/qtbase/portfile.cmake
+++ vcpkg/ports/qtbase/portfile.cmake
@@ -27,6 +27,7 @@ set(${PORT}_PATCHES
         moltenvk.patch
         xcodebuild-not-installed.patch
         fix-libresolv-test.patch
+        android_keyboard_fix.patch
 )
  
 if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
