d66578dde92482be77ecbb3795f86d5b75495e50
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-12-05 19:07:49 +0700

    Use absolute path to Qt6 lupdate

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -4,7 +4,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 SOURCE_DIR=${DIR}/../..
 source ${SOURCE_DIR}/scripts/version_number.sh
 
-lupdate -recursive ${SOURCE_DIR} -ts ${SOURCE_DIR}/i18n/qfield_en.ts
+/usr/lib/qt6/bin/lupdate -recursive ${SOURCE_DIR} -ts ${SOURCE_DIR}/i18n/qfield_en.ts
 
 # release only if the branch is master
 if [[ ${CI_BRANCH} = master ]]; then
