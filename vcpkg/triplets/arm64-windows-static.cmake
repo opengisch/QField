9ab33764d5f513f6c7b5d48f9e24a02f36a083bf
Matthias Kuhn <matthias@opengis.ch>
2026-01-06 07:48:43 +0100

    Arm64 windows

---
+++ vcpkg/triplets/arm64-windows-static.cmake
@@ -0,0 +1,6 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
+# set(VCPKG_PLATFORM_TOOLSET_VERSION "14.38") # Can be removed in a future update; aligned with .github/workflows/windows.yml : ilammy/msvc-dev-cmd/toolset
+
+set(VCPKG_BUILD_TYPE release)
