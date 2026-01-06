e47c8385e0f81614c2504c9ad0a6517134187fbb
Matthias Kuhn <matthias@opengis.ch>
2026-01-06 07:14:08 +0100

    Add arm64-linux triplet configuration

---
+++ vcpkg/triplets/arm64-linux.cmake
@@ -0,0 +1,15 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+
+include("${CMAKE_CURRENT_LIST_DIR}/static-ports.cmake")
+
+if (PORT IN_LIST STATIC_PORTS)
+    set(VCPKG_LIBRARY_LINKAGE static)
+else()
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+set(VCPKG_BUILD_TYPE release)
+
+set(VCPKG_FIXUP_ELF_RPATH ON)
