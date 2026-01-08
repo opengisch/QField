c9f4e46b690d128bd32b815d1077b07d3ad74c25
Mathieu Pellerin <nirvn.asia@gmail.com>
2026-01-08 14:55:05 +0700

    Shot in the dark to fix reported freezes on Android

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -113,6 +113,8 @@ void initAuthManager( QgsAuthManager *authManager )
 
 int main( int argc, char **argv )
 {
+  qputenv( "QT_ANDROID_DISABLE_ACCESSIBILITY", "1" );
+
   QCoreApplication::setOrganizationName( "OPENGIS.ch" );
   QCoreApplication::setOrganizationDomain( "opengis.ch" );
   QCoreApplication::setApplicationName( qfield::appName );
