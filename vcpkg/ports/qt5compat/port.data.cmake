d98d62437558a767abd28d09c56537013e0145b6
Mathieu Pellerin <nirvn.asia@gmail.com>
2026-04-03 10:20:34 +0700

    Upgrade used Qt ports to 6.10.3

--- vcpkg/ports/qt5compat/port.data.cmake
+++ vcpkg/ports/qt5compat/port.data.cmake
@@ -1,3 +1,3 @@
-set(qt5compat_HASH "c55a8f2fe3fba9bec291f5278f6e6b43bf1cfa724737a48049f222f84f420a233bda5f392019846d9160fefbbe063598a10ec14b0fe58d08b3a05916696159b2")
-set(qt5compat_URL "https://download.qt.io/archive/qt/6.10/6.10.2/submodules/qt5compat-everywhere-src-6.10.2.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.10/6.10.2/submodules/qt5compat-everywhere-src-6.10.2.tar.xz")
-set(qt5compat_FILENAME "qt5compat-everywhere-src-6.10.2.tar.xz")
+set(qt5compat_HASH "1ec608ba901666e6e3ed83b895694c29e1a996b4c0a146b685ccfb31ca23a71326d061d50be23fbfb006026a059a3668bd1f28b6410cd97c66ccd7d752e67c17")
+set(qt5compat_URL "https://download.qt.io/archive/qt/6.10/6.10.3/submodules/qt5compat-everywhere-src-6.10.3.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.10/6.10.3/submodules/qt5compat-everywhere-src-6.10.3.tar.xz")
+set(qt5compat_FILENAME "qt5compat-everywhere-src-6.10.3.tar.xz")
