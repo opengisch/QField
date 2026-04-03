9c3275e46c32afeecdcfc4d11ed2a2cd6d6957ea
Mathieu Pellerin <nirvn.asia@gmail.com>
2026-04-03 14:12:16 +0700

    Fix qt5compat sha512 sum

--- vcpkg/ports/qt5compat/port.data.cmake
+++ vcpkg/ports/qt5compat/port.data.cmake
@@ -1,3 +1,3 @@
-set(qt5compat_HASH "1ec608ba901666e6e3ed83b895694c29e1a996b4c0a146b685ccfb31ca23a71326d061d50be23fbfb006026a059a3668bd1f28b6410cd97c66ccd7d752e67c17")
+set(qt5compat_HASH "a690b570b3c22cad4e05dedef2c6a3c5a59d0c720319998b07ed13542573a1145472fd043c355079a07367a10a3b90ac69b51e558aa3000072e442658af3acf2")
 set(qt5compat_URL "https://download.qt.io/archive/qt/6.10/6.10.3/submodules/qt5compat-everywhere-src-6.10.3.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.10/6.10.3/submodules/qt5compat-everywhere-src-6.10.3.tar.xz")
 set(qt5compat_FILENAME "qt5compat-everywhere-src-6.10.3.tar.xz")
