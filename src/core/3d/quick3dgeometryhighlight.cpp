22f04cf7b4bfa5dcae5b7b1209a6f19c9fbc0fa2
Mohsen <mohsen_d98@yahoo.com>
2026-04-21 15:33:25 +0330

    Add Quick3DGeometryHighlight for point and line features on terrain.

---
+++ src/core/3d/quick3dgeometryhighlight.cpp
@@ -0,0 +1,317 @@
+/***************************************************************************
+  quick3dgeometryhighlight.cpp - Quick3DGeometryHighlight
+
+ ---------------------
+ begin                : 20.4.2026
+ copyright            : (C) 2026 by Mohsen Dehghanzadeh
+ email                : mohsen@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "quick3dgeometryhighlight.h"
+#include "quick3dgeometryutils.h"
+
+#include <qgsgeometrycollection.h>
+#include <qgslinestring.h>
+#include <qgspoint.h>
+
+#include <algorithm>
+#include <cmath>
+
+Quick3DGeometryHighlight::Quick3DGeometryHighlight( QQuick3DObject *parent )
+  : QQuick3DGeometry( parent )
+{
+}
+
+void Quick3DGeometryHighlight::setQgsGeometry( const QgsGeometry &geometry )
+{
+  if ( mGeometry.equals( geometry ) )
+    return;
+
+  mGeometry = geometry;
+  mDirty = true;
+  emit qgsGeometryChanged();
+  updateGeometry();
+}
+
+void Quick3DGeometryHighlight::setCrs( const QgsCoordinateReferenceSystem &crs )
+{
+  if ( mCrs == crs )
+    return;
+
+  mCrs = crs;
+  if ( mTerrainProvider && mTerrainProvider->mapSettings() )
+  {
+    mTransform = QgsCoordinateTransform( mCrs,
+                                         mTerrainProvider->mapSettings()->mapSettings().destinationCrs(),
+                                         mTerrainProvider->mapSettings()->mapSettings().transformContext() );
+  }
+
+  mDirty = true;
+  emit crsChanged();
+  updateGeometry();
+}
+
+void Quick3DGeometryHighlight::setTerrainProvider( Quick3DTerrainProvider *provider )
+{
+  if ( mTerrainProvider == provider )
+    return;
+
+  if ( mTerrainProvider )
+    disconnect( mTerrainProvider, nullptr, this, nullptr );
+
+  mTerrainProvider = provider;
+
+  if ( mTerrainProvider )
+  {
+    connect( mTerrainProvider, &Quick3DTerrainProvider::extentChanged, this, &Quick3DGeometryHighlight::markDirtyAndUpdate );
+    connect( mTerrainProvider, &Quick3DTerrainProvider::normalizedDataChanged, this, &Quick3DGeometryHighlight::markDirtyAndUpdate );
+
+    if ( mTerrainProvider->mapSettings() && mCrs.isValid() )
+    {
+      mTransform = QgsCoordinateTransform( mCrs,
+                                           mTerrainProvider->mapSettings()->mapSettings().destinationCrs(),
+                                           mTerrainProvider->mapSettings()->mapSettings().transformContext() );
+    }
+  }
+
+  mDirty = true;
+  emit terrainProviderChanged();
+  updateGeometry();
+}
+
+void Quick3DGeometryHighlight::setLineWidth( float width )
+{
+  width = std::max( 0.1f, width );
+  if ( qFuzzyCompare( mLineWidth, width ) )
+    return;
+
+  mLineWidth = width;
+  mDirty = true;
+  emit lineWidthChanged();
+  updateGeometry();
+}
+
+void Quick3DGeometryHighlight::setColor( const QColor &color )
+{
+  if ( mColor == color )
+    return;
+
+  mColor = color;
+  mDirty = true;
+  emit colorChanged();
+  updateGeometry();
+}
+
+void Quick3DGeometryHighlight::setHeightOffset( float offset )
+{
+  if ( qFuzzyCompare( mHeightOffset, offset ) )
+    return;
+
+  mHeightOffset = offset;
+  mDirty = true;
+  emit heightOffsetChanged();
+  updateGeometry();
+}
+
+void Quick3DGeometryHighlight::markDirtyAndUpdate()
+{
+  mDirty = true;
+  updateGeometry();
+}
+
+QVector3D Quick3DGeometryHighlight::vertexTo3D( double x, double y ) const
+{
+  double tx = x;
+  double ty = y;
+
+  if ( mTransform.isValid() && !mTransform.isShortCircuited() )
+  {
+    try
+    {
+      double z = 0.0;
+      mTransform.transformInPlace( tx, ty, z );
+    }
+    catch ( const QgsCsException & )
+    {
+      return QVector3D( std::numeric_limits<float>::quiet_NaN(), 0, 0 );
+    }
+  }
+
+  return mTerrainProvider->geoTo3D( tx, ty, mHeightOffset );
+}
+
+QVector<QVector<QVector3D>> Quick3DGeometryHighlight::buildPaths( const QgsAbstractGeometry *geom ) const
+{
+  QVector<QVector<QVector3D>> result;
+  if ( !geom || !mTerrainProvider )
+    return result;
+
+  if ( const QgsLineString *ls = dynamic_cast<const QgsLineString *>( geom ) )
+  {
+    QVector<QVector3D> path;
+    path.reserve( ls->numPoints() );
+    for ( int i = 0; i < ls->numPoints(); ++i )
+    {
+      const QVector3D pos = vertexTo3D( ls->xAt( i ), ls->yAt( i ) );
+      if ( !std::isnan( pos.x() ) )
+        path.append( pos );
+    }
+    if ( path.size() > 1 )
+      result.append( std::move( path ) );
+  }
+  else if ( const QgsGeometryCollection *collection = dynamic_cast<const QgsGeometryCollection *>( geom ) )
+  {
+    for ( int i = 0; i < collection->numGeometries(); ++i )
+      result.append( buildPaths( collection->geometryN( i ) ) );
+  }
+
+  return result;
+}
+
+void Quick3DGeometryHighlight::updateGeometry()
+{
+  if ( !mDirty )
+    return;
+
+  auto resetGeometry = [this]() {
+    clear();
+    setPrimitiveType( QQuick3DGeometry::PrimitiveType::Triangles );
+    mDirty = false;
+    update();
+  };
+
+  if ( mGeometry.isNull() || !mTerrainProvider || mTerrainProvider->extent().isEmpty() )
+  {
+    resetGeometry();
+    return;
+  }
+
+  const QgsAbstractGeometry *abstractGeom = mGeometry.constGet();
+  if ( !abstractGeom )
+  {
+    resetGeometry();
+    return;
+  }
+
+  const Qgis::GeometryType geomType = mGeometry.type();
+
+  const int segments = 8;
+  const int sphereStacks = 6;
+  const int sphereSlices = 8;
+  const float sphereRadius = mLineWidth * 2.25f;
+  const int singleSphereVtxCount = Quick3DGeometryUtils::sphereVertexCount( sphereStacks, sphereSlices );
+  const int singleSphereIdxCount = Quick3DGeometryUtils::sphereIndexCount( sphereStacks, sphereSlices );
+
+  const float r = mColor.redF();
+  const float g = mColor.greenF();
+  const float b = mColor.blueF();
+  const float a = mColor.alphaF();
+
+  QVector3D minBound( std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max() );
+  QVector3D maxBound( std::numeric_limits<float>::lowest(), std::numeric_limits<float>::lowest(), std::numeric_limits<float>::lowest() );
+  const int stride = Quick3DGeometryUtils::VERTEX_STRIDE;
+
+  auto finalize = [this, stride]( QByteArray &vertexData, QByteArray &indexData, const QVector3D &lo, const QVector3D &hi ) {
+    clear();
+    setVertexData( vertexData );
+    setIndexData( indexData );
+    setStride( stride );
+    addAttribute( QQuick3DGeometry::Attribute::PositionSemantic, 0, QQuick3DGeometry::Attribute::F32Type );
+    addAttribute( QQuick3DGeometry::Attribute::NormalSemantic, 3 * sizeof( float ), QQuick3DGeometry::Attribute::F32Type );
+    addAttribute( QQuick3DGeometry::Attribute::ColorSemantic, 6 * sizeof( float ), QQuick3DGeometry::Attribute::F32Type );
+    addAttribute( QQuick3DGeometry::Attribute::IndexSemantic, 0, QQuick3DGeometry::Attribute::U32Type );
+    setPrimitiveType( QQuick3DGeometry::PrimitiveType::Triangles );
+    setBounds( lo, hi );
+    mDirty = false;
+    update();
+  };
+
+  if ( geomType == Qgis::GeometryType::Point )
+  {
+    QVector<QVector3D> points;
+    QgsVertexIterator vit = mGeometry.vertices();
+    while ( vit.hasNext() )
+    {
+      const QgsPoint pt = vit.next();
+      const QVector3D pos = vertexTo3D( pt.x(), pt.y() );
+      if ( !std::isnan( pos.x() ) )
+        points.append( pos );
+    }
+
+    if ( points.isEmpty() )
+    {
+      resetGeometry();
+      return;
+    }
+
+    const int totalVertexCount = points.size() * singleSphereVtxCount;
+    const int totalIndexCount = points.size() * singleSphereIdxCount;
+
+    QByteArray vertexData;
+    vertexData.resize( static_cast<qsizetype>( totalVertexCount ) * stride );
+    QByteArray indexData;
+    indexData.resize( static_cast<qsizetype>( totalIndexCount ) * sizeof( quint32 ) );
+
+    float *vptr = reinterpret_cast<float *>( vertexData.data() );
+    quint32 *iptr = reinterpret_cast<quint32 *>( indexData.data() );
+    quint32 vertexOffset = 0;
+
+    const float pointSphereRadius = mLineWidth * 3.5f;
+    for ( const QVector3D &center : points )
+      Quick3DGeometryUtils::generateSphere( center, pointSphereRadius, sphereStacks, sphereSlices, r, g, b, a, vptr, iptr, vertexOffset, minBound, maxBound );
+
+    finalize( vertexData, indexData, minBound, maxBound );
+    return;
+  }
+
+  const QVector<QVector<QVector3D>> paths = buildPaths( abstractGeom );
+  if ( paths.isEmpty() )
+  {
+    resetGeometry();
+    return;
+  }
+
+  int totalPoints = 0;
+  int totalConnections = 0;
+
+  for ( const QVector<QVector3D> &path : paths )
+  {
+    totalPoints += path.size();
+    totalConnections += path.size() - 1;
+  }
+
+  const int tubeVertexCount = totalPoints * segments;
+  const int tubeIndexCount = totalConnections * segments * 2 * 3;
+  const int sphereVertexTotal = totalPoints * singleSphereVtxCount;
+  const int sphereIndexTotal = totalPoints * singleSphereIdxCount;
+
+  const int totalVertexCount = tubeVertexCount + sphereVertexTotal;
+  const int totalIndexCount = tubeIndexCount + sphereIndexTotal;
+
+  QByteArray vertexData;
+  vertexData.resize( static_cast<qsizetype>( totalVertexCount ) * stride );
+  QByteArray indexData;
+  indexData.resize( static_cast<qsizetype>( totalIndexCount ) * sizeof( quint32 ) );
+
+  float *vptr = reinterpret_cast<float *>( vertexData.data() );
+  quint32 *iptr = reinterpret_cast<quint32 *>( indexData.data() );
+  quint32 vertexOffset = 0;
+
+  for ( const QVector<QVector3D> &path : paths )
+    Quick3DGeometryUtils::generateTube( path, segments, mLineWidth, r, g, b, a, vptr, iptr, vertexOffset, minBound, maxBound );
+
+  for ( const QVector<QVector3D> &path : paths )
+  {
+    for ( const QVector3D &center : path )
+      Quick3DGeometryUtils::generateSphere( center, sphereRadius, sphereStacks, sphereSlices, r, g, b, a, vptr, iptr, vertexOffset, minBound, maxBound );
+  }
+
+  finalize( vertexData, indexData, minBound, maxBound );
+}
---
+++ src/core/3d/quick3dgeometryhighlight.h
@@ -0,0 +1,109 @@
+/***************************************************************************
+  quick3dgeometryhighlight.h - Quick3DGeometryHighlight
+
+ ---------------------
+ begin                : 20.4.2026
+ copyright            : (C) 2026 by Mohsen Dehghanzadeh
+ email                : mohsen@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QUICK3DGEOMETRYHIGHLIGHT_H
+#define QUICK3DGEOMETRYHIGHLIGHT_H
+
+#include "quick3dterrainprovider.h"
+
+#include <QColor>
+#include <QQuick3DGeometry>
+#include <qgscoordinatereferencesystem.h>
+#include <qgscoordinatetransform.h>
+#include <qgsgeometry.h>
+
+/**
+ * Generates 3D highlight geometry for an identified or selected feature on
+ * top of the terrain mesh.
+ *
+ * Points become spheres and lines become extruded tubes with sphere joints.
+ * The geometry is reprojected from the source CRS to the map CRS before being
+ * draped on the terrain surface.
+ *
+ * \note QML Type: Quick3DGeometryHighlight
+ * \ingroup core
+ */
+class Quick3DGeometryHighlight : public QQuick3DGeometry
+{
+    Q_OBJECT
+    QML_ELEMENT
+
+    //! The geometry to highlight
+    Q_PROPERTY( QgsGeometry qgsGeometry READ qgsGeometry WRITE setQgsGeometry NOTIFY qgsGeometryChanged )
+    //! Coordinate reference system the geometry is expressed in
+    Q_PROPERTY( QgsCoordinateReferenceSystem crs READ crs WRITE setCrs NOTIFY crsChanged )
+    //! The terrain provider used for geo-to-3D coordinate conversion
+    Q_PROPERTY( Quick3DTerrainProvider *terrainProvider READ terrainProvider WRITE setTerrainProvider NOTIFY terrainProviderChanged )
+    //! Outline tube thickness in scene units
+    Q_PROPERTY( float lineWidth READ lineWidth WRITE setLineWidth NOTIFY lineWidthChanged )
+    //! Per-vertex color applied to the highlight mesh
+    Q_PROPERTY( QColor color READ color WRITE setColor NOTIFY colorChanged )
+    //! Extra vertical offset above the terrain surface
+    Q_PROPERTY( float heightOffset READ heightOffset WRITE setHeightOffset NOTIFY heightOffsetChanged )
+
+  public:
+    explicit Quick3DGeometryHighlight( QQuick3DObject *parent = nullptr );
+
+    QgsGeometry qgsGeometry() const { return mGeometry; }
+    void setQgsGeometry( const QgsGeometry &geometry );
+
+    QgsCoordinateReferenceSystem crs() const { return mCrs; }
+    void setCrs( const QgsCoordinateReferenceSystem &crs );
+
+    Quick3DTerrainProvider *terrainProvider() const { return mTerrainProvider; }
+    void setTerrainProvider( Quick3DTerrainProvider *provider );
+
+    float lineWidth() const { return mLineWidth; }
+    void setLineWidth( float width );
+
+    QColor color() const { return mColor; }
+    void setColor( const QColor &color );
+
+    float heightOffset() const { return mHeightOffset; }
+    void setHeightOffset( float offset );
+
+  signals:
+    void qgsGeometryChanged();
+    void crsChanged();
+    void terrainProviderChanged();
+    void lineWidthChanged();
+    void colorChanged();
+    void heightOffsetChanged();
+
+  private slots:
+    void markDirtyAndUpdate();
+
+  private:
+    void updateGeometry();
+
+    //! Projects a geometry vertex from layer CRS into 3D scene space
+    QVector3D vertexTo3D( double x, double y ) const;
+
+    //! Walks an abstract geometry and returns its lines as 3D paths
+    QVector<QVector<QVector3D>> buildPaths( const QgsAbstractGeometry *geom ) const;
+
+    QgsGeometry mGeometry;
+    QgsCoordinateReferenceSystem mCrs;
+    QgsCoordinateTransform mTransform;
+    Quick3DTerrainProvider *mTerrainProvider = nullptr;
+
+    float mLineWidth = 3.0f;
+    float mHeightOffset = 15.0f;
+    bool mDirty = true;
+    QColor mColor = QColor( 255, 255, 0 );
+};
+
+#endif // QUICK3DGEOMETRYHIGHLIGHT_H
