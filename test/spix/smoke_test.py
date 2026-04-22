11af95f78353e1c1f3ab0c8e266cf62326c56924
Kaustuv Pokharel <kastuvpokharel@gmail.com>
2026-04-21 20:22:57 -0400

    finishing tests with screenshot check asserts

--- test/spix/smoke_test.py
+++ test/spix/smoke_test.py
@@ -404,6 +404,7 @@ def test_gallery_editor(app, screenshot_path, screenshot_check, extra, process_a
     )
     assert process_alive()
     extra.append(extras.html('<img src="images/test_gallery_editor_grid.png"/>'))
+    assert screenshot_check("test_gallery_editor", "test_gallery_editor_grid", 0.025)
 
     # Click the sort button in the gallery editor header to reverse card order
     bounds = app.getBoundingBox(
@@ -421,6 +422,7 @@ def test_gallery_editor(app, screenshot_path, screenshot_check, extra, process_a
     )
     assert process_alive()
     extra.append(extras.html('<img src="images/test_gallery_editor_sorted.png"/>'))
+    assert screenshot_check("test_gallery_editor", "test_gallery_editor_sorted", 0.025)
 
     # Click sort again to restore original card order before tapping a specific card
     pyautogui.click(interval=0.5)
@@ -442,6 +444,9 @@ def test_gallery_editor(app, screenshot_path, screenshot_check, extra, process_a
     )
     assert process_alive()
     extra.append(extras.html('<img src="images/test_gallery_editor_child_form.png"/>'))
+    assert screenshot_check(
+        "test_gallery_editor", "test_gallery_editor_child_form", 0.025
+    )
 
     # Close the child feature form (X close button at top right of the form)
     bounds = app.getBoundingBox("mainWindow/featureForm")
@@ -479,6 +484,7 @@ def test_gallery_editor(app, screenshot_path, screenshot_check, extra, process_a
     )
     assert process_alive()
     extra.append(extras.html('<img src="images/test_gallery_editor_list.png"/>'))
+    assert screenshot_check("test_gallery_editor", "test_gallery_editor_list", 0.025)
 
 
 if __name__ == "__main__":
