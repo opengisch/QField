ab71a8022483521f35ab4f247c60aa110eab3aaa
Mathieu Pellerin <nirvn.asia@gmail.com>
2026-04-26 16:18:09 +0700

    Add fix to guard against WebView items stealing focus

--- vcpkg/ports/qtbase/portfile.cmake
+++ vcpkg/ports/qtbase/portfile.cmake
@@ -27,6 +27,7 @@ set(${PORT}_PATCHES
         fix-libresolv-test.patch
         use_inotify_on_freebsd.patch
         fix-qyieldcpu-apple-clang.patch
+        webviewfocus.patch
 )
  
 if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
