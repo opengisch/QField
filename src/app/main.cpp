259013f7200c39c0c1c632448767e5c58fa939c0
Mathieu Pellerin <nirvn.asia@gmail.com>
2026-06-30 16:41:22 +0700

    Use QV4_FORCE_INTERPRETER to bypass JIT

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -114,6 +114,7 @@ void initAuthManager( QgsAuthManager *authManager )
 int main( int argc, char **argv )
 {
   qputenv( "QT_ANDROID_DISABLE_ACCESSIBILITY", "1" );
+  qputenv( "QV4_FORCE_INTERPRETER", "1" );
 
   QCoreApplication::setOrganizationName( "OPENGIS.ch" );
   QCoreApplication::setOrganizationDomain( "opengis.ch" );
