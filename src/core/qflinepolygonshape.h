e474c8f39a700ce2489f3c30462dbf10473950dc
Mathieu Pellerin <nirvn.asia@gmail.com>
2026-09-02 09:42:21 +0700

    Address review

--- src/core/qflinepolygonshape.h
+++ src/core/qflinepolygonshape.h
@@ -94,6 +94,9 @@ class QfLinePolygonShape : public QQuickItem
     double mGeometryMUPP = 0.0;
     QList<QPolygonF> mPolylines;
     Qgis::GeometryType mPolylinesType = Qgis::GeometryType::Null;
+
+    static constexpr double MAX_SIZE = 250000;
+    static constexpr double MAX_OFFSET = 200000;
 };
 
 #endif // QFLINEPOLYGONSHAPE_H
