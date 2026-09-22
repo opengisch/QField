6a131ec238188a726c604b48837c6beb1e09b721
Mathieu Pellerin <nirvn.asia@gmail.com>
2026-09-22 17:55:46 +0700

    Add group permissions

--- src/core/platforms/qfplatformutilities.cpp
+++ src/core/platforms/qfplatformutilities.cpp
@@ -245,7 +245,7 @@ bool QfPlatformUtilities::renameFile( const QString &oldFilePath, const QString 
   }
 
   // Insure correct permissions are allowing for user access
-  QFile::setPermissions( newFi.absoluteFilePath(), QFileDevice::ReadUser | QFileDevice::WriteUser | QFileDevice::ReadOwner | QFileDevice::WriteOwner );
+  QFile::setPermissions( newFi.absoluteFilePath(), QFileDevice::ReadUser | QFileDevice::WriteUser | QFileDevice::ReadOwner | QFileDevice::WriteOwner | QFileDevice::ReadGroup | QFileDevice::WriteGroup );
 
   return ok;
 }
